3bacb2fdb9f663e91fcc07604b71cfcec1b21c90
Robert Bartel <robert.bartel@noaa.gov>
2020-02-24 14:30:38 -0600

    Minor format refactoring of NGen.cpp.

--- src/core/NGen.cpp
+++ src/core/NGen.cpp
@@ -4,8 +4,8 @@
 #include "NGenConfig.h"
 #include "Hymod.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     std::cout << "Hello there " << ngen_VERSION_MAJOR << "."
-        << ngen_VERSION_MINOR << "."
-        << ngen_VERSION_PATCH << std::endl;
+              << ngen_VERSION_MINOR << "."
+              << ngen_VERSION_PATCH << std::endl;
 }
