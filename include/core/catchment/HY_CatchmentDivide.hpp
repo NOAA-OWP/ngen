aeb420534de6c6fd8ac5cde4ee48f57cbc939b77
Nels Frazier <nels.frazier@noaa.gov>
2020-02-27 08:31:20 -0600

    Removing erronous HY_CatchementDivide

--- include/core/catchment/HY_CatchmentDivide.hpp
+++ include/core/catchment/HY_CatchmentDivide.hpp
@@ -2,18 +2,33 @@
 #define HY_CATCHMENTDIVIDE_H
 
 #include "HY_CatchmentRealization.h"
+#include "GM_Object.h"
 
+#include <memory>
+#include <string>
 
-class HY_CatchmentDivide : public HY_CatchmentRealization
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/geometries.hpp>
+
+namespace bg = boost::geometry;
+
+class HY_CatchementDivide : public HY_CatchmentRealization, public GM_Object
 {
     public:
 
-    HY_CatchmentDivide();
-    virtual ~HY_CatchmentDivide();
+    typedef bg::model::point<double, 2, bg::cs::cartesian> point_t;
+    typedef bg::model::linestring<point_t> linestring_t;
+
+    HY_CatchementDivide();
+    virtual ~HY_CatchementDivide();
 
     protected:
 
     private:
-};
 
+    std::shared_ptr<linestring_t> divide_path;
+
+    unsigned long id_number;
+    std::string id;
+};
 #endif // HY_CATCHMENTDIVIDE_H
