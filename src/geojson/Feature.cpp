b61072e5e8acb93f29989c0a1e1f4df46f45b040
Chris Tubbs <christopher.tubbs@noaa.gov>
2020-03-24 11:03:17 -0500

    Initial Geojson work

--- src/geojson/Feature.cpp
+++ src/geojson/Feature.cpp
@@ -1,5 +1,19 @@
 #include "Feature.hpp"
 
+Feature::Feature(std::vector<JSONGeometry> new_geometry_collection,
+                 double *new_bounding_box,
+                 property_map new_properties
+) {
+    type = FeatureType::GeometryCollection;
+    geometry_collection = new_geometry_collection;
+    properties = new_properties;
+}
+
+JSONProperty Feature::get_property(std::string key) const {
+    return properties.at(key);
+}
+
+/*
 #include <utility>
 
 using geojson::Feature;
@@ -7,8 +21,8 @@ using geojson::JSONGeometry;
 using geojson::JSONProperty;
 
 Feature::Feature(std::vector<geojson::JSONGeometry> new_geometry_collection,
-        std::vector<double> new_bounding_box,
-        property_map new_properties) {
+                 std::vector<double> new_bounding_box,
+                 property_map new_properties) {
     type = geojson::FeatureType::GeometryCollection;
     geometry_collection = std::move(new_geometry_collection);
     properties = std::move(new_properties);
@@ -18,3 +32,4 @@ Feature::Feature(std::vector<geojson::JSONGeometry> new_geometry_collection,
 JSONProperty Feature::get_property(const std::string& key) const {
     return properties.at(key);
 }
+ */
