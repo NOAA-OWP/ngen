0633670eb480c96745120f984d2dc3f52bb9b03f
Robert Bartel <robert.bartel@noaa.gov>
2020-03-26 13:42:40 -0500

    Getting geojson code compiling successfully after last rebase onto d6d72dd6192, and incorporated into the rest of the project; this includes getting the automated tests running, though not with all tests pass successfully yet.

--- src/geojson/Feature.cpp
+++ src/geojson/Feature.cpp
@@ -1,14 +1,20 @@
 #include "Feature.hpp"
 
-Feature::Feature(std::vector<JSONGeometry> new_geometry_collection,
-        double *new_bounding_box,
-        property_map new_properties
-    ) {
-    type = FeatureType::GeometryCollection;
-    geometry_collection = new_geometry_collection;
-    properties = new_properties;   
+#include <utility>
+
+using geojson::Feature;
+using geojson::JSONGeometry;
+using geojson::JSONProperty;
+
+Feature::Feature(std::vector<geojson::JSONGeometry> new_geometry_collection,
+        std::vector<double> new_bounding_box,
+        property_map new_properties) {
+    type = geojson::FeatureType::GeometryCollection;
+    geometry_collection = std::move(new_geometry_collection);
+    properties = std::move(new_properties);
+    bounding_box = std::move(new_bounding_box);
 }
 
-JSONProperty Feature::get_property(std::string key) const {
+JSONProperty Feature::get_property(const std::string& key) const {
     return properties.at(key);
 }
