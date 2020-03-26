2fe111d39e3d1cf4addd79923e8e4663e5f2da39
John David Mattern <jdmattern@localhost.localdomain>
2020-03-26 14:58:45 -0400

    integrated nonlinear reservoir into hymod and add total outlet velocities to nonlinear reservoir

--- models/kernels/Nonlinear_Reservoir.hpp
+++ models/kernels/Nonlinear_Reservoir.hpp
@@ -72,7 +72,7 @@ class Nonlinear_Reservoir
     public:
 
     //Constructor for a reservoir with no outlets.
-    Nonlinear_Reservoir(double minimum_storage_meters, double maximum_storage_meters, double current_storage_height_meters)
+    Nonlinear_Reservoir(double minimum_storage_meters = 0.0, double maximum_storage_meters = 1.0, double current_storage_height_meters = 0.0)
     {
         parameters.minimum_storage_meters = minimum_storage_meters;
         parameters.maximum_storage_meters = maximum_storage_meters;
@@ -81,7 +81,7 @@ class Nonlinear_Reservoir
 
     //Constructor for a reservoir with only one outlet.
     Nonlinear_Reservoir(double minimum_storage_meters, double maximum_storage_meters, double current_storage_height_meters, double a, 
-    double b, double activation_threshold_meters) : Nonlinear_Reservoir(minimum_storage_meters, maximum_storage_meters, 	current_storage_height_meters)
+    double b, double activation_threshold_meters) : Nonlinear_Reservoir(minimum_storage_meters, maximum_storage_meters, current_storage_height_meters)
     {
         //Ensure that the activation threshold is less than the maximum storage
         //if (activation_threshold_meters > maximum_storage_meters)
@@ -107,14 +107,25 @@ class Nonlinear_Reservoir
     }	
 
     //Function to update the response of storage in meters to an influx and timestep.
-    void response_storage_meters(double in_flux_meters_per_second, int delta_time_seconds, double &excess_water_meters)
+    double response_storage_meters(double in_flux_meters_per_second, int delta_time_seconds, double &excess_water_meters)
     {	
+        double outlet_velocity_meters_per_second = 0;
+        double sum_of_outlet_velocities_meters_per_second = 0;
+
+        //Update current storage from influx multiplied by delta time.
         state.current_storage_height_meters += in_flux_meters_per_second * delta_time_seconds;
 	
+        //Loop through reservoir outlets.
         for (auto& outlet : this->outlets) //pointer to outlets
         {
+            //Calculate outlet velocity.
+            outlet_velocity_meters_per_second = outlet.velocity_meters_per_second(parameters, state);
+
             //Update storage from outlet velocity multiplied by delta time.
-            state.current_storage_height_meters -= outlet.velocity_meters_per_second(parameters, state) * delta_time_seconds;
+            state.current_storage_height_meters -= outlet_velocity_meters_per_second * delta_time_seconds;
+
+            //Add outlet velocity to the sum of velocities.
+            sum_of_outlet_velocities_meters_per_second += outlet_velocity_meters_per_second;
         }
 
         //If storage is less than minimum storage, set to minimum storage and 
@@ -138,6 +149,8 @@ class Nonlinear_Reservoir
         //If storage remains in bounds of the reservoir, return zero excess water to the input flux.
         else
             excess_water_meters = 0.0;
+
+        return sum_of_outlet_velocities_meters_per_second;
     }
 
     //Ensure that reservoir outlet activation thresholds are sorted from least to greatest height
