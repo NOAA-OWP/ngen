6a560c9d5f02f97fa8f0e2bae186d17f771a78d9
Donald Johnson <donald.w.johnson@noaa.gov>
2020-03-27 14:27:56 -0500

    Update hymod to include et function from pdm03.

--- models/kernels/Pdm03.h
+++ models/kernels/Pdm03.h
@@ -14,6 +14,7 @@ extern "C"
         double Cpar;
         double B;
         double XHuz;
+        double Huz;
         double OV;
         double AE;
         double XCuz;
@@ -91,6 +92,152 @@ extern "C"
 
       return;
 
+    }
+
+    void pdm03_wrapper(pdm03_struct* pdm_data)
+    {
+        return Pdm03(pdm_data->modelDay,
+        pdm_data->Cpar,
+        pdm_data->B,
+        &pdm_data->XHuz,
+        pdm_data->Huz,
+        &pdm_data->OV,
+        &pdm_data->AE,
+        &pdm_data->XCuz,
+        pdm_data->effPrecip,
+        pdm_data->PE,
+        pdm_data->Kv);
+    }
+
+    /*
+    %%=========================================================================
+    %% Hamon Potential ET model
+    %% INPUTS
+    %%   avgTemp      - average air temperature this time step (C)
+    %%   Latitude     - latitude of catchment centroid (degrees)
+    %%   day of year  - (1-365) or (1-366) in leap years
+    % OUTPUTS
+    %%   PE           - potential evapotranspiration this time step
+    %%=========================================================================
+    */
+
+    inline double calculateHamonPE(double avgTemp, double Latitude, int day_of_year)
+    {
+        double solarDeclination, dayLength, eStar, PE;
+
+        solarDeclination =
+            asin(0.39795*cos(0.2163108 + 2.0 * atan(0.9671396*tan(0.00860*
+                                                (double)(day_of_year-186)))));
+        dayLength = 24.0 - (24.0/M_PI)*(acos((sin(0.8333*M_PI/180.0)+
+                    sin(Latitude*M_PI/180.0)*sin(solarDeclination))/
+                    (cos(Latitude*M_PI/180.0)*cos(solarDeclination))));
+        eStar = 0.6108*exp((17.27*avgTemp)/(237.3+avgTemp));
+        PE = (715.5*dayLength/24.0)*eStar/(avgTemp + 273.2);
+
+        return PE;
+    }
+
+     /*
+     * convert Gregorian days to Julian date
+     *
+     * Modify as needed for your application.
+     *
+     * The Julian day starts at noon of the Gregorian day and extends
+     * to noon the next Gregorian day.
+     *
+     */
+    /*
+    ** Takes a date, and returns a Julian day. A Julian day is the number of
+    ** days since some base date  (in the very distant past).
+    ** Handy for getting date of x number of days after a given Julian date
+    ** (use jdate to get that from the Gregorian date).
+    ** Author: Robert G. Tantzen, translator: Nat Howard
+    ** Translated from the algol original in Collected Algorithms of CACM
+    ** (This and jdate are algorithm 199).
+    */
+
+
+    inline double greg_2_jul(long year, long mon, long day, long h, long mi, double se)
+    {
+        long m = mon, d = day, y = year;
+        long c, ya, j;
+        double seconds = h * 3600.0 + mi * 60 + se;
+
+        if (m > 2)
+        m -= 3;
+        else {
+        m += 9;
+        --y;
+        }
+        c = y / 100L;
+        ya = y - (100L * c);
+        j = (146097L * c) / 4L + (1461L * ya) / 4L + (153L * m + 2L) / 5L + d + 1721119L;
+        if (seconds < 12 * 3600.0) {
+        j--;
+        seconds += 12.0 * 3600.0;
+        }
+        else {
+        seconds = seconds - 12.0 * 3600.0;
+        }
+        return (j + (seconds / 3600.0) / 24.0);
+    }
+
+    /* Julian date converter. Takes a julian date (the number of days since
+    ** some distant epoch or other), and returns an int pointer to static space.
+    ** ip[0] = month;
+    ** ip[1] = day of month;
+    ** ip[2] = year (actual year, like 1977, not 77 unless it was  77 a.d.);
+    ** ip[3] = day of week (0->Sunday to 6->Saturday)
+    ** These are Gregorian.
+    ** Copied from Algorithm 199 in Collected algorithms of the CACM
+    ** Author: Robert G. Tantzen, Translator: Nat Howard
+    **
+    **
+    */
+    inline void calc_date(double jd, long *y, long *m, long *d, long *h, long *mi,
+                          double *sec)
+    {
+        static int ret[4];
+
+        long j;
+        double tmp;
+        double frac;
+
+        j=(long)jd;
+        frac = jd - j;
+
+        if (frac >= 0.5) {
+        frac = frac - 0.5;
+            j++;
+        }
+        else {
+        frac = frac + 0.5;
+        }
+
+        ret[3] = (j + 1L) % 7L;
+        j -= 1721119L;
+        *y = (4L * j - 1L) / 146097L;
+        j = 4L * j - 1L - 146097L * *y;
+        *d = j / 4L;
+        j = (4L * *d + 3L) / 1461L;
+        *d = 4L * *d + 3L - 1461L * j;
+        *d = (*d + 4L) / 4L;
+        *m = (5L * *d - 3L) / 153L;
+        *d = 5L * *d - 3 - 153L * *m;
+        *d = (*d + 5L) / 5L;
+        *y = 100L * *y + j;
+        if (*m < 10)
+        *m += 3;
+        else {
+        *m -= 9;
+        *y=*y+1;
+        }
+
+        tmp = 3600.0 * (frac * 24.0);
+        *h = (long) (tmp / 3600.0);
+        tmp = tmp - *h * 3600.0;
+        *mi = (long) (tmp / 60.0);
+        *sec = tmp - *mi * 60.0;
     }
 
 }
