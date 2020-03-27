b476a33213f599dae16ff3653b6cd389ef9e9526
Robert Bartel <robert.bartel@noaa.gov>
2020-03-26 22:44:20 -0500

    Moving one constructor from Feature.hpp to Feature.cpp, and ensure get_property function is in place.

--- src/geojson/Feature.cpp
+++ src/geojson/Feature.cpp
@@ -1,19 +1,5 @@
 #include "Feature.hpp"
 
-Feature::Feature(std::vector<JSONGeometry> new_geometry_collection,
-                 double *new_bounding_box,
-                 property_map new_properties
-) {
-    type = FeatureType::GeometryCollection;
-    geometry_collection = new_geometry_collection;
-    properties = new_properties;
-}
-
-JSONProperty Feature::get_property(std::string key) const {
-    return properties.at(key);
-}
-
-/*
 #include <utility>
 
 using geojson::Feature;
@@ -32,4 +18,3 @@ Feature::Feature(std::vector<geojson::JSONGeometry> new_geometry_collection,
 JSONProperty Feature::get_property(const std::string& key) const {
     return properties.at(key);
 }
- */
