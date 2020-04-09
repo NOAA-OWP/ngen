dba04cc0698949c036c9b63cdd79db580699b0ff
Nels Frazier <nels.frazier@noaa.gov>
2020-04-09 13:23:30 -0500

    more tweaks from Fred, refactored for style/consistency

--- models/kernels/schaake_test.cpp
+++ models/kernels/schaake_test.cpp
@@ -18,15 +18,15 @@ int    num_soil_discretizations;                        // nsoil;
 double timestep_s;                                      // dt;
 double *soil_discretization_thickness_m;                // *dz;
 double *soil_volumetric_moisture_content;               // *sh2o;
-double *depth_to_bottom_of_soil_discretization;         // *zsoil;
 double surface_input_flux_m_per_s;                      // qinsur;
-double surface_runoff_flux_m_per_s;                     // runsrf;
-double infiltration_rate_m_per_s;                       // pddum;
-double soil_effective_porosity;                         // smcmax;
+double water_input_depth_m;                             // partitioning is not rate based, so this depth is passed to Schaake function
+double surface_runoff_depth_m;                          // depth of applied water partitioned to surface water this time step
+double infiltration_depth_m;                            // depth of applied water partitioned to soil moisture this time step
+double soil_max_water_content;                          // smcmax;
 double soil_wilting_point_moisture_content;             // smcwlt;
 double *soil_max_water_storage_per_discretization_m;    // dmax;
 double soil_max_moisture_capacity;                      // smcmax;
-double column_total_soil_moisture_deficit_m;            // NEW variable by FLO to pass to infil() because that is what it really wants.
+double column_total_soil_moisture_deficit_m;            // passed to partitioning function because that is what it really wants.
 double soil_max_avail_moisture_capacity;                // dimensionless soil water storage capacity given as (effective porosity-wilting point water content)
 double soil_saturated_hydraulic_conductivity_m_per_s;   // dksat;  This is the value read in from the soil parameter table.
 double Schaake_magic_constant_equal_to_three;           // refkdt from GENPARM.dat this is _really_ just a constant equal to 3.0.
@@ -41,25 +41,22 @@ num_soil_discretizations=4;
 
 //ALLOCATE MEMORY FOR THESE FOUR ARRAYS
 d_alloc(&soil_discretization_thickness_m,num_soil_discretizations);
-d_alloc(&depth_to_bottom_of_soil_discretization,num_soil_discretizations);
 d_alloc(&soil_volumetric_moisture_content,num_soil_discretizations);
 d_alloc(&soil_max_water_storage_per_discretization_m,num_soil_discretizations);
 
-soil_discretization_thickness_m[0]=0.0;     // NOTE: it is strange that they use negative signs here, normally in soils physics,
-soil_discretization_thickness_m[1]=-0.10;   //       people create a coordinate system that is positive downward. -FLO
-soil_discretization_thickness_m[2]=-0.30;   //       The end result is that the math below is weird in calculating the thickness
-soil_discretization_thickness_m[3]=-0.60;   //       of the various discretizations, which are actually already known.
-soil_discretization_thickness_m[4]=-1.00;
+/* the thickness of the discretizations */
+soil_discretization_thickness_m[0]=0.0;     // NOTE: it is strange that they used negative signs here, normally in soils physics,
+soil_discretization_thickness_m[1]=0.10;   //       people create a coordinate system that is positive downward. -FLO
+soil_discretization_thickness_m[2]=0.30;   //       The end result is that the math below is weird in calculating the thickness
+soil_discretization_thickness_m[3]=0.60;   //       of the various discretizations, which are actually already known.
+soil_discretization_thickness_m[4]=1.00;   //          I define these as POSITIVE real numbers.
 
-soil_effective_porosity = 0.463999987;
+
+soil_max_water_content = 0.463999987;
 soil_wilting_point_moisture_content = 0.12;
 
-depth_to_bottom_of_soil_discretization[0]=0.0;
-for(k=1;k<=num_soil_discretizations;k++)
-  {
-  depth_to_bottom_of_soil_discretization[k]=depth_to_bottom_of_soil_discretization[k-1]+soil_discretization_thickness_m[k];
-  }
 
+/* the assumed initial soil moisture contents */
 soil_volumetric_moisture_content[0] = 0.0;
 soil_volumetric_moisture_content[1] = 0.285762608;
 soil_volumetric_moisture_content[2] = 0.292563140;
@@ -68,11 +65,23 @@ soil_volumetric_moisture_content[4] = 0.306821257;
 
 // jdate=greg_2_jul(year,month,day,hour,minute,dsec);
 
+
+// dt=1800.0; //seconds
+timestep_s = 900.0;
 /*! ----------------------------------------------------------------------*/
-/*! Set-up universal parameters (not dependent on SOILTYP, VEGTYP)        */
+/* soil type dependent params.                                            */
 /*! ----------------------------------------------------------------------*/
-/* DKSAT  = SATDK (SOILTYP) */
+/* the uniform soil properties needed to calculate storage deficit in soil column */
+
+soil_max_water_content = 0.464;                   // these would come from SOILPARM.TBL
+soil_wilting_point_moisture_content = 0.120;
+
+/* DKSAT  = SATDK (SOILTYP) */  // <- THIS IS AN EXCELLENT EXAMPLE OF OBFUSCATION!
 soil_saturated_hydraulic_conductivity_m_per_s=5.23E-6;                   // was dksat.  In noah-mp, this value from SOILPARM.TBL for a particular soil type
+
+/*! ------------------------------------------------------------------------------------------*/
+/*! Set-up "universal" parameters <- a funny comment from the original noah-mp code...        */
+/*! ------------------------------------------------------------------------------------------*/
 
 Schaake_reference_saturated_hydraulic_conductivity_m_per_s=2.0e-06;      // was refdk, the magical value of reference hydraulic conductivity identified as super cool by Schaake et al.
 Schaake_magic_constant_equal_to_three=3.0;                               // refkdt=3.0;     from GENPARM.TBL.  A constant equal to three.
@@ -81,27 +90,19 @@ Schaake_magic_constant_equal_to_three=3.0;                               // refk
 
 Schaake_adjusted_magic_constant_by_soil_type = Schaake_magic_constant_equal_to_three*(soil_saturated_hydraulic_conductivity_m_per_s/Schaake_reference_saturated_hydraulic_conductivity_m_per_s); // was kdt
 
-// dt=1800.0; //seconds
-timestep_s = 900.0;
-/*! ----------------------------------------------------------------------*/
-/* soil type dependent params.                                            */
-/*! ----------------------------------------------------------------------*/
-// soil_effective_porosity=0.40;
-soil_effective_porosity = 0.464;
-// soil_wilting_point_moisture_content=0.26;
-soil_wilting_point_moisture_content = 0.120;
 
-
-surface_input_flux_m_per_s=25.4;  // rate of water applied to soil surface [mm/h]
-surface_input_flux_m_per_s/=(double)3600000.0;  // rate of applied water in units of [m/s] */
+// forcing magnitude
+surface_input_flux_m_per_s=25.4;                            // rate of water applied to soil surface [mm/h] (human readable)
+surface_input_flux_m_per_s/=(double)3600000.0;              // rate of applied water converted into units of [m/s]
+water_input_depth_m=surface_input_flux_m_per_s*timestep_s;  // this is the amount of applied water this time step.
 
 // compute the average soil moisture deficit in all soil discretizations, and sum them up.
 // NOTE: if used with a nonlinear reservoir, you wouldn't do this.  Rather it would just be (storage_max_m - current_storage_m)
 
-soil_max_avail_moisture_capacity = soil_effective_porosity-soil_wilting_point_moisture_content;  // this is the volumetric pore space between effective saturation and wilting point.  Dimensionless.
-                                                                                              // NOTE: this is constant for a given soil type in noah-mp.  Doesn't need to be calculated each time in infilt()
+soil_max_avail_moisture_capacity = soil_max_water_content-soil_wilting_point_moisture_content;  // this is the volumetric pore space between effective saturation and wilting point.  Dimensionless.
+                                                                                                // NOTE: this is constant for a given soil type in noah-mp.  Doesn't need to be calculated each time in infilt()
 
-soil_max_water_storage_per_discretization_m[1]= -depth_to_bottom_of_soil_discretization[1] * soil_max_avail_moisture_capacity;                        // max.depth of water storage [m] in upper most soil discretization
+soil_max_water_storage_per_discretization_m[1]= soil_discretization_thickness_m[1] * soil_max_avail_moisture_capacity;                        // max.depth of water storage [m] in upper most soil discretization
 soil_max_water_storage_per_discretization_m[1]= soil_max_water_storage_per_discretization_m[1]* (1.0-(soil_volumetric_moisture_content[1] - soil_wilting_point_moisture_content)/soil_max_avail_moisture_capacity);  // Actual water storage deficit [m] in uppermost soil discretization
                                                    //     Note in previous line (soil_volumetric_moisture_content[1]-soil_wilting_point_moisture_content)/soil_max_avail_moisture_capacity is relative storage (0 to 1)
 
@@ -109,7 +110,7 @@ column_total_soil_moisture_deficit_m = soil_max_water_storage_per_discretization
 
 for(k = 2;k<=num_soil_discretizations;k++)
   {
-  soil_max_water_storage_per_discretization_m[k] = (depth_to_bottom_of_soil_discretization[k-1] - depth_to_bottom_of_soil_discretization[k]) * soil_max_avail_moisture_capacity;          // max. depth of moisture storage in kth discretization
+  soil_max_water_storage_per_discretization_m[k] = soil_discretization_thickness_m[k] * soil_max_avail_moisture_capacity;          // max. depth of moisture storage in kth discretization
   soil_max_water_storage_per_discretization_m[k] = soil_max_water_storage_per_discretization_m[k] * (1.0-(soil_volumetric_moisture_content[k] - soil_wilting_point_moisture_content)/soil_max_avail_moisture_capacity); // Actual water storage deficit [m] in kth discretization, dmax[k]*(1-(a ratio from 0 to 1))
   column_total_soil_moisture_deficit_m  += soil_max_water_storage_per_discretization_m[k];        // Sum of available water storage in entire profile [m].
   }
@@ -119,9 +120,13 @@ for(k = 2;k<=num_soil_discretizations;k++)
   printf("column_total_soil_moisture_deficit_m= %lf\n", column_total_soil_moisture_deficit_m);
 
   Schaake_partitioning_scheme(timestep_s,Schaake_adjusted_magic_constant_by_soil_type,column_total_soil_moisture_deficit_m,
-        surface_input_flux_m_per_s,&surface_runoff_flux_m_per_s,&infiltration_rate_m_per_s);          // this is all that this function really needs.
+        water_input_depth_m,&surface_runoff_depth_m,&infiltration_depth_m);          // this is all that this function really needs.
 
-fprintf(out_fptr,"%e %e %e\n",surface_input_flux_m_per_s,surface_runoff_flux_m_per_s,infiltration_rate_m_per_s);
+fprintf(out_fptr,"input water this timestep: %e mm partitioned into:\n            surface water: %e mm\n               soil water: %e mm\n                 residual: %e mm\n",
+         water_input_depth_m*1000.0,
+         surface_runoff_depth_m*1000.0,
+         infiltration_depth_m*1000.0,
+         water_input_depth_m-surface_runoff_depth_m-infiltration_depth_m);
 
 fclose(out_fptr);
 
