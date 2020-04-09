775d9cf4b8b4aeca88f0bb457e15e30456d85bfa
Nels Frazier <nels.frazier@noaa.gov>
2020-04-09 12:40:33 -0500

    Removing utility functions and unneeded includes

--- models/kernels/schaake_partitioning.hpp
+++ models/kernels/schaake_partitioning.hpp
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include <float.h>
 #include <math.h>
-#include <stdlib.h>
-#include <string.h>
 
-void infil(int nsoil, double dt, double refkdt, double refdk, double kdt, double smcmax, double smcwlt, 
+
+void infil(int nsoil, double dt, double refkdt, double refdk, double kdt, double smcmax, double smcwlt,
            double *dz, double *zsoil, double *sh2o, double qinsur,double *runsrf,double *pddum)
 {
 
@@ -28,7 +25,7 @@ void infil(int nsoil, double dt, double refkdt, double refdk, double kdt, double
   REAL,                     INTENT(IN) :: SICEMAX!maximum soil ice content (m3/m3)
 
 ! outputs
-  REAL,                    INTENT(OUT) :: RUNSRF !surface runoff [mm/s] 
+  REAL,                    INTENT(OUT) :: RUNSRF !surface runoff [mm/s]
   REAL,                    INTENT(OUT) :: PDDUM  !infiltration rate at surface
 
 ! locals
@@ -51,7 +48,7 @@ int k;
 double dt1,dd,val,ddt,px,smcav,infmax;
 double dmax[5];
 
-if (qinsur >  0.0) 
+if (qinsur >  0.0)
   {
   dt1 = dt /86400.0;
   smcav = smcmax - smcwlt;
@@ -77,7 +74,7 @@ if (qinsur >  0.0)
 
   if(qinsur>0.0) px=qinsur*dt;
   else           px=0.0;
-  
+
   /* px  = max(0.,qinsur * dt) */
   infmax = (px * (ddt / (px + ddt)))/ dt;
 
@@ -89,117 +86,3 @@ if (qinsur >  0.0)
 
 return;
 }
-
-/**************************************************************************/
-/**************************************************************************/
-/**************************************************************************/
-/* ALL THE STUFF BELOW HERE IS JUST UTILITY MEMORY AND TIME FUNCTION CODE */
-/**************************************************************************/
-/**************************************************************************/
-/**************************************************************************/
-
-/****************************************/
-void itwo_alloc(int ***array,int rows, int cols)
-{
-int  i,frows,fcols, numgood=0;
-int error=0;
-
-if ((rows==0)||(cols==0))
-  {
-  printf("Error: Attempting to allocate array of size 0\n");
-  exit;
-  }
-
-frows=rows+1;  /* added one for FORTRAN numbering */
-fcols=cols+1;  /* added one for FORTRAN numbering */
-
-*array=(int **)malloc(frows*sizeof(int *));
-if (*array) 
-  {
-  memset((*array), 0, frows*sizeof(int*));
-  for (i=0; i<frows; i++)
-    {
-    (*array)[i] =(int *)malloc(fcols*sizeof(int ));
-    if ((*array)[i] == NULL)
-      {
-      error = 1;
-      numgood = i;
-      i = frows;
-      }
-     else memset((*array)[i], 0, fcols*sizeof(int )); 
-     }
-   }
-return;
-}
-
-
-
-void dtwo_alloc(double ***array,int rows, int cols)
-{
-int  i,frows,fcols, numgood=0;
-int error=0;
-
-if ((rows==0)||(cols==0))
-  {
-  printf("Error: Attempting to allocate array of size 0\n");
-  exit;
-  }
-
-frows=rows+1;  /* added one for FORTRAN numbering */
-fcols=cols+1;  /* added one for FORTRAN numbering */
-
-*array=(double **)malloc(frows*sizeof(double *));
-if (*array) 
-  {
-  memset((*array), 0, frows*sizeof(double *));
-  for (i=0; i<frows; i++)
-    {
-    (*array)[i] =(double *)malloc(fcols*sizeof(double ));
-    if ((*array)[i] == NULL)
-      {
-      error = 1;
-      numgood = i;
-      i = frows;
-      }
-     else memset((*array)[i], 0, fcols*sizeof(double )); 
-     }
-   }
-return;
-}
-
-
-
-void d_alloc(double **var,int size)
-{
-  size++;  /* just for safety */
-
-   *var = (double *)malloc(size * sizeof(double));
-   if (*var == NULL)
-      {
-      printf("Problem allocating memory for array in d_alloc.\n");
-      return;
-      }
-   else memset(*var,0,size*sizeof(double));
-   return;
-}
-
-void i_alloc(int **var,int size)
-{
-   size++;  /* just for safety */
-
-   *var = (int *)malloc(size * sizeof(int));
-   if (*var == NULL)
-      {
-      printf("Problem allocating memory in i_alloc\n");
-      return; 
-      }
-   else memset(*var,0,size*sizeof(int));
-   return;
-}
-
-int dayofweek(double j)
-{
-    j += 0.5;
-    return (int) (j + 1) % 7;
-}
-
