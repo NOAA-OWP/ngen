9179091e4f2249a7833105c988b2b3d735fe0adc
Shengting Cui <shengting.cui@noaa.gov>
2020-04-13 20:07:53 +0000

    Modifies schaake_partitioning.hpp code variables from upper to lower case Modifies schaake_test.cpp code variables from upper to lower case Codes are tested and compared with the results of Fred's version and the orginial simple_v1.cpp and NoahMP fortran codes

--- models/kernels/schaake_partitioning.hpp
+++ models/kernels/schaake_partitioning.hpp
@@ -4,12 +4,12 @@
 #include <math.h>
 
 
-void Schaake_partitioning_scheme(double timestep_s, double Schaake_adjusted_magic_constant_by_soil_type,
+void schaake_partitioning_scheme(double timestep_s, double schaake_adjusted_magic_constant_by_soil_type,
                                  double column_total_soil_moisture_deficit_m, double water_input_depth_m,
                                  double *surface_runoff_depth_m, double *infiltration_depth_m)
 {
   int k;
-  double timestep_d,Schaake_parenthetical_term,Ic,Px,infilt_dep_m;
+  double timestep_d,schaake_parenthetical_term,ic,px,infilt_dep_m;
 
   if(0.0 < water_input_depth_m) {
     if (0.0 > column_total_soil_moisture_deficit_m) {
@@ -22,22 +22,22 @@ void Schaake_partitioning_scheme(double timestep_s, double Schaake_adjusted_magi
         // timestep_d is the time step in days.   They switch from dt in [s] to dt1 in [d] because kdt has units of [d^(-1)]
         timestep_d = timestep_s /86400.0;
         // calculate the parenthetical part of Eqn. 34 from Schaake et al. Note the magic constant has units of [d^(-1)]
-        Schaake_parenthetical_term = (1.0 - exp ( - Schaake_adjusted_magic_constant_by_soil_type * timestep_d));
+        schaake_parenthetical_term = (1.0 - exp ( - schaake_adjusted_magic_constant_by_soil_type * timestep_d));
         // From Schaake et al. Eqn. 2., using the column total moisture deficit
         // BUT the way it is used here, it is the cumulative soil moisture deficit in the entire soil profile.
         // "Layer" info not used in this subroutine.
         // NOTE: when column_total_soil_moisture_deficit_m becomes zero, which occurs when the soil column is saturated,
-        // then Ic=0 Where Ic in the Schaake paper is called the
+        // then ic=0 Where ic in the Schaake paper is called the
         // "spatially averaged infiltration capacity", and is defined in Eqn. 12.
-        Ic = column_total_soil_moisture_deficit_m * Schaake_parenthetical_term;
+        ic = column_total_soil_moisture_deficit_m * schaake_parenthetical_term;
 
         // Total water input to partitioning scheme this time step [m]
-        Px=water_input_depth_m;
+        px=water_input_depth_m;
 
         // This is eqn 24 from Schaake et al.
-        // NOTE: this is 0 in the case of a saturated soil column, when Ic=0.
+        // NOTE: this is 0 in the case of a saturated soil column, when ic=0.
         // Physically happens only if soil has no-flow lower b.c.
-        *infiltration_depth_m = (Px * (Ic / (Px + Ic)));
+        *infiltration_depth_m = (px * (ic / (px + ic)));
 
 
         if( 0.0 < (water_input_depth_m-(*infiltration_depth_m)) ){
