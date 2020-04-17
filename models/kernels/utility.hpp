a0a8fc88b8e20c74c4551d7404995b4c11cbc828
Shengting Cui <shengting.cui@noaa.gov>
2020-04-17 20:54:49 +0000

    Added schaake_partition, utility and unit test

--- models/kernels/utility.hpp
+++ models/kernels/utility.hpp
@@ -2,7 +2,7 @@
 #define KERNEL_UTILITY_H
 
 #include <iostream>
-
+#include <vector>
 /**************************************************************************/
 /**************************************************************************/
 /**************************************************************************/
@@ -11,109 +11,32 @@
 /**************************************************************************/
 /**************************************************************************/
 
-/****************************************/
-void itwo_alloc(int ***array,int rows, int cols)
+
+inline int dayofweek(double j)
 {
-int  i,frows,fcols, numgood=0;
-int error=0;
+  j += 0.5;
+  return (int) (j + 1) % 7;
+}
 
-if ((rows==0)||(cols==0))
-  {
-  printf("Error: Attempting to allocate array of size 0\n");
-  exit;
-  }
-
-frows=rows+1;  /* added one for FORTRAN numbering */
-fcols=cols+1;  /* added one for FORTRAN numbering */
-
-*array=(int **)malloc(frows*sizeof(int *));
-if (*array)
-  {
-  memset((*array), 0, frows*sizeof(int*));
-  for (i=0; i<frows; i++)
-    {
-    (*array)[i] =(int *)malloc(fcols*sizeof(int ));
-    if ((*array)[i] == NULL)
-      {
-      error = 1;
-      numgood = i;
-      i = frows;
-      }
-     else memset((*array)[i], 0, fcols*sizeof(int ));
-     }
-   }
-return;
+inline void itwo_alloc(std::vector<std::vector<int>> *array,int rows, int cols)
+{
+    array->resize(rows, std::vector<int>(cols));
 }
 
 
-
-void dtwo_alloc(double ***array,int rows, int cols)
+inline void dtwo_alloc(std::vector<std::vector<double>>  *array, int rows, int cols)
 {
-int  i,frows,fcols, numgood=0;
-int error=0;
-
-if ((rows==0)||(cols==0))
-  {
-  printf("Error: Attempting to allocate array of size 0\n");
-  exit;
-  }
-
-frows=rows+1;  /* added one for FORTRAN numbering */
-fcols=cols+1;  /* added one for FORTRAN numbering */
-
-*array=(double **)malloc(frows*sizeof(double *));
-if (*array)
-  {
-  memset((*array), 0, frows*sizeof(double *));
-  for (i=0; i<frows; i++)
-    {
-    (*array)[i] =(double *)malloc(fcols*sizeof(double ));
-    if ((*array)[i] == NULL)
-      {
-      error = 1;
-      numgood = i;
-      i = frows;
-      }
-     else memset((*array)[i], 0, fcols*sizeof(double ));
-     }
-   }
-return;
+    array->resize(rows, std::vector<double>(cols));
 }
 
-
-
-void d_alloc(double **var,int size)
+inline void d_alloc(std::vector<double> *var,int size)
 {
-  size++;  /* just for safety */
-
-   *var = (double *)malloc(size * sizeof(double));
-   if (*var == NULL)
-      {
-      printf("Problem allocating memory for array in d_alloc.\n");
-      return;
-      }
-   else memset(*var,0,size*sizeof(double));
-   return;
+    var->resize(size);
 }
 
-void i_alloc(int **var,int size)
+inline void i_alloc(std::vector<int> *var,int size)
 {
-   size++;  /* just for safety */
-
-   *var = (int *)malloc(size * sizeof(int));
-   if (*var == NULL)
-      {
-      printf("Problem allocating memory in i_alloc\n");
-      return;
-      }
-   else memset(*var,0,size*sizeof(int));
-   return;
-}
-
-int dayofweek(double j)
-{
-    j += 0.5;
-    return (int) (j + 1) % 7;
+    var->resize(size);
 }
 
 #endif // KERNEL_UTILITY_H
