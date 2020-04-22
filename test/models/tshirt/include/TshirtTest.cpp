a39f3fc018931727046a1a405142075ab220bcdb
Robert Bartel <robert.bartel@noaa.gov>
2020-04-22 11:28:12 -0500

    Add trivial unit test for Tshirt kernel class.
    Adding the file and shell of testing class for Tshirt model, along with
    very simple test case that just make sure the run() function completes
    without error when given arbitrary (but valid) arguments.

---
+++ test/models/tshirt/include/TshirtTest.cpp
@@ -0,0 +1,61 @@
+#include "gtest/gtest.h"
+#include "tshirt/include/Tshirt.h"
+
+class TshirtKernelTest : public ::testing::Test {
+
+protected:
+
+    TshirtKernelTest() {
+
+    }
+
+    ~TshirtKernelTest() override {
+
+    }
+
+    void SetUp() override;
+
+    void TearDown() override;
+
+    void setupArbitraryExampleCase();
+
+};
+
+void TshirtKernelTest::SetUp() {
+
+}
+
+void TshirtKernelTest::TearDown() {
+
+}
+
+//! Test that Tshirt executes its 'run' function fully when passed arbitrary valid arguments.
+TEST_F(TshirtKernelTest, TestRun0)
+{
+    // TODO: fix what was copied below from Hymod test for Tshirt
+
+
+    double et_storage = 0.0;
+
+    tshirt_params params{1000.0, 1.0, 10.0, 0.1, 0.01, 3, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+    double storage = 1.0;
+
+    tshirt_state state(1.0,1.0);
+
+    tshirt_state new_state(0.0,0.0);
+
+    tshirt_fluxes fluxes(0.0, 0.0, 0.0, 0.0);
+    double input_flux = 1.0;
+
+    //hymod_kernel::run(params, h_state, ks_fluxes, new_state, new_fluxes, input_flux, et_params);
+    tshirt_kernel::run(86400.0,
+                      params,
+                      state,
+                      new_state,
+                      fluxes,
+                      input_flux,
+                      &et_storage);
+
+    ASSERT_TRUE(true);
+}
+
