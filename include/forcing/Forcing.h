aba0511a2d99c533e93720913a1bb7e22869de2d
John David Mattern <jdmattern@localhost.localdomain>
2020-04-23 00:05:13 -0400

    added date-time elements to forcing

--- include/forcing/Forcing.h
+++ include/forcing/Forcing.h
@@ -6,6 +6,10 @@
 #include <iostream>
 #include <boost/algorithm/string.hpp>
 #include "CSV_Reader.h"
+#include <chrono>  // chrono::system_clock
+#include <ctime>
+#include <time.h>
+
 
 
 /*
@@ -38,18 +42,78 @@ class Forcing
 {
     public:
 
+// Add start and end time to constructor
+
+//Make forcing csv subclass.
+
+//day_of_year would be a vector and not passed in. built alongside the precip vector
+//could be vector circular of vector of 365. Look into date-time. Maybe in AD hydro code. 
+
     //Default Constructor
-    Forcing(): air_temperature_fahrenheit(0.0), basin_latitude(0.0), day_of_year(0)
+    Forcing(): air_temperature_fahrenheit(0.0), basin_latitude(0.0), day_of_year(0), forcing_file_name("")
     {
 
     }
 
+
+
+    Forcing(double air_temperature_fahrenheit, double basin_latitude, int day_of_year, string forcing_file_name, tm *start_date_time, tm *end_date_time): air_temperature_fahrenheit(air_temperature_fahrenheit), basin_latitude(basin_latitude), day_of_year(day_of_year), forcing_file_name(forcing_file_name), start_date_time(*start_date_time), end_date_time(*end_date_time)
+    {
+
+        //Call read_forcing function
+        read_forcing(forcing_file_name); //also pass in start and end times
+
+        //Initialize forcing vector index to 0;
+        *forcing_vector_index_ptr = 0;
+    }
+
+
+
+
+/*
+template<typename Clock>
+Forcing(double air_temperature_fahrenheit, double basin_latitude, int day_of_year, std::chrono::time_point<Clock> start_date_time, std::chrono::time_point<Clock> end_date_time): air_temperature_fahrenheit(air_temperature_fahrenheit), basin_latitude(basin_latitude), day_of_year(day_of_year), std::chrono::time_point<Clock>(start_date_time), std::chrono::time_point<Clock>(end_date_time)
+    {
+
+    //call read forcing
+
+
+    }
+*/
+
+/*
+template<typename Clock>
+Forcing(double air_temperature_fahrenheit, double basin_latitude, int day_of_year, string forcing_file_name, std::chrono::time_point<Clock> start_date_time, std::chrono::time_point<Clock> end_date_time): air_temperature_fahrenheit(air_temperature_fahrenheit), basin_latitude(basin_latitude), day_of_year(day_of_year), forcing_file_name(forcing_file_name), start_date_time(start_date_time), end_date_time(end_date_time)
+    {
+
+        //Call read_forcing function
+        read_forcing(forcing_file_name); //also pass in start and end times
+
+        //Initialize forcing vector index to 0;
+        *forcing_vector_index_ptr = 0;
+    }
+*/
+
+/////////////PREVIOUS
+//add file name
     //Parameterized Constructor
+/*
     Forcing(double air_temperature_fahrenheit, double basin_latitude, int day_of_year): air_temperature_fahrenheit(air_temperature_fahrenheit), basin_latitude(basin_latitude), day_of_year(day_of_year)
     {
 
+    //call read forcing
     }
+*/
 
+
+//builds w/ 2 lines below
+//template<typename Clock>
+//void my_function(std::chrono::time_point<Clock> time_point);
+
+
+    private:
+
+    //Make private and call this function from constuctor
     //Read Forcing Data from CSV
     //void read_forcing(std::string file_name)
     void read_forcing(string file_name)    //builds fine
@@ -60,20 +124,49 @@ class Forcing
 	// Get the data from CSV File
 	std::vector<std::vector<std::string> > data_list = reader.getData();
 
-        vector<double> precipitation_vector_meters_per_second;
+        //vector<double> precipitation_vector_meters_per_second_vector;
 
         std::string::size_type sz;     // alias of size_t
 
         // Print the content of row by row on screen
         for(std::vector<std::string> vec : data_list)
         {
+                 
+                //Year
+                string year_str = vec[0];
 
+                //Month
+                string month_str = vec[1];
+              
+                if (sizeof(month_str) < 2)
+                    month_str = "0" + month_str;
+ 
+                //Day
+                string day_str = vec[2];
+
+                if (sizeof(day_str) < 2)
+                    day_str = "0" + day_str;           
+
+                //Hour
+                string hour_str = vec[3];
+
+                if (sizeof(hour_str) < 2)
+                    hour_str = "0" + hour_str; 
+
+                //Concatentate full date-time
+                string date_time_str = year_str + month_str + day_str + hour_str;
+
+                ///////////////////
+                //if within date range, then add precip to precip vector
+
+                //Precipitation
                 string precip_str = vec[5];
 
                 //Convert from string to double and from mm/hr to m/s
                 double precip = atof(precip_str.c_str()) / (1000 * 3600);
 
-                precipitation_vector_meters_per_second.push_back(precip);
+                precipitation_vector_meters_per_second_vector.push_back(precip);
+
 
         }
 
@@ -91,10 +184,60 @@ class Forcing
     }
 
 
-    private:
+//function for current (current time what forcing object is poitning at) no parameters, be clear of what frequency is
 
+//documeent assumption that it is hourly for now
+//function for get_next (inc ptr and then call return current function)  TODO: (could have dt for different freq data than model)
+
+//future quesiton: how do I agg data?
+
+    //Precipitation frequency is assumed to be hourly for now.
+    //TODO: Add input for dt (delta time) for different frequencies in the data than the model frequency.
+    double get_current_hourly_precipitation_meters_per_second()
+    { 
+        return precipitation_vector_meters_per_second_vector[*forcing_vector_index_ptr];
+
+    }
+
+    //Precipitation frequency is assumed to be hourly for now.
+    //TODO: Add input for dt (delta time) for different frequencies in the data than the model frequency.
+    double get_next_hourly_precipitation_meters_per_second()
+    {
+        //Increment forcing index
+        //*forcing_vector_index = *forcing_vector_index + 1;
+        *forcing_vector_index_ptr++;
+
+        return get_current_hourly_precipitation_meters_per_second();
+
+    }
+
+
+//ptr for
+    vector<double> precipitation_vector_meters_per_second_vector;
+    int *forcing_vector_index_ptr; 
     double precipitation_rate_meters_per_second;
     double air_temperature_fahrenheit;
     double basin_latitude;
     int day_of_year;
+    string forcing_file_name;
+    tm start_date_time;
+    tm end_date_time;
+
+
+/*
+    template<typename Clock>
+    std::chrono::time_point<Clock> start_date_time;
+    template<typename Clock>
+    std::chrono::time_point<Clock> end_date_time;
+*/
+    /////////////
+    //std::chrono::steady_clock::time_point start_date_time;
+    //std::chrono::steady_clock::time_point end_date_time;
+    //////////// 
+
+    //std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+
+
+    //template<typename Clock>
+    //std::chrono::time_point<Clock> time_point;
 };
--- test/forcing/Forcing_Test.cpp
+++ test/forcing/Forcing_Test.cpp
@@ -8,6 +8,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <limits.h>
+#include <chrono>  // chrono::system_clock
+#include <ctime>
 
 
 class ForcingTest : public ::testing::Test {
@@ -35,10 +37,95 @@ void ForcingTest::TearDown() {
 
 }
 
+
 //Construct a forcing object
 void ForcingTest::setupForcing()
 {
-    Forcing_Object1 = std::make_shared<Forcing>(0.0, 0.0, 0);
+    //auto now1 = std::chrono::system_clock::now();
+
+    //auto now2 = std::chrono::system_clock::now();
+
+    //auto now1 = std::chrono::high_resolution_clock::now();
+
+    //auto now2 = std::chrono::high_resolution_clock::now();
+
+
+    //std::chrono::system_clock::time_point now1 = std::chrono::system_clock::now();
+
+    //std::chrono::system_clock::time_point now2 = std::chrono::system_clock::now();
+
+
+    //std::chrono::steady_clock::time_point now1 = std::chrono::steady_clock::now();
+
+    //std::chrono::steady_clock::time_point now2 = std::chrono::steady_clock::now();
+
+
+    //std::chrono::steady_clock::time_point start_date_time;
+
+    //std::chrono::steady_clock::time_point end_date_time;
+
+
+    //Forcing_Object1 = std::make_shared<Forcing>(0.0, 0.0, 0);
+
+cout << "a0";
+
+    string forcing_file_name = "../test/forcing/Sample_Tropical_Hourly_Rainfall.csv";
+
+cout << "a1";
+    
+    struct tm *start_date_time;
+
+    start_date_time->tm_year = 108;
+    start_date_time->tm_mon = 5;
+    start_date_time->tm_mday = 19;
+    start_date_time->tm_hour = 15;
+
+cout << "a2";
+
+    //string start_date_time_str = asctime(start_date_time);
+
+    //cout << start_date_time_str;
+
+
+    //mktime ( start_date_time );
+
+    //cout << "start_date_time: " + start_date_time << endl; 
+
+    //cout << "start_date_time: " + asctime(start_date_time) << endl; 
+
+    //char buffer [80];
+    //strftime (buffer,80,"Now it's %I:%M%p.",start_date_time);
+    //puts (buffer);
+
+    //char* dt = ctime(&start_date_time);
+
+
+    //char buffer[26];
+    //strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", start_date_time);
+    //puts(buffer);
+
+    //char buffer[26];
+    //strftime(buffer, 6, "%Y-%m-%d %H", start_date_time);
+    //puts(buffer);
+    //cout << buffer;
+cout << "a3";
+
+    struct tm *end_date_time;
+
+    end_date_time->tm_year = 108;
+    end_date_time->tm_mon = 5;
+    end_date_time->tm_mday = 22;
+    end_date_time->tm_hour = 20;
+
+cout << "a4";
+
+    //cout << "end_date_time: " + asctime(end_date_time) << endl; 
+
+    Forcing_Object1 = std::make_shared<Forcing>(0.0, 0.0, 0, forcing_file_name, start_date_time, end_date_time);
+
+
+
+    //Forcing_Object1 = std::make_shared<Forcing>(0.0, 0.0, 0, forcing_file_name, now1, now2);
 }
 
 
@@ -61,11 +148,10 @@ TEST_F(ForcingTest, TestForcingDataRead)
    
    //forcing_file_name = getcwd();
 
-   Forcing_Object1->read_forcing(forcing_file_name);
-
-
+   //Forcing_Object1->read_forcing(forcing_file_name);
 
 }
+
 
 
 
