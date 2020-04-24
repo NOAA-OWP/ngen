f31d5c8b2d8c486850c69f16c3bb1575c3f4d6e6
Nels Frazier <nels.frazier@noaa.gov>
2020-04-24 11:03:32 -0500

    geojson linking not working, squash later

--- src/NGen.cpp
+++ src/NGen.cpp
@@ -1,9 +1,77 @@
 #include <iostream>
 
+#include <FeatureBuilder.hpp>
+#include <FeatureVisitor.hpp>
+
 #include "NGenConfig.h"
+
+std::string catchmentRealizationFile = "/Users/nels.frazier/workspace/ngen/data/sugar_creek/catchment_data.geojson";
+std::string nexusRealizationFile = "/Users/nels.frazier/workspace/ngen/data/sugar_creek/nexus_data.geojson";
+
+//TODO this is possible, but ASSUMES realizations based on feature geom type, so not quite ready for prime time
+class RealizaitonVisitor : public geojson::FeatureVisitor {
+
+};
 
 int main(int argc, char *argv[]) {
     std::cout << "Hello there " << ngen_VERSION_MAJOR << "."
               << ngen_VERSION_MINOR << "."
               << ngen_VERSION_PATCH << std::endl;
+
+    //Read the collection of nexus
+    std::cout << "Building Nexus collection" << std::endl;
+    geojson::GeoJSON nexus_collection = geojson::read(nexusRealizationFile);
+    std::cout << "Iterating Nexus Features" << std::endl;
+
+    for(auto& feature: *nexus_collection){
+      feature->set_id(feature->get_property("ID").as_string());
+      //std::cout << "Got Nexus Feature " << feature->get_id() << std::endl;
+    }
+
+    //Now read the collection of catchments, iterate it and add them to the nexus collection
+    //also link them by to->id
+    std::cout << "Building Catchment collection" << std::endl;
+    geojson::GeoJSON catchment_collection = geojson::read(catchmentRealizationFile);
+    std::cout << "Iterating Catchment Features" << std::endl;
+    for(auto& feature: *catchment_collection){
+      feature->set_id(feature->get_property("ID").as_string());
+      nexus_collection->add_feature(feature);
+    }
+    std::string linkage = "toID";
+    nexus_collection->link_features_from_property(nullptr, &linkage);
+
+    for(auto& feature: *nexus_collection){
+      //feature->set_id(feature->get_property("ID").as_string());
+      //std::cout << "Got Nexus Feature " << feature->get_id() << std::endl;
+      if( feature->get_id().substr(0, 3) == "cat") {
+        auto downstream = feature->upstream_features();
+        if(downstream.size() > 1) {
+          std::cerr << "catchment " << feature->get_id() << " has more than one downstream connection" << std::endl;
+        }
+        else if(downstream.size() == 0) {
+            std::cerr << "catchment " << feature->get_id() << " has NO downstream connection" << std::endl;
+        }
+        else {
+          std::cout <<"catchment feature " << feature->get_id() << " to nexus feature " << downstream[0]->get_id() << std::endl;
+        }
+      }
+    }
+    /*
+        The basic driving algorithm looks something like this:
+
+        Iteration 1: Only read catchments and nexus
+        Read configuration;
+        From configuration, read inputs
+        inputs: id, realization enum, input file (geojson), forcing provider (enum)
+
+        loop:
+          id -> read geojson -> lookup by id -> construct catchment realization and nexus
+        TODO catchment realizations will need to construct forcing objects (implement forcing provider interface) upon constructuion
+        One INDEPDENT timestep
+        run catchment A -> forcing.get(time, dt, <grid?> ) -> run_model(time, dt, forcing)
+        run catchment B
+
+        Iteration 2: Read waterbodies, build linkage between nexus/waterbody, apply routing, multiple timesteps
+
+    */
 }
