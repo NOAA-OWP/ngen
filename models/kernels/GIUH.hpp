47c4a5a9ff0a9159ce28fe07d818e31b91651f8f
Robert Bartel <robert.bartel@noaa.gov>
2020-05-11 20:56:24 -0500

    Initial, incredibly bare-bones GIUH kernel class.

---
+++ models/kernels/GIUH.hpp
@@ -0,0 +1,18 @@
+#ifndef NGEN_GIUH_H
+#define NGEN_GIUH_H
+
+class giuh_kernel
+{
+
+    // TODO: create more complete class definition and implementation
+
+public:
+    double calc_giuh_output(double dt, double direct_runoff)
+    {
+        // TODO: implement to actually performs some real calculations
+        return direct_runoff;
+    }
+
+};
+
+#endif //NGEN_GIUH_H
