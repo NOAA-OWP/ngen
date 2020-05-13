cc1a0c24479a4acc422a839a494f5af7dce4928d
Robert Bartel <robert.bartel@noaa.gov>
2020-05-12 20:48:33 -0500

    Creating cpp file for giuh_kernel definition.

---
+++ src/core/catchment/giuh/GIUH.cpp
@@ -0,0 +1,7 @@
+#include "GIUH.hpp"
+
+double giuh_kernel::calc_giuh_output(double dt, double direct_runoff)
+{
+    // TODO: implement to actually performs some real calculations
+    return direct_runoff;
+}
