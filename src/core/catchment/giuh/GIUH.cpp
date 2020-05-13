0cd119ca2d7d83f338561395d25b8bcd0dbbd1c1
Robert Bartel <robert.bartel@noaa.gov>
2020-05-12 21:02:10 -0500

    Move giuh_kernel class to new giuh namespace.

--- src/core/catchment/giuh/GIUH.cpp
+++ src/core/catchment/giuh/GIUH.cpp
@@ -1,5 +1,7 @@
 #include "GIUH.hpp"
 
+using namespace giuh;
+
 double giuh_kernel::calc_giuh_output(double dt, double direct_runoff)
 {
     // TODO: implement to actually performs some real calculations
