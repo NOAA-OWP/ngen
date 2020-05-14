1c8ff083cf76fe7248f3e1a66d3e2ae7a386ded3
Robert Bartel <robert.bartel@noaa.gov>
2020-05-14 12:20:44 -0500

    Implement giuh_kernel::get_catchment_id function.

--- src/core/catchment/giuh/GIUH.cpp
+++ src/core/catchment/giuh/GIUH.cpp
@@ -7,3 +7,8 @@ double giuh_kernel::calc_giuh_output(double dt, double direct_runoff)
     // TODO: implement to actually performs some real calculations
     return direct_runoff;
 }
+
+std::string giuh_kernel::get_catchment_id()
+{
+    return catchment_id;
+}
