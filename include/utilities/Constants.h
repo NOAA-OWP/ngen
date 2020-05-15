39deaf5673765845ebf85a377f935c80d8946e13
Robert Bartel <robert.bartel@noaa.gov>
2020-05-15 15:21:42 -0500

    Rename of STANDARD_ATMOSPHERIC_PRESSURE_PASCALS.
    Renaming the macro constant ATMOSPHERIC_PRESSURE_PASCALS to
    STANDARD_ATMOSPHERIC_PRESSURE_PASCALS.

--- include/utilities/Constants.h
+++ include/utilities/Constants.h
@@ -2,7 +2,8 @@
 #ifndef NGEN_CONSTANTS_H
 #define NGEN_CONSTANTS_H
 
-#define ATMOSPHERIC_PRESSURE_PASCALS 101325
+// "Standard" Atmosphere pressure
+#define STANDARD_ATMOSPHERIC_PRESSURE_PASCALS 101325
 // Units for water specific weight value: Newtons / meters^3
 #define WATER_SPECIFIC_WEIGHT 9810
 
