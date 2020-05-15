d34bf94fb7ba0af450c770441975aad4df1f0009
Robert Bartel <robert.bartel@noaa.gov>
2020-05-15 11:09:17 -0500

    Putting shell giuh_kernel in dedicated namespace.

--- models/kernels/GIUH.hpp
+++ models/kernels/GIUH.hpp
@@ -1,18 +1,18 @@
 #ifndef NGEN_GIUH_H
 #define NGEN_GIUH_H
 
-class giuh_kernel
-{
+namespace giuh {
+    class giuh_kernel {
 
-    // TODO: create more complete class definition and implementation
+        // TODO: create more complete class definition and implementation
 
-public:
-    double calc_giuh_output(double dt, double direct_runoff)
-    {
-        // TODO: implement to actually performs some real calculations
-        return direct_runoff;
-    }
+    public:
+        double calc_giuh_output(double dt, double direct_runoff) {
+            // TODO: implement to actually performs some real calculations
+            return direct_runoff;
+        }
 
-};
+    };
+}
 
 #endif //NGEN_GIUH_H
