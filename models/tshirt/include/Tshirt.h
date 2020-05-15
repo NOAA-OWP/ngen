c7ce6107441280406e3c1a284b677a1746d5cf9a
Robert Bartel <robert.bartel@noaa.gov>
2020-05-14 22:57:15 -0500

    Declare tshirt_model class.
    Declare a new tshirt_model class that is similar in purpose to the
    existing tshirt_kernel class, except that the former manages its
    internal state, while the latter is only used for its static functions.

--- models/tshirt/include/Tshirt.h
+++ models/tshirt/include/Tshirt.h
@@ -8,6 +8,7 @@
 #include "GIUH.hpp"
 #include "Pdm03.h"
 #include <cmath>
+#include <utility>
 #include <vector>
 
 namespace tshirt {
@@ -111,6 +112,112 @@ namespace tshirt {
     enum TshirtErrorCodes {
         TSHIRT_NO_ERROR = 0,
         TSHIRT_MASS_BALANCE_ERROR = 100
+    };
+
+    /**
+     * Tshirt model class.
+     *
+     * A less static, more OO implementation of the Tshirt hydrological model.
+     */
+    class tshirt_model {
+
+    public:
+
+        tshirt_model(tshirt_params model_params,
+                     const shared_ptr<tshirt_state>& initial_state) : model_params(model_params),
+                                                                      previous_state(initial_state),
+                                                                      current_state(initial_state) {
+            // This will get used a few times ...
+            double Sfc = calc_soil_field_capacity_storage();
+
+            // Create the vector of Nash Cascade reservoirs used at the end of the soil lateral flow outlet
+            soil_lf_nash_res.resize(model_params.nash_n);
+            // TODO: verify correctness of activation_threshold (Sfc) and max_velocity (max_lateral_flow) arg values
+            for (unsigned long i = 0; i < soil_lf_nash_res.size(); ++i) {
+                //construct a single outlet nonlinear reservoir
+                soil_lf_nash_res[i] = make_unique<Nonlinear_Reservoir>(
+                        Nonlinear_Reservoir(0.0, model_params.max_soil_storage_meters,
+                                            previous_state->nash_cascade_storeage_meters[i], model_params.Kn, 1.0,
+                                            Sfc, model_params.max_lateral_flow));
+            }
+
+            // Create the soil reservoir
+            // TODO: probably will need to change this to be vector of pointers once Reservoir class changes are done
+            vector<Reservoir_Outlet> soil_res_outlets(2);
+
+            // init subsurface later flow outlet
+            soil_res_outlets[lf_outlet_index] = Reservoir_Outlet(model_params.Klf, 1.0, Sfc,
+                                                                 model_params.max_lateral_flow);
+            // init subsurface percolation flow outlet
+            // The max perc flow should be equal to the params.satdk value
+            soil_res_outlets[perc_outlet_index] = Reservoir_Outlet(model_params.satdk * model_params.slope, 1.0, Sfc,
+                                                                   model_params.satdk);
+
+            soil_reservoir = Nonlinear_Reservoir(0.0, model_params.depth, previous_state->soil_storage_meters, soil_res_outlets);
+
+            // Create the groundwater reservoir
+            // Given the equation:
+            //      double groundwater_flow_meters_per_second = params.Cgw * ( exp(params.expon * state.groundwater_storage_meters / params.max_groundwater_storage_meters) - 1 );
+            // The max value should be when groundwater_storage_meters == max_groundwater_storage_meters, or ...
+            double max_gw_velocity = model_params.Cgw * (exp(model_params.expon) - 1);
+            // TODO: verify activation threshold
+            groundwater_reservoir = Nonlinear_Reservoir(0, model_params.max_groundwater_storage_meters,
+                                                        previous_state->groundwater_storage_meters, model_params.Cgw, 1,
+                                                        0, max_gw_velocity);
+            fluxes = nullptr;
+
+        }
+
+        tshirt_model(tshirt_params model_params) :
+            tshirt_model(model_params, make_shared<tshirt_state>(tshirt_state(0.0, 0.0))) {}
+
+        /**
+         * Calculate losses due to evapotranspiration.
+         *
+         * @param soil_m
+         * @param et_params
+         * @return
+         */
+        double calc_evapotranspiration(double soil_m, pdm03_struct *et_params);
+
+        /**
+         * Calculate soil field capacity storage, the level at which free drainage stops (i.e., "Sfc").
+         *
+         * @return
+         */
+        double calc_soil_field_capacity_storage();
+
+        /**
+         * Run the model to one time step, moving the initial `current_state` value to `previous_state` and resetting
+         * other members applicable only to in the context of the current time step so that they are recalculated.
+         *
+         * @param dt the time step
+         * @param input_flux_meters the amount water entering the system this time step, in meters
+         * @param et_params ET parameters struct
+         * @return
+         */
+        int run(double dt, double input_flux_meters, pdm03_struct *et_params);
+
+    private:
+        /** Model state for the "current" time step, which may not be calculated yet. */
+        shared_ptr<tshirt_state> current_state;
+        /** Model execution parameters. */
+        tshirt_params model_params;
+        /** Model state from that previous time step before the current. */
+        shared_ptr<tshirt_state> previous_state;
+        /**
+         * A collection of reservoirs for a Nash Cascade at the end of the lateral flow output from the subsurface soil
+         * reservoir.
+         */
+        vector<unique_ptr<Nonlinear_Reservoir>> soil_lf_nash_res;
+        /** The index of the subsurface lateral flow outlet in the soil reservoir. */
+        int lf_outlet_index = 0;
+        /** The index of the percolation flow outlet in the soil reservoir. */
+        int perc_outlet_index = 1;
+        Nonlinear_Reservoir soil_reservoir;
+        Nonlinear_Reservoir groundwater_reservoir;
+        shared_ptr<tshirt_fluxes> fluxes;
+
     };
 
     /*!
