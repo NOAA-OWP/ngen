a682c2817367ebad114aec740038e67988aa0e6a
Nels Frazier <nels.frazier@noaa.gov>
2020-05-18 10:08:47 -0500

    Moving catchment/nexus linking to function prepare_features

--- src/NGen.cpp
+++ src/NGen.cpp
@@ -5,43 +5,35 @@
 
 #include "NGenConfig.h"
 
-std::string catchmentRealizationFile = "/Users/nels.frazier/workspace/ngen/data/sugar_creek/catchment_data.geojson";
-std::string nexusRealizationFile = "/Users/nels.frazier/workspace/ngen/data/sugar_creek/nexus_data.geojson";
+std::string catchmentRealizationFile = "/Users/nels.frazier/workspace/ngen/data/sugar_creek/catchment_data_subset.geojson";
+std::string nexusRealizationFile = "/Users/nels.frazier/workspace/ngen/data/sugar_creek/nexus_data_subset.geojson";
 
 //TODO this is possible, but ASSUMES realizations based on feature geom type, so not quite ready for prime time
 class RealizaitonVisitor : public geojson::FeatureVisitor {
 
 };
 
-int main(int argc, char *argv[]) {
-    std::cout << "Hello there " << ngen_VERSION_MAJOR << "."
-              << ngen_VERSION_MINOR << "."
-              << ngen_VERSION_PATCH << std::endl;
+void prepare_features(geojson::GeoJSON& nexus, geojson::GeoJSON& catchments, bool validate=false)
+{
+  for(auto& feature: *nexus){
+    feature->set_id(feature->get_property("ID").as_string());
+    //std::cout << "Got Nexus Feature " << feature->get_id() << std::endl;
+  }
+  nexus->update_ids();
+  //Now read the collection of catchments, iterate it and add them to the nexus collection
+  //also link them by to->id
+  //std::cout << "Iterating Catchment Features" << std::endl;
+  for(auto& feature: *catchments){
+    feature->set_id(feature->get_property("ID").as_string());
+    nexus->add_feature(feature);
+    //std::cout<<"Catchment "<<feature->get_id()<<" -> Nexus "<<feature->get_property("toID").as_string()<<std::endl;
+  }
+  std::string linkage = "toID";
+  nexus->link_features_from_property(nullptr, &linkage);
 
-    //Read the collection of nexus
-    std::cout << "Building Nexus collection" << std::endl;
-    geojson::GeoJSON nexus_collection = geojson::read(nexusRealizationFile);
-    std::cout << "Iterating Nexus Features" << std::endl;
-
-    for(auto& feature: *nexus_collection){
-      feature->set_id(feature->get_property("ID").as_string());
-      //std::cout << "Got Nexus Feature " << feature->get_id() << std::endl;
-    }
-    nexus_collection->update_ids();
-    //Now read the collection of catchments, iterate it and add them to the nexus collection
-    //also link them by to->id
-    std::cout << "Building Catchment collection" << std::endl;
-    geojson::GeoJSON catchment_collection = geojson::read(catchmentRealizationFile);
-    std::cout << "Iterating Catchment Features" << std::endl;
-    for(auto& feature: *catchment_collection){
-      feature->set_id(feature->get_property("ID").as_string());
-      nexus_collection->add_feature(feature);
-      std::cout<<"Catchment "<<feature->get_id()<<" -> Nexus "<<feature->get_property("toID").as_string()<<std::endl;
-    }
-    std::string linkage = "toID";
-    nexus_collection->link_features_from_property(nullptr, &linkage);
-
-    for(auto& feature: *nexus_collection){
+  if(validate){
+    //Convience check on linkage
+    for(auto& feature: *nexus){
       //feature->set_id(feature->get_property("ID").as_string());
       //std::cout << "Got Nexus Feature " << feature->get_id() << std::endl;
       if( feature->get_id().substr(0, 3) == "cat") {
@@ -55,8 +47,26 @@ int main(int argc, char *argv[]) {
         else {
           std::cout <<"catchment feature " << feature->get_id() << " to nexus feature " << downstream[0]->get_id() << std::endl;
         }
-      }
-    }
+       }
+     }//end for(feature: nexus)
+   }//end if(validate)
+}
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello there " << ngen_VERSION_MAJOR << "."
+              << ngen_VERSION_MINOR << "."
+              << ngen_VERSION_PATCH << std::endl;
+
+    //Read the collection of nexus
+    std::cout << "Building Nexus collection" << std::endl;
+    geojson::GeoJSON nexus_collection = geojson::read(nexusRealizationFile);
+    std::cout << "Iterating Nexus Features" << std::endl;
+
+
+    std::cout << "Building Catchment collection" << std::endl;
+    geojson::GeoJSON catchment_collection = geojson::read(catchmentRealizationFile);
+
+    prepare_features(nexus_collection, catchment_collection, true);
     /*
         The basic driving algorithm looks something like this:
 
