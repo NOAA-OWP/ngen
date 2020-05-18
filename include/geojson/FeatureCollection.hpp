596febb0623570484817f13853e2116f16b35881
Chris Tubbs <christopher.tubbs@noaa.gov>
2020-05-18 13:20:09 -0500

    Added functions for finding features, removing features, setting and updating ids on features, and equivalence checks for features and properties

--- include/geojson/FeatureCollection.hpp
+++ include/geojson/FeatureCollection.hpp
@@ -18,21 +18,6 @@ namespace geojson {
     class FeatureCollection {
         public:
             /**
-             * Add a reference to a feature by id
-             * 
-             * @param id The id used to reference a feature
-             * @param feature A feature that may be referred to by id
-             */
-            void add_feature_id(std::string id, Feature feature) {
-                if (id != "") {
-                    feature_by_id.emplace(id, feature);
-                }
-                else {
-                    throw std::invalid_argument("id");
-                }
-            }
-
-            /**
              * Constructor
              * 
              * @param features An overall list of features
@@ -62,12 +47,64 @@ namespace geojson {
              */
             virtual ~FeatureCollection(){};
 
+            /**
+             * @return The number of elements within the collection
+             */
             int get_size();
+
+            /**
+             * @return Whether or not the collection is empty
+             */
+            bool is_empty();
 
             std::vector<double> get_bounding_box() const;
 
+            /**
+             * Retrieve Feature by index in collection
+             * 
+             * @param index The index of the feature
+             * @return The Feature at the given index
+             */
             Feature get_feature(int index) const;
 
+            /**
+             * Finds the index of given Feature in the collection
+             * 
+             * @param feature The feature to look for
+             * @return -1 if the feature isn't in the collection, the numerical index otherwise
+             */
+            int find(Feature feature);
+
+            /**
+             * Finds the index of a Feature with the given ID
+             * 
+             * @param ID The ID of the Feature to look for
+             * @return -1 if the feature isn't in the collection, the numerical index otherwise
+             */
+            int find(std::string ID);
+
+            /**
+             * Removes a feature from the collection based on index
+             * 
+             * @param index The index of the Feature to remove
+             * @return The Feature that was removed
+             */
+            Feature remove_feature(int index);
+
+            /**
+             * Removes a Feature based on its ID
+             * 
+             * @param ID The ID of the Feature to remove
+             * @return The removed Feature; null if a Feature wasn't found 
+             */
+            Feature remove_feature_by_id(std::string ID);
+
+            /**
+             * Retrieves a Feature based on its ID
+             * 
+             * @param ID The ID of the Feature to retrieve
+             * @return The feature with the given ID; null if a Feature with that ID isn't present
+             */
             Feature get_feature(std::string id) const;
 
             FeatureList::const_iterator begin() const;
@@ -93,6 +130,20 @@ namespace geojson {
             void set(std::string key, JSONProperty& property);
 
             void add_feature(Feature feature, std::string *id = nullptr);
+
+            /**
+             * Add a reference to a feature by id
+             * 
+             * @param id The id used to reference a feature
+             * @param feature A feature that may be referred to by id
+             */
+            void add_feature_id(std::string id, Feature feature);
+
+            void set_ids_from_member(std::string member_name = "id");
+
+            void set_ids_from_property(std::string property_name = "id");
+
+            void set_ids(std::string id_field_name = "id");
 
             void update_ids();
 
