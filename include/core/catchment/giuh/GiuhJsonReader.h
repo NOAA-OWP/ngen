c6ba536dee1935a6849bb27c5c35a30a181edd35
Robert Bartel <robert.bartel@noaa.gov>
2020-05-21 12:08:53 -0500

    Update GiuhJsonReader for COMIDs.
    Updating GiuhJsonReader class with logic to take and manage a mapping
    of catchment ids to COMIDs, and then have various functions properly
    handle the two (as the GIUH data JSON is actual based on COMIDs rather
    than catchment ids).

--- include/core/catchment/giuh/GiuhJsonReader.h
+++ include/core/catchment/giuh/GiuhJsonReader.h
@@ -16,20 +16,41 @@ namespace giuh {
     class GiuhJsonReader {
 
     public:
-        GiuhJsonReader(std::string data_file_path) : data_json_file_path(data_file_path) {
-            // Confirm file exists and is readable
-            if (FILE *file = fopen(data_file_path.c_str(), "r")) {
+        GiuhJsonReader(std::string data_file_path, std::string id_map_path) : data_json_file_path(data_file_path),
+                                                                              id_map_json_file_path(id_map_path) {
+            // Confirm data JSON file exists and is readable
+            if (FILE *file = fopen(data_json_file_path.c_str(), "r")) {
                 fclose(file);
                 data_json_file_readable = true;
                 ptree tree;
                 boost::property_tree::json_parser::read_json(data_json_file_path, tree);
                 data_json_tree = std::make_unique<ptree>(tree);
-            }
-            else {
+            } else {
                 data_json_file_readable = false;
                 data_json_tree = nullptr;
             }
+
+            // Do the same for the id mapping data
+            if (FILE *file = fopen(id_map_json_file_path.c_str(), "r")) {
+                fclose(file);
+                id_map_json_file_readable = true;
+                id_map = std::make_unique<std::map<std::string, std::string>>(std::map<std::string, std::string>());
+
+                ptree id_map_tree;
+                boost::property_tree::json_parser::read_json(id_map_json_file_path, id_map_tree);
+
+                for (ptree::iterator pos = id_map_tree.begin(); pos != id_map_tree.end(); ++pos) {
+                    std::string local_id = pos->second.get<std::string>("local_id");
+                    std::string comid = pos->second.get<std::string>("COMID");
+                    id_map->emplace(local_id, comid);
+                }
+            } else {
+                id_map_json_file_readable = false;
+                //id_map_json_tree = nullptr;
+            }
         }
+
+        std::string get_associated_comid(std::string catchment_id);
 
         std::shared_ptr<giuh_kernel> get_giuh_kernel_for_id(std::string catchment_id);
 
@@ -42,16 +63,39 @@ namespace giuh {
          */
         bool is_data_json_file_readable();
 
+        /**
+         * Get whether a file at the path represented by `id_map_json_file_path` exists and is readable.
+         *
+         * @return Whether a file at the path represented by `id_map_json_file_path` exists and is readable.
+         */
+        bool is_id_map_json_file_readable();
+
     private:
+        // TODO: document tree structure for both data and id mapping ptrees
+
         /** Whether a file at the path represented by `data_json_file_path` exists and is readable. */
         bool data_json_file_readable;
         /** The path to the JSON file containing GIUH data, as a string. */
         std::string data_json_file_path;
+        /** A ptree representation of the GIUH data from the `data_json_file_path` file. */
         std::unique_ptr<ptree> data_json_tree;
+        /** Whether a file at the path represented by `id_map_json_file_path` exists and is readable. */
+        bool id_map_json_file_readable;
+        /** The path to the JSON file containing map of catchment ids to COMIDs, as a string. */
+        std::string id_map_json_file_path;
+        /** A ptree representation of the id mapping data from the `id_map_json_file_path` file. */
+        //std::unique_ptr<ptree> id_map_json_tree;
 
-        std::unique_ptr<ptree> find_data_node_for_catchment_id(std::string catchment_id);
+        std::unique_ptr<std::map<std::string, std::string>> id_map;
 
-        std::shared_ptr<giuh_kernel> build_giuh_kernel(std::string catchment_id, ptree catchment_data_node);
+
+
+        std::unique_ptr<ptree> find_data_node_for_comid(std::string comid);
+
+        std::string get_mapped_comid(std::string catchment_id);
+
+        std::shared_ptr<giuh_kernel> build_giuh_kernel(std::string catchment_id, std::string comid,
+                ptree catchment_data_node);
 
     };
 
--- src/core/catchment/giuh/GiuhJsonReader.cpp
+++ src/core/catchment/giuh/GiuhJsonReader.cpp
@@ -2,7 +2,8 @@
 
 using namespace giuh;
 
-std::shared_ptr<giuh_kernel> GiuhJsonReader::build_giuh_kernel(std::string catchment_id, ptree catchment_data_node) {
+std::shared_ptr<giuh_kernel> GiuhJsonReader::build_giuh_kernel(std::string catchment_id, std::string comid,
+        ptree catchment_data_node) {
     // Get times and freqs and convert to vectors
     std::vector<double> cumulative_freqs, cdf_times;
 
@@ -16,39 +17,52 @@ std::shared_ptr<giuh_kernel> GiuhJsonReader::build_giuh_kernel(std::string catch
         cdf_times.push_back(times.second.get_value<double>());
     }
 
-    return std::make_shared<giuh_kernel>(giuh_kernel(catchment_id, cdf_times, cumulative_freqs));
+    return std::make_shared<giuh_kernel>(giuh_kernel(catchment_id, comid, cdf_times, cumulative_freqs));
 }
 
-std::unique_ptr<ptree> GiuhJsonReader::find_data_node_for_catchment_id(std::string catchment_id) {
+std::unique_ptr<ptree> GiuhJsonReader::find_data_node_for_comid(std::string comid) {
     // Traverse tree until finding node with correct id
     for (ptree::iterator pos = data_json_tree->begin(); pos != data_json_tree->end(); ++pos) {
         // TODO: confirm this logic is correct, with respect to iterator members and what they retrieve
         // When the right node is found, use it to build a referenced kernel, and return the reference
-        if (pos->first == catchment_id) {
+        auto first = pos->first;
+        if (pos->first == comid) {
             return std::make_unique<ptree>(pos->second);
         }
     }
     return nullptr;
 }
 
+std::string GiuhJsonReader::get_associated_comid(std::string catchment_id) {
+    return id_map != nullptr && id_map->count(catchment_id) == 1 ? id_map->at(catchment_id) : "";
+}
+
 std::shared_ptr<giuh_kernel> GiuhJsonReader::get_giuh_kernel_for_id(std::string catchment_id) {
     if (!is_data_json_file_readable()) {
         return nullptr;
     }
+    std::string comid = get_associated_comid(catchment_id);
+    if (comid == "") {
+        return nullptr;
+    }
     // Traverse tree until finding node with correct id
-    std::unique_ptr<ptree> data_node = find_data_node_for_catchment_id(catchment_id);
-    return data_node == nullptr ? nullptr : build_giuh_kernel(catchment_id, *data_node);
+    std::unique_ptr<ptree> data_node = find_data_node_for_comid(comid);
+    return data_node == nullptr ? nullptr : build_giuh_kernel(catchment_id, comid, *data_node);
 }
 
 bool GiuhJsonReader::is_giuh_kernel_for_id_exists(std::string catchment_id) {
-    if (!is_data_json_file_readable()) {
+    if (!is_data_json_file_readable() || !is_id_map_json_file_readable()) {
+        return false;
+    }
+    std::string comid = get_associated_comid(catchment_id);
+    if (comid == "") {
         return false;
     }
     // Traverse tree until finding node with correct id
     for (ptree::iterator pos = data_json_tree->begin(); pos != data_json_tree->end(); ++pos) {
         // TODO: confirm this logic is correct, with respect to iterator members and what they retrieve
         // When the right node is found, use it to build a referenced kernel, and return the reference
-        if (pos->first == catchment_id) {
+        if (pos->first == comid) {
             return true;
         }
     }
@@ -57,4 +71,8 @@ bool GiuhJsonReader::is_giuh_kernel_for_id_exists(std::string catchment_id) {
 
 bool GiuhJsonReader::is_data_json_file_readable() {
     return data_json_file_readable;
+}
+
+bool GiuhJsonReader::is_id_map_json_file_readable() {
+    return id_map_json_file_readable;
 }
