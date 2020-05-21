27572bad70b2b07bf16a3a96f73ceb09ecdf8aaa
Robert Bartel <robert.bartel@noaa.gov>
2020-05-21 12:11:33 -0500

    Update GIUH_Test for catchment id to COMID map.
    Update GIUH_Test to handle the new mapping in the giuh_kernel of
    catchment ids to COMIDs, and altering the test class's design somewhat
    to work within the currently available testing data.

--- test/core/catchment/giuh/GIUH_Test.cpp
+++ test/core/catchment/giuh/GIUH_Test.cpp
@@ -7,7 +7,9 @@ class GIUH_Test : public ::testing::Test {
 
 protected:
 
-    GIUH_Test() : abridged_reader(giuh::GiuhJsonReader("../test/data/giuh/GIUH_abridged.json")) {}
+    GIUH_Test() : abridged_json_file("../test/data/giuh/GIUH_abridged.json"),
+                  complete_json_file("../test/data/giuh/GIUH.json"),
+                  id_map_json_file("../data/sugar_creek/crosswalk_subset.json") {}
 
     ~GIUH_Test() override {
 
@@ -17,7 +19,9 @@ protected:
 
     void TearDown() override;
 
-    giuh::GiuhJsonReader abridged_reader;
+    std::string abridged_json_file;
+    std::string complete_json_file;
+    std::string id_map_json_file;
 
 };
 
@@ -33,9 +37,17 @@ void GIUH_Test::TearDown() {
 //! Test that giuh_kernel objects initialize properly.
 TEST_F(GIUH_Test, TestInit0)
 {
-    std::string id_0 = "9731300";
+    //std::string json_file = abridged_json_file;
+    std::string json_file = complete_json_file;
 
-    std::shared_ptr<giuh::giuh_kernel> kernel_obj = abridged_reader.get_giuh_kernel_for_id(id_0);
+    giuh::GiuhJsonReader reader = giuh::GiuhJsonReader(json_file, id_map_json_file);
+
+    std::string expected_comid_0 = "9731236";
+    std::string catcment_id_0 = "wat-88";
+    std::string comid_0 = reader.get_associated_comid(catcment_id_0);
+    ASSERT_EQ(comid_0, expected_comid_0);
+
+    std::shared_ptr<giuh::giuh_kernel> kernel_obj = reader.get_giuh_kernel_for_id(catcment_id_0);
 
     ASSERT_TRUE(kernel_obj != nullptr);
 }
@@ -43,9 +55,14 @@ TEST_F(GIUH_Test, TestInit0)
 //! Test that giuh_kernel objects output properly.
 TEST_F(GIUH_Test, TestOutput0)
 {
-    std::string id_0 = "9731300";
+    //std::string json_file = abridged_json_file;
+    std::string json_file = complete_json_file;
 
-    std::shared_ptr<giuh::giuh_kernel> kernel_obj = abridged_reader.get_giuh_kernel_for_id(id_0);
+    giuh::GiuhJsonReader reader = giuh::GiuhJsonReader(json_file, id_map_json_file);
+
+    std::string catchment_id_0 = "wat-88";
+
+    std::shared_ptr<giuh::giuh_kernel> kernel_obj = reader.get_giuh_kernel_for_id(catchment_id_0);
 
     std::vector<double> inputs = {10000, 10000, 100, 0, 100};
     std::vector<double> time_steps = {86400, 86400, 300, 300, 300};
