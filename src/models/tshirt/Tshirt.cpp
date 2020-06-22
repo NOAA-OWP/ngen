a1d3d63bb4c9467703bef8626fd4c48aba437c91
Robert Bartel <robert.bartel@noaa.gov>
2020-06-22 15:24:17 -0500

    Clean up and add comments in Tshirt.cpp.

--- src/models/tshirt/Tshirt.cpp
+++ src/models/tshirt/Tshirt.cpp
@@ -66,6 +66,8 @@ namespace tshirt {
      * @return The appropriate code value indicating whether mass was conserved in the current time step's calculations.
      */
     int tshirt_model::mass_check(double input_flux_meters, double timestep_seconds) {
+        // TODO: change this to have those be part of state somehow, either of object or struct, or just make private
+        // TODO: overload and parameterize the allowed error range that is acceptable
         // Initialize both mass values from current and next states storage
         double previous_mass_meters = previous_state->soil_storage_meters + previous_state->groundwater_storage_meters;
         double current_mass_meters = current_state->soil_storage_meters + current_state->groundwater_storage_meters;
@@ -86,6 +88,7 @@ namespace tshirt {
         current_mass_meters += fluxes->groundwater_flow_meters_per_second * timestep_seconds;
 
         double mass_diff_meters = previous_mass_meters - current_mass_meters;
+        // TODO: fix this to use absolute value
         return mass_diff_meters > 0.000001 ? tshirt::TSHIRT_MASS_BALANCE_ERROR : tshirt::TSHIRT_NO_ERROR;
     }
 
@@ -99,6 +102,7 @@ namespace tshirt {
      */
     int tshirt_model::run(double dt, double input_flux_meters, shared_ptr<pdm03_struct> et_params) {
         // Do resetting/housekeeping for new calculations
+        // TODO: move this to separate small function; e.g., reset_state_members_for_next_time_step_run
         previous_state = current_state;
         current_state = make_shared<tshirt_state>(tshirt_state(0.0, 0.0, vector<double>(model_params.nash_n)));
         fluxes = make_shared<tshirt_fluxes>(tshirt_fluxes(0.0, 0.0, 0.0, 0.0, 0.0));
@@ -106,6 +110,7 @@ namespace tshirt {
         double soil_column_moisture_deficit =
                 model_params.max_soil_storage_meters - previous_state->soil_storage_meters;
 
+        // Perform Schaake partitioning, passing some declared references to hold the calculated values.
         double surface_runoff, subsurface_infiltration_flux;
         Schaake_partitioning_scheme(dt, model_params.Cschaake, soil_column_moisture_deficit, input_flux_meters,
                                     &surface_runoff, &subsurface_infiltration_flux);
@@ -120,16 +125,14 @@ namespace tshirt {
         double Qperc = soil_reservoir.velocity_meters_per_second_for_outlet(perc_outlet_index);
 
         // TODO: make sure ET doesn't need to be taken out sooner
+        // Get new soil storage amount calculated by reservoir
         double new_soil_storage = soil_reservoir.get_storage_height_meters();
+        // Calculate and store ET
         fluxes->et_loss_meters = calc_evapotranspiration(new_soil_storage, et_params);
+        // Update the current soil storage, accounting for ET
         current_state->soil_storage_meters = new_soil_storage - fluxes->et_loss_meters;
 
-        // TODO: potentially be able to update the max flow (currently always just max_lateral_flow)
-        //for (unsigned long i = 0; i < get_soil_lf_nash_res()->size(); ++i) {
-        //    get_soil_lf_nash_res()[i]->update_activation(*get_soil_field_capacity_storage());
-        //}
-
-        // cycle through lateral flow Nash cascade of nonlinear reservoirs
+        // Cycle through lateral flow Nash cascade of nonlinear reservoirs
         // loop essentially copied from Hymod logic, but with different variable names
         for (unsigned long int i = 0; i < soil_lf_nash_res.size(); ++i) {
             // get response water velocity of nonlinear reservoir
@@ -138,10 +141,6 @@ namespace tshirt {
             Qlf += nash_subsurface_excess / dt;
             current_state->nash_cascade_storeage_meters[i] = soil_lf_nash_res[i]->get_storage_height_meters();
         }
-
-        // "raw" GW calculations
-        //state.groundwater_storage_meters += soil_percolation_flow_meters_per_second * dt;
-        //double groundwater_flow_meters_per_second = params.Cgw * ( exp(params.expon * state.groundwater_storage_meters / params.max_groundwater_storage_meters) - 1 );
 
         double excess_gw_water;
         fluxes->groundwater_flow_meters_per_second = groundwater_reservoir.response_meters_per_second(Qperc, dt,
@@ -153,9 +152,7 @@ namespace tshirt {
         fluxes->soil_lateral_flow_meters_per_second = Qlf;
         fluxes->soil_percolation_flow_meters_per_second = Qperc;
 
-        // Save "raw" runoff here and have realization class calculate GIUH surface runoff
-        //fluxes->surface_runoff_meters_per_second = giuh_obj->calc_giuh_output(dt, surface_runoff);
-
+        // Save "raw" runoff here and have realization class calculate GIUH surface runoff using that kernel
         // TODO: for now add this to runoff, but later adjust calculations to limit flow into reservoir to avoid excess
         fluxes->surface_runoff_meters_per_second = surface_runoff + (subsurface_excess / dt) + (excess_gw_water / dt);
         //fluxes->surface_runoff_meters_per_second = surface_runoff;
