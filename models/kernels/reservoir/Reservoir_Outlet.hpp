3261bc14334c0cc2abacbbe89fd4e4202a53d847
Robert Bartel <robert.bartel@noaa.gov>
2020-06-25 13:09:27 -0500

    Refining abstraction in Reservoir_Outlet.hpp.
    Refining abstraction of Reservoir_Outlet class by isolating logic to
    calculate discharge velocity in a more specific function, thus reducing
    duplicatation of more general code for things like activation, staying
    under max velocity, and updating object state.

--- models/kernels/reservoir/Reservoir_Outlet.hpp
+++ models/kernels/reservoir/Reservoir_Outlet.hpp
@@ -7,10 +7,15 @@
 
 /**
  * @brief Base Single Reservior Outlet Class
- * This class is for a single reservior outlet that holds the parameters a, b, and the activation threhold, which is
+ *
+ * This class is for a single reservior outlet that holds the parameters a, b, and the activation threshold, which is
  * the height in meters the bottom of the outlet is above the bottom of the reservoir. This class also contains a
  * function to return the velocity in meters per second of the discharge through the outlet. This function will only
  * run if the reservoir storage passed in is above the activation threshold.
+ *
+ * To modify the details of the calculations for discharge velocity, subclasses should typically override the
+ * calc_velocity_meters_per_second_local function, rather than the velocity_meters_per_second function, which works at a
+ * higher-level.
  */
 class Reservoir_Outlet
 {
@@ -40,30 +45,33 @@ public:
     }
 
     /**
-     * @brief Function to return the velocity in meters per second of the discharge through the outlet
+     * @brief Function to update and return the velocity in meters per second of the discharge through the outlet.
+     *
+     * Function determines, sets, and returns the value for discharge velocity of the object.  The value itself is
+     * maintained within the velocity_meters_per_second_local member.
+     *
+     * Function performs sanity checking regarding whether storage is above the activation threshold before calculating
+     * the updated velocity value with a call to calc_velocity_meters_per_second_local.  Additionally, it checks the
+     * value returned by calc_velocity_meters_per_second_local, ensuring that velocity_meters_per_second_local does not
+     * exceed the max allowed discharged velocity for the outlet set in max_velocity_meters_per_second member.
+     *
      * @param parameters_struct reservoir parameters struct
      * @param storage_struct reservoir state storage struct
      * @param activation_threshold_meters meters from the bottom of the reservoir to the bottom of the outlet
      * @param max_velocity_meters_per_second max outlet velocity in meters per second
      * @return velocity_meters_per_second_local the velocity in meters per second of the discharge through the outlet
+     * @see calc_velocity_meters_per_second_local
      */
-    virtual double velocity_meters_per_second(reservoir_parameters &parameters_struct, reservoir_state &storage_struct)
+    double velocity_meters_per_second(reservoir_parameters &parameters_struct, reservoir_state &storage_struct)
     {
-
         // Return velocity of 0.0 if the storage passed in is less than the activation threshold
         if (storage_struct.current_storage_height_meters <= activation_threshold_meters) {
             velocity_meters_per_second_local = 0.0;
-            return 0.0;
+            return velocity_meters_per_second_local;
         }
 
         // Calculate the velocity in meters per second of the discharge through the outlet
-        velocity_meters_per_second_local =
-                a * std::pow(
-                        (
-                                (storage_struct.current_storage_height_meters - activation_threshold_meters)
-                                /
-                                (parameters_struct.maximum_storage_meters - activation_threshold_meters)
-                        ), b);
+        velocity_meters_per_second_local = calc_velocity_meters_per_second_local(parameters_struct, storage_struct);
 
         // If calculated outlet velocity is greater than max velocity, then set to max velocity and return a warning.
         if (velocity_meters_per_second_local > max_velocity_meters_per_second) {
@@ -109,6 +117,36 @@ protected:
     double activation_threshold_meters;
     double max_velocity_meters_per_second;
     double velocity_meters_per_second_local;
+
+    /**
+     * @brief Calculate outlet discharge velocity in meters per second.
+     *
+     * Perform appropriate calculations to return the velocity, in meters per second, of the discharge through this
+     * outlet.
+     *
+     * Typically this is only used by the velocity_meters_per_second function.
+     *
+     * This function should be overridden to adjust the behavior of subtypes with respect to how the discharge velocity
+     * is calculated, assuming the reservoir storage is above the activation threshold.  However, it is left to the
+     * velocity_meters_per_second function to actually update the object's state.
+     *
+     * @param parameters_struct reservoir parameters struct
+     * @param storage_struct reservoir state storage struct
+     * @param activation_threshold_meters meters from the bottom of the reservoir to the bottom of the outlet
+     * @param max_velocity_meters_per_second max outlet velocity in meters per second
+     * @return velocity_meters_per_second_local the velocity in meters per second of the discharge through the outlet
+     */
+    virtual double calc_velocity_meters_per_second_local(reservoir_parameters &parameters_struct,
+                                                         reservoir_state &storage_struct)
+    {
+         return a * std::pow(
+                                (
+                                        (storage_struct.current_storage_height_meters - activation_threshold_meters)
+                                        /
+                                        (parameters_struct.maximum_storage_meters - activation_threshold_meters)
+                                ), b);
+    }
+
 };
 
 #endif //NGEN_RESERVOIR_OUTLET_H
