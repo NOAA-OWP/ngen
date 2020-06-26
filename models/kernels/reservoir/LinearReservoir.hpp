24064f0ecbf91e6a618f26e5d89aeba65c476bab
Robert Bartel <robert.bartel@noaa.gov>
2020-06-26 09:51:44 -0500

    Create LinearReservoir.cpp.
    Create LinearReservoir.cpp and moving definitions there from
    LinearReservoir.hpp.

--- models/kernels/reservoir/LinearReservoir.hpp
+++ models/kernels/reservoir/LinearReservoir.hpp
@@ -4,31 +4,16 @@
 class LinearReservoir
 {
     public:
-        LinearReservoir(double init_storage = 1.0,double res_max_storage = 2.0, double res_constant = 0.1, double internal_time_step = 3600.0 ) :
-            storage(init_storage), max_storage(res_max_storage), linear_reservoir_constant(res_constant),  internal_dt(internal_time_step) {}
+        LinearReservoir(double init_storage = 1.0, double res_max_storage = 2.0, double res_constant = 0.1,
+                double internal_time_step = 3600.0 );
 
-        void update_storage(double input_storage)
-        {
-            this->storage += input_storage;
-        }
+        void update_storage(double input_storage);
 
-        double flux(double dt_secs)
-        {
-            return (this->storage / this->max_storage) * this->linear_reservoir_constant * (dt_secs/ internal_dt);
-        }
+        double flux(double dt_secs);
 
-        double response(double input_storage, double dt_secs)
-        {
-            this->storage += input_storage;
-            double output_flux = this->flux(dt_secs);
-            this->storage -= output_flux;
-            return output_flux;
-        }
+        double response(double input_storage, double dt_secs);
 
-        double get_storage()
-        {
-            return this->storage;
-        }
+        double get_storage();
 
     private:
         double linear_reservoir_constant;
