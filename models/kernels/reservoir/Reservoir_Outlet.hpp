ccf297fcc9413afdcaeafb2677f508ce1cd1b587
Robert Bartel <robert.bartel@noaa.gov>
2020-06-26 10:21:38 -0500

    Create Reservoir_Outlet.cpp.
    Creating Reservoir_Outlet.cpp and moving definitions there from
    Reservoir_Outlet.hpp.

--- models/kernels/reservoir/Reservoir_Outlet.hpp
+++ models/kernels/reservoir/Reservoir_Outlet.hpp
@@ -24,11 +24,7 @@ public:
     /**
      * @brief Default Constructor building an empty Reservoir Outlet Object
      */
-    Reservoir_Outlet(): a(0.0), b(0.0), activation_threshold_meters(0.0), max_velocity_meters_per_second(0.0),
-                        velocity_meters_per_second_local(0.0)
-    {
-
-    }
+    Reservoir_Outlet();
 
     /**
      * @brief Parameterized Constuctor that builds a Reservoir Outlet object
@@ -37,12 +33,26 @@ public:
      * @param activation_threshold_meters meters from the bottom of the reservoir to the bottom of the outlet
      * @param max_velocity_meters_per_second max outlet velocity in meters per second
      */
-    Reservoir_Outlet(double a, double b, double activation_threshold_meters, double max_velocity_meters_per_second):
-            a(a), b(b), activation_threshold_meters(activation_threshold_meters),
-            max_velocity_meters_per_second(max_velocity_meters_per_second), velocity_meters_per_second_local(0.0)
-    {
+    Reservoir_Outlet(double a, double b, double activation_threshold_meters, double max_velocity_meters_per_second);
 
-    }
+    /**
+     * Set the locally maintained velocity state variable value.
+     *
+     * @param velocity_meters_per_second The velocity value to set, in meters per second.
+     */
+    void adjust_velocity(double velocity_meters_per_second);
+
+    /**
+     * @brief Accessor to return activation_threshold_meters
+     * @return activation_threshold_meters meters from the bottom of the reservoir to the bottom of the outlet
+     */
+    double get_activation_threshold_meters() const;
+
+    /**
+     * @brief Accessor to return velocity_meters_per_second_local that is previously calculated
+     * @return velocity_meters_per_second_local
+     */
+    double get_previously_calculated_velocity_meters_per_second();
 
     /**
      * @brief Function to update and return the velocity in meters per second of the discharge through the outlet.
@@ -62,54 +72,7 @@ public:
      * @return velocity_meters_per_second_local the velocity in meters per second of the discharge through the outlet
      * @see calc_velocity_meters_per_second_local
      */
-    double velocity_meters_per_second(reservoir_parameters &parameters_struct, reservoir_state &storage_struct)
-    {
-        // Return velocity of 0.0 if the storage passed in is less than the activation threshold
-        if (storage_struct.current_storage_height_meters <= activation_threshold_meters) {
-            velocity_meters_per_second_local = 0.0;
-            return velocity_meters_per_second_local;
-        }
-
-        // Calculate the velocity in meters per second of the discharge through the outlet
-        velocity_meters_per_second_local = calc_velocity_meters_per_second_local(parameters_struct, storage_struct);
-
-        // If calculated outlet velocity is greater than max velocity, then set to max velocity and return a warning.
-        if (velocity_meters_per_second_local > max_velocity_meters_per_second) {
-            velocity_meters_per_second_local = max_velocity_meters_per_second;
-
-            //TODO: Return appropriate warning
-            std::__1::cout
-                    << "WARNING: Nonlinear reservoir calculated an outlet velocity over max velocity, and therefore "
-                    << "set the outlet velocity to max velocity."
-                    << std::__1::endl;
-        }
-
-        //Return the velocity in meters per second of the discharge through the outlet
-        return velocity_meters_per_second_local;
-    };
-
-    /**
-     * @brief Accessor to return activation_threshold_meters
-     * @return activation_threshold_meters meters from the bottom of the reservoir to the bottom of the outlet
-     */
-    inline double get_activation_threshold_meters() const
-    {
-        return activation_threshold_meters;
-    };
-
-    /**
-     * @brief Accessor to return velocity_meters_per_second_local that is previously calculated
-     * @return velocity_meters_per_second_local
-     */
-    double get_previously_calculated_velocity_meters_per_second()
-    {
-        return velocity_meters_per_second_local;
-    };
-
-    void adjust_velocity(double velocity_meters_per_second)
-    {
-        velocity_meters_per_second_local = velocity_meters_per_second;
-    }
+    double velocity_meters_per_second(reservoir_parameters &parameters_struct, reservoir_state &storage_struct);
 
 protected:
     double a;
@@ -137,15 +100,7 @@ protected:
      * @return velocity_meters_per_second_local the velocity in meters per second of the discharge through the outlet
      */
     virtual double calc_velocity_meters_per_second_local(reservoir_parameters &parameters_struct,
-                                                         reservoir_state &storage_struct)
-    {
-         return a * std::pow(
-                                (
-                                        (storage_struct.current_storage_height_meters - activation_threshold_meters)
-                                        /
-                                        (parameters_struct.maximum_storage_meters - activation_threshold_meters)
-                                ), b);
-    }
+                                                         reservoir_state &storage_struct);
 
 };
 
