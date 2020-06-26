04871bcae8d1f74ba0abb12c7aa01e388229b0d2
Robert Bartel <robert.bartel@noaa.gov>
2020-06-26 10:26:01 -0500

    Create Reservoir_Exponential_Outlet.hpp.
    Creating Reservoir_Exponential_Outlet.cpp and moving definitions there
    from Reservoir_Exponential_Outlet.hpp.

---
+++ src/models/kernels/reservoir/Reservoir_Exponential_Outlet.cpp
@@ -0,0 +1,51 @@
+#include "Reservoir_Exponential_Outlet.hpp"
+
+/**
+ * @brief Default Constructor building an empty Reservoir Exponential Outlet Object.
+ */
+Reservoir_Exponential_Outlet::Reservoir_Exponential_Outlet() : Reservoir_Outlet(), c(0.0), expon(0.0)
+{
+
+}
+
+/**
+ * @brief Parameterized Constructor that builds a Reservoir Outlet object.
+ *
+ * @param c outlet velocity calculation coefficient
+ * @param expon outlet velocity calculation exponential coefficient
+ * @param activation_threshold_meters meters from the bottom of the reservoir to the bottom of the outlet
+ * @param max_velocity_meters_per_second max outlet velocity in meters per second
+ */
+Reservoir_Exponential_Outlet::Reservoir_Exponential_Outlet(double c, double expon, double activation_threshold_meters,
+                             double max_velocity_meters_per_second) :
+        Reservoir_Outlet(-999.0, -999.0, activation_threshold_meters, max_velocity_meters_per_second),
+        c(c),
+        expon(expon)
+{
+
+}
+
+/**
+ * @brief Calculate outlet discharge velocity in meters per second.
+ *
+ * Perform appropriate calculations to return the velocity, in meters per second, of the discharge through this
+ * outlet.
+ *
+ * Typically this is only used by the velocity_meters_per_second function.
+ *
+ * This function should be overridden to adjust the behavior of subtypes with respect to how the discharge velocity
+ * is calculated, assuming the reservoir storage is above the activation threshold.  However, it is left to the
+ * velocity_meters_per_second function to actually update the object's state.
+ *
+ * @param parameters_struct reservoir parameters struct
+ * @param storage_struct reservoir state storage struct
+ * @param activation_threshold_meters meters from the bottom of the reservoir to the bottom of the outlet
+ * @param max_velocity_meters_per_second max outlet velocity in meters per second
+ * @return velocity_meters_per_second_local the velocity in meters per second of the discharge through the outlet
+ */
+double Reservoir_Exponential_Outlet::calc_velocity_meters_per_second_local(reservoir_parameters &parameters_struct,
+                                                                           reservoir_state &storage_struct)
+{
+    return c * (exp(expon * storage_struct.current_storage_height_meters /
+                    parameters_struct.maximum_storage_meters) - 1);
+}
