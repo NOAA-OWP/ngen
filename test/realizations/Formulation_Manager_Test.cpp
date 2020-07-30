4af4fca89773dffd2e2b6fb321a9646565172f81
Chris Tubbs <christopher.tubbs@noaa.gov>
2020-07-30 15:00:22 -0500

    Fixed some issues with the realization manager where simple lumped would have incorrectly initialized doubles and fixed an issue where the realization manager test wasn't being read.

--- test/realizations/Formulation_Manager_Test.cpp
+++ test/realizations/Formulation_Manager_Test.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <memory>
 
+#include <iostream>
+
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
@@ -93,7 +95,11 @@ const std::string EXAMPLE = "{ "
                 "\"t\": 0 "
             "}, "
             "\"forcing\": { "
+<<<<<<< HEAD:test/realizations/Formulation_Manager_Test.cpp
                 "\"path\": \"./data/forcing/cat-88_2015-12-01 00_00_00_2015-12-30 23_00_00.csv\", "
+=======
+                "\"path\": \"./data/sugar_creek/forcing/cat-88_2015-12-01 00_00_00_2015-12-30 23_00_00.csv\", "
+>>>>>>> Fixed some issues with the realization manager where simple lumped would have incorrectly initialized doubles and fixed an issue where the realization manager test wasn't being read.:test/realizations/Realization_Manager_Test.cpp
                 "\"start_time\": \"2015-12-01 00:00:00\", "
                 "\"end_time\": \"2015-12-30 23:00:00\" "
             "} "
@@ -129,11 +135,19 @@ const std::string EXAMPLE = "{ "
                 "\"timestep\": 3600, "
                 "\"giuh\": { "
                     "\"giuh_path\": \"./test/data/giuh/GIUH.json\", "
+<<<<<<< HEAD:test/realizations/Formulation_Manager_Test.cpp
                     "\"crosswalk_path\": \"./data/crosswalk.json\" "
                 "} "
             "}, "
             "\"forcing\": { "
                 "\"path\": \"./data/forcing/cat-89_2015-12-01 00_00_00_2015-12-30 23_00_00.csv\", "
+=======
+                    "\"crosswalk_path\": \"./data/sugar_creek/crosswalk_subset.json\" "
+                "} "
+            "}, "
+            "\"forcing\": { "
+                "\"path\": \"./data/sugar_creek/forcing/cat-89_2015-12-01 00_00_00_2015-12-30 23_00_00.csv\", "
+>>>>>>> Fixed some issues with the realization manager where simple lumped would have incorrectly initialized doubles and fixed an issue where the realization manager test wasn't being read.:test/realizations/Realization_Manager_Test.cpp
                 "\"start_time\": \"2015-12-01 00:00:00\", "
                 "\"end_time\": \"2015-12-30 23:00:00\" "
             "} "
@@ -168,8 +182,13 @@ TEST_F(Formulation_Manager_Test, basic_run) {
     std::shared_ptr<std::ostream> s_ptr(raw_pointer, [](void*) {});
     utils::StreamHandler catchment_output(s_ptr);
 
+<<<<<<< HEAD:test/realizations/Formulation_Manager_Test.cpp
     realization::Formulation_Manager manager = realization::Formulation_Manager(stream);
     manager.read(catchment_output);
+=======
+    realization::Realization_Manager manager = realization::Realization_Manager(stream);
+    manager.read();
+>>>>>>> Fixed some issues with the realization manager where simple lumped would have incorrectly initialized doubles and fixed an issue where the realization manager test wasn't being read.:test/realizations/Realization_Manager_Test.cpp
 
     std::map<std::string, std::map<long, double>> calculated_results;
 
@@ -182,7 +201,11 @@ TEST_F(Formulation_Manager_Test, basic_run) {
 
     double dt = 3600.0;
 
+<<<<<<< HEAD:test/realizations/Formulation_Manager_Test.cpp
     for (std::pair<std::string, std::shared_ptr<realization::Formulation>> realization : manager) {
+=======
+    for (std::pair<std::string, std::shared_ptr<realization::Realization>> realization : manager) {
+>>>>>>> Fixed some issues with the realization manager where simple lumped would have incorrectly initialized doubles and fixed an issue where the realization manager test wasn't being read.:test/realizations/Realization_Manager_Test.cpp
         if (calculated_results.count(realization.first) == 0) {
             calculated_results.emplace(realization.first, std::map<long, double>());
         }
