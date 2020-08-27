d1eddfeb37a8912298628992294a4a2fcf03e3c3
Nels Frazier <nels.frazier@noaa.gov>
2020-08-27 16:38:04 -0500

    Test reading and building a collection subset by id

--- test/geojson/FeatureCollection_Test.cpp
+++ test/geojson/FeatureCollection_Test.cpp
@@ -56,7 +56,10 @@ class Visitor : public geojson::FeatureVisitor {
         }
 
         std::string get(int index) {
-            return this->types[index];
+            if( index >= 0 && index < types.size() )
+              return this->types[index];
+            else
+              return "NULL";
         }
 
     private:
@@ -128,4 +131,72 @@ TEST_F(FeatureCollection_Test, ptree_test) {
 
     ASSERT_EQ(visitor.get(0), "PointFeature");
     ASSERT_EQ(visitor.get(1), "LineStringFeature");
+}
+
+TEST_F(FeatureCollection_Test, subset_test) {
+    std::string data = "{ "
+        "\"type\": \"FeatureCollection\", "
+        "\"bbox\": [1, 2, 3, 4, 5 ], "
+        "\"features\": [ "
+            "{ "
+                "\"type\": \"Feature\", "
+                "\"id\": \"First\", "
+                "\"geometry\": { "
+                "    \"type\": \"Point\", "
+                "    \"coordinates\": [102.0, 0.5] "
+                "} "
+            "}, "
+            "{ "
+                "\"type\": \"Feature\", "
+                "\"id\": \"Second\", "
+                "\"geometry\": { "
+                    "\"type\": \"LineString\", "
+                    "\"coordinates\": [ "
+                        "[102.0, 0.0], "
+                        "[103.0, 1.0], "
+                        "[104.0, 0.0], "
+                        "[105.0, 1.0] "
+                    "] "
+                "} "
+            "} "
+        "] "
+        "}";
+
+    std::stringstream stream;
+    stream << data;
+    std::vector<std::string> subset = {"First"};
+
+    geojson::GeoJSON collection = geojson::read(stream, subset);
+
+    std::vector<double> bbox = collection->get_bounding_box();
+    ASSERT_EQ(bbox.size(), 5);
+    ASSERT_EQ(bbox[0], 1.0);
+    ASSERT_EQ(bbox[1], 2.0);
+    ASSERT_EQ(bbox[2], 3.0);
+    ASSERT_EQ(bbox[3], 4.0);
+    ASSERT_EQ(bbox[4], 5.0);
+    //Should only have the first feature in the collection
+    ASSERT_EQ(1, collection->get_size());
+
+    geojson::Feature first = collection->get_feature(0);
+    geojson::Feature second = collection->get_feature(1);
+
+    ASSERT_EQ(first->get_id(), "First");
+
+    ASSERT_TRUE(second == nullptr);
+    ASSERT_EQ(first->get_type(), geojson::FeatureType::Point);
+
+    ASSERT_TRUE(first->is_leaf());
+    ASSERT_TRUE(first->is_root());
+
+    ASSERT_EQ(collection->get_feature("First"), first);
+    ASSERT_EQ(collection->get_feature("Second"), nullptr);
+
+    Visitor visitor;
+
+    collection->visit_features(visitor);
+
+    ASSERT_EQ(visitor.get(0), "PointFeature");
+    ASSERT_EQ(visitor.get(1), "NULL");
+    std::cout<<"HERE\n";
 }
