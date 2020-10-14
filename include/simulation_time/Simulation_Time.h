3b6f0eb96038638f24de4c2e4fe826ecc58e3cea
John David Mattern <jdmattern@localhost.localdomain>
2020-10-14 15:57:30 -0400

    initial addition of simulation time to formulation manager

---
+++ include/simulation_time/Simulation_Time.h
@@ -0,0 +1,26 @@
+#ifndef SIMULATION_TIME_H
+#define SIMULATION_TIME_H
+
+
+//#include <ctime>
+//#include <time.h>
+
+using namespace std;
+
+/**
+ * @brief simulation_time_params providing configuration information for simulation time period.
+ */
+struct simulation_time_params
+{
+  std::string start_time;
+  std::string end_time;
+  std::string date_format =  "%Y-%m-%d %H:%M:%S";
+  //time_t start_t;
+  //time_t end_t;
+
+
+
+
+
+#endif // SIMULATION_TIME_H
+
