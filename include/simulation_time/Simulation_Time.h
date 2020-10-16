7328e52ba75fca0e38bee1acca5cb293deacff81
John David Mattern <jdmattern@localhost.localdomain>
2020-10-16 16:04:50 -0400

    added unit tests for simulation time class

--- include/simulation_time/Simulation_Time.h
+++ include/simulation_time/Simulation_Time.h
@@ -17,8 +17,12 @@ struct simulation_time_params
   time_t start_t;
   time_t end_t;
   int output_time_step;
+
   /*
-    Constructor for simulation_time_params
+   * @brief Constructor for simulation_time_params
+   * @param start_time
+   * @param end_time
+   * @param output_time_step
   */
   simulation_time_params(std::string start_time, std::string end_time, int output_time_step):
     start_time(start_time), end_time(end_time), output_time_step(output_time_step)
@@ -38,37 +42,45 @@ struct simulation_time_params
 
 
 /**
- * @brief Simulation Time class providing time-series arrays to the model.
+ * @brief Simulation Time class providing time-series variables and methods to the model.
  */
 class Simulation_Time
 {
     public:
     /**
-     * Constructor building a Simulation Time object
+     * @brief Constructor building a Simulation Time object
+     * @param start_date_time_epoch
+     * @param end_date_time_epoch
+     * @param current_date_time_epoch
+     * @param output_time_step_seconds
      */
     Simulation_Time(simulation_time_params simulation_time_config):start_date_time_epoch(simulation_time_config.start_t),
                                            end_date_time_epoch(simulation_time_config.end_t),
                                            current_date_time_epoch(simulation_time_config.start_t),
-                                           output_time_step_seconds(simulation_time_config.output_time_step),
-                                           simulation_time_vector_index(-1)
+                                           output_time_step_seconds(simulation_time_config.output_time_step)
     {
 
         simulation_total_time_seconds = end_date_time_epoch - start_date_time_epoch;
 
         /**
-         * Calculate total time_steps. Adding 1 to account for the first time time_step.
+         * @brief Calculate total time_steps. Adding 1 to account for the first time time_step.
          */
         total_time_steps = simulation_total_time_seconds / output_time_step_seconds + 1;
-
-
     }
 
-
+    /**
+     * @brief Accessor to the total number of time steps
+     * @return total_time_steps
+     */
     int get_total_time_steps()
     {
         return total_time_steps;
     }
 
+    /**
+     * @brief Accessor to the current timestamp string
+     * @return current_timestamp
+     */ 
     std::string get_timestamp(int current_time_step)
     {
         current_date_time_epoch = start_date_time_epoch + current_time_step * output_time_step_seconds;
@@ -78,7 +90,7 @@ class Simulation_Time
         temp_gmtime_struct = gmtime(&current_date_time_epoch);
 
         char current_timestamp[20];
-        const char* time_format = "%y-%m-%d %T";
+        const char* time_format = "%Y-%m-%d %T";
 
         if (strftime(current_timestamp, sizeof(current_timestamp), time_format, temp_gmtime_struct) == 0) { 
             fprintf(stderr, "ERROR: strftime returned 0");
@@ -88,28 +100,16 @@ class Simulation_Time
         return current_timestamp;
     }
 
-
     private:
-
-    vector<time_t> simulation_time_epoch_vector; 
-    vector<string> simulation_time_string_vector;
-    
-    int simulation_time_vector_index;
 
     int total_time_steps;
     int simulation_total_time_seconds;
-    
-
     int output_time_step_seconds;
-
 
     time_t start_date_time_epoch;
     time_t end_date_time_epoch;
     time_t current_date_time_epoch;
-
-
 };
 
 
 #endif // SIMULATION_TIME_H
-
