5360eec6134337076bb96133331aba78c05798aa
Robert Bartel <robert.bartel@noaa.gov>
2020-10-16 14:10:55 -0500

    Go back to default param for delimiter.
    Going back to use of default parameter value for delimiter in several
    output-related functions.

--- include/realizations/catchment/Formulation.hpp
+++ include/realizations/catchment/Formulation.hpp
@@ -53,49 +53,13 @@ namespace realization {
 
             /**
              * Get a header line appropriate for a file made up of entries from this type's implementation of
-             * ``get_output_line_for_timestep``, using the default delimiter.
-             *
-             * Note that like the output generating function, this line does not include anything for time step.
-             *
-             * The default implementation uses a delimiter of "," and returns the result of the overloaded function
-             * accepting a single string argument.
-             *
-             * @return An appropriate header line for this type.
-             */
-            virtual std::string get_output_header_line() {
-                return get_output_header_line(DEFAULT_FORMULATION_OUTPUT_DELIMITER);
-            }
-
-            /**
-             * Get a header line appropriate for a file made up of entries from this type's implementation of
              * ``get_output_line_for_timestep``.
              *
              * Note that like the output generating function, this line does not include anything for time step.
              *
              * @return An appropriate header line for this type.
              */
-            virtual std::string get_output_header_line(std::string delimiter) =0;
-
-            /**
-             * Get a formatted line of output values for the given time step as a delimited string, using the default
-             * delimiter of ",".
-             *
-             * This method is useful for preparing calculated data in a representation useful for output files, such as
-             * CSV files.
-             *
-             * The resulting string will contain calculated values for applicable output variables for the particular
-             * formulation, as determined for the given time step.  However, the string will not contain any
-             * representation of the time step itself.
-             *
-             * An empty string is returned if the time step value is not in the range of valid time steps for which there
-             * are calculated values for all variables.
-             *
-             * @param timestep The time step for which data is desired.
-             * @return A delimited string with all the output variable values for the given time step.
-             */
-            virtual std::string get_output_line_for_timestep(int timestep) {
-                return get_output_line_for_timestep(timestep, DEFAULT_FORMULATION_OUTPUT_DELIMITER);
-            }
+            virtual std::string get_output_header_line(std::string delimiter=",") =0;
 
             /**
              * Get a formatted line of output values for the given time step as a delimited string.
@@ -114,7 +78,7 @@ namespace realization {
              * @param delimiter The value delimiter for the string.
              * @return A delimited string with all the output variable values for the given time step.
              */
-            virtual std::string get_output_line_for_timestep(int timestep, std::string delimiter) = 0;
+            virtual std::string get_output_line_for_timestep(int timestep, std::string delimiter=",") = 0;
             
             virtual void create_formulation(boost::property_tree::ptree &config, geojson::PropertyMap *global = nullptr) = 0;
             virtual void create_formulation(geojson::PropertyMap properties) = 0;
