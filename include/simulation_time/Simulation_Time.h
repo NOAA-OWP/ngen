b486a1f60a0c36d0b6562c476654d2c9ab4103f0
John David Mattern <jdmattern@localhost.localdomain>
2020-10-16 00:30:04 -0400

    added Simulation_Time class variables

--- include/simulation_time/Simulation_Time.h
+++ include/simulation_time/Simulation_Time.h
@@ -16,11 +16,12 @@ struct simulation_time_params
   std::string date_format =  "%Y-%m-%d %H:%M:%S";
   time_t start_t;
   time_t end_t;
+  int output_time_step;
   /*
     Constructor for simulation_time_params
   */
-  simulation_time_params(std::string start_time, std::string end_time):
-    start_time(start_time), end_time(end_time)
+  simulation_time_params(std::string start_time, std::string end_time, int output_time_step):
+    start_time(start_time), end_time(end_time), output_time_step(output_time_step)
     {
       /// \todo converting to UTC can be tricky, especially if thread safety is a concern
       /* https://stackoverflow.com/questions/530519/stdmktime-and-timezone-info */
@@ -34,6 +35,63 @@ struct simulation_time_params
       this->end_t = timegm( &tm );
     }
 };
+
+
+/**
+ * @brief Simulation Time class providing time-series arrays to the model.
+ */
+class Simulation_Time
+{
+    public:
+int total_time_steps;
+    /**
+     * Constructor building a Simulation Time object
+     */
+    Simulation_Time(simulation_time_params simulation_time_config):start_date_time_epoch(simulation_time_config.start_t),
+                                           end_date_time_epoch(simulation_time_config.end_t),
+                                           current_date_time_epoch(simulation_time_config.start_t),
+                                           output_time_step_seconds(simulation_time_config.output_time_step),
+                                           simulation_time_vector_index(-1)
+    {
+        //read_forcing_aorc(forcing_config.path);
+
+        simulation_total_time_seconds = end_date_time_epoch - start_date_time_epoch;
+
+        total_time_steps = simulation_total_time_seconds / output_time_step_seconds;
+
+
+    }
+
+/*
+    int get_total_time_steps()
+    {
+        return total_time_steps;
+    }
+*/
+    //int total_time_steps;
+
+
+    private:
+
+    vector<time_t> simulation_time_epoch_vector; 
+    vector<string> simulation_time_string_vector;
+    
+    int simulation_time_vector_index;
+
+    //int total_time_steps;
+    int simulation_total_time_seconds;
+    
+   
+    int output_time_step_seconds; //INCLUDE THIS????
+
+
+    time_t start_date_time_epoch;
+    time_t end_date_time_epoch;
+    time_t current_date_time_epoch;
+
+
+};
+
 
 #endif // SIMULATION_TIME_H
 
