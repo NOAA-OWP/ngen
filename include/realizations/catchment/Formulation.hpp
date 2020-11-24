b8b1e4950cd1abedbb839e88887840dd62bd9e2c
Robert Bartel <robert.bartel@noaa.gov>
2020-11-24 15:18:27 -0600

    Make sure to clean up in BMI C test Actions.

--- include/realizations/catchment/Formulation.hpp
+++ include/realizations/catchment/Formulation.hpp
@@ -28,6 +28,19 @@ namespace realization {
 
             virtual std::string get_formulation_type() = 0;
 
+            // TODO: to truly make this properly generalized (beyond catchments, and to some degree even in that
+            //  context) a more complex type for the entirety of the response/output is needed, perhaps with independent
+            //  access functions
+
+            // TODO: a reference to the actual time of the initial time step is needed
+
+            // TODO: a reference to the last calculated time step is needed
+
+            // TODO: a mapping of previously calculated time steps to the size/delta of each is needed (unless we
+            //  introduce a way to enforce immutable time step delta values for an object)
+
+            // TODO: a convenience method for getting the actual time of calculated time steps (at least the last)
+
             /**
              * Execute the backing model formulation for the given time step, where it is of the specified size, and
              * return the response output.
