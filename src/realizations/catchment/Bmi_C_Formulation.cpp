fefcfb00a107d2fbaf16547c517dff94aa17806d
Robert Bartel <robert.bartel@noaa.gov>
2020-11-30 11:30:00 -0600

    Helper getter func in BMI C formulation.
    Adding helper getter function to get an output value for a specified
    variable and time step, represented as a double.

--- src/realizations/catchment/Bmi_C_Formulation.cpp
+++ src/realizations/catchment/Bmi_C_Formulation.cpp
@@ -25,6 +25,27 @@ std::string Bmi_C_Formulation::get_output_header_line(std::string delimiter) {
     return boost::algorithm::join(get_output_header_fields(), delimiter);
 }
 
+double Bmi_C_Formulation::get_var_value_as_double(time_step_t t_index, const std::string& var_name) {
+    // TODO: change to using "at index" option
+    std::string type = get_bmi_model()->GetVarType(var_name);
+    if (type == "double") {
+        std::vector<double> outputs = get_bmi_model()->GetValue<double>(var_name);
+        return outputs[t_index];
+    }
+    else if (type == "float") {
+        std::vector<float> outputs = get_bmi_model()->GetValue<float>(var_name);
+        return (double)(outputs[t_index]);
+    }
+    else if (type == "int") {
+        std::vector<int> outputs = get_bmi_model()->GetValue<int>(var_name);
+        return (double)(outputs[t_index]);
+    }
+    else if (type == "long") {
+        std::vector<long> outputs = get_bmi_model()->GetValue<long>(var_name);
+        return (double)(outputs[t_index]);
+    }
+}
+
 bool Bmi_C_Formulation::is_model_initialized() {
     return get_bmi_model()->is_model_initialized();
 }
@@ -85,55 +106,5 @@ void Bmi_C_Formulation::inner_create_formulation(geojson::PropertyMap properties
     }
     else {
         set_output_header_fields(get_output_variable_names());
-    }
-}
-
-/**
- * Get the model response for this time step.
- *
- * Get the model response for this time step, execute the backing model formulation one or more times if the time step
- * of the given index has not already been processed.
- *
- * Function assumes the backing model has been fully initialized an that any additional input values have been applied.
- *
- * The function will return the value of the primary output variable (see `get_bmi_main_output_var()`) for the given
- * time step. The type returned will always be a `double`, with other numeric types being cast if necessary.
- *
- * Because of the nature of BMI, the `t_delta` parameter is ignored, as this cannot be passed meaningfully via
- * the `update()` BMI function.
- *
- * @param t_index The index of the time step for which to run model calculations.
- * @param d_delta_s The duration, in seconds, of the time step for which to run model calculations.
- * @return The total discharge of the model for the given time step.
- */
-double Bmi_C_Formulation::get_response(time_step_t t_index, time_step_t t_delta) {
-    if (get_bmi_model() == nullptr) {
-        throw std::runtime_error("Trying to process response of improperly created BMI C formulation.");
-    }
-    if (t_index < 0) {
-        throw std::invalid_argument("Getting response of negative time step in BMI C formulation is not allowed.");
-    }
-
-    int last_processed_time_step = get_bmi_model()->get_last_processed_time_step();
-    while (last_processed_time_step++ < t_index) {
-        get_bmi_model()->Update();
-    }
-    // TODO: change to using "at index" option
-    std::string type = get_bmi_model()->GetVarType(get_bmi_main_output_var());
-    if (type == "double") {
-        std::vector<double> outputs = get_bmi_model()->GetValue<double>(get_bmi_main_output_var());
-        return outputs[t_index];
-    }
-    else if (type == "float") {
-        std::vector<float> outputs = get_bmi_model()->GetValue<float>(get_bmi_main_output_var());
-        return (double)(outputs[t_index]);
-    }
-    else if (type == "int") {
-        std::vector<int> outputs = get_bmi_model()->GetValue<int>(get_bmi_main_output_var());
-        return (double)(outputs[t_index]);
-    }
-    else if (type == "long") {
-        std::vector<long> outputs = get_bmi_model()->GetValue<long>(get_bmi_main_output_var());
-        return (double)(outputs[t_index]);
     }
 }
