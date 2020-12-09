17fa7764bb4e32f97bf2275a24e0d182f98e3a8b
Nels Frazier <nels.frazier@noaa.gov>
2020-12-09 11:38:08 -0600

    decouple parameters from configuration

---
+++ models/lstm/include/lstm_config.h
@@ -0,0 +1,37 @@
+#ifndef NGEN_LSTM_CONFIGURATION_H
+#define NGEN_LSTM_CONFIGURATION_H
+
+#include <string>
+
+namespace lstm {
+
+    /**
+     * Structure for containing and organizing the parameters of the lstm hydrological model.
+     */
+    struct lstm_config {
+        std::string pytorch_model_path;
+        std::string normalization_path;
+        std::string initial_state_path;
+
+        /**
+         * Constructor for instances, initializing members that correspond one-to-one with a parameter and deriving
+         * @param pytorch_model_path string path to the pytorch .pt or .ptc trace file
+         * @param normalization_path string path to the csv file containing input normalization parameters (mean and standard deviation)
+         * @param initial_state_path string path to the csv file containing serialized state tensors h_t and c_t
+         */
+        lstm_config(
+           std::string pytorch_model_path,
+           std::string normalization_path,
+           std::string initial_state_path
+          ) :
+              pytorch_model_path(pytorch_model_path),
+              normalization_path(normalization_path),
+              initial_state_path(initial_state_path)
+              {}
+        lstm_config()
+          : pytorch_model_path(""), normalization_path(""), initial_state_path("")
+        {}
+    };
+}
+
+#endif //NGEN_LSTM_CONIGURATION_H
--- models/lstm/include/lstm_params.h
+++ models/lstm/include/lstm_params.h
@@ -7,47 +7,26 @@ namespace lstm {
      * Structure for containing and organizing the parameters of the lstm hydrological model.
      */
     struct lstm_params {
-        std::string pytorch_model_path;
-        std::string normalization_path;
         double latitude;
         double longitude;
         double area;
-
-
 
         /**
          * Constructor for instances, initializing members that correspond one-to-one with a parameter and deriving
          * remaining (non-const and hard-coded) members.
          *
-         * @param maxsmc Initialization param for maxsmc member.
-         * @param wltsmc Initialization param for wltsmc member.
-         * @param satdk Initialization param for satdk member.
-         * @param satpsi Initialization param for satpsi member.
-         * @param slope Initialization param for slope member.
-         * @param b Initialization param for b member.
-         * @param multiplier Initialization param for multiplier member.
-         * @param alpha_fc Initialization param for alpha_fc member.
-         * @param Klf Initialization param for Klf member.
-         * @param Kn Initialization param for Kn member.
-         * @param nash_n Initialization param for nash_n member.
-         * @param Cgw Initialization param for Cgw member.
-         * @param expon Initialization param for expon member.
-         * @param max_gw_storage Initialization param for max_groundwater_storage_meters member.
+         * @param lat Initialization param for latitude member.
+         * @param lon Initialization param for longitude member.
+         * @param area Initialization param for basin area member (in FIXME units).
          */
-        lstm_params(
-
-           std::string pytorch_model_path,
-           std::string normalization_path,
-           double lat, double lon, double area) :
-
-              pytorch_model_path(pytorch_model_path),
-              normalization_path(normalization_path),
+        lstm_params(double lat, double lon, double area) :
               latitude(lat),
               longitude(lon),
               area(area)
               {}
-
-
+        lstm_params()
+          : latitude(0), longitude(0), area(0)
+        {}
     };
 }
 
