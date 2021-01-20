94244d67937e08f379b24864ad723de763b5b56d
Robert Bartel <robert.bartel@noaa.gov>
2021-01-20 09:55:36 -0600

    Add custom exception for bad external model state.

---
+++ include/realizations/catchment/State_Exception.hpp
@@ -0,0 +1,39 @@
+#ifndef NGEN_BMI_MODEL_STATE_EXCEPTION_H
+#define NGEN_BMI_MODEL_STATE_EXCEPTION_H
+
+#include <exception>
+#include <utility>
+
+using namespace std;
+
+namespace models {
+    namespace external {
+
+        /**
+         * Custom exception indicating bad external model state.
+         *
+         * Custom exception for indicating that an external model has, or at least potentially has, entered into an
+         * invalid state.
+         *
+         */
+        class State_Exception : public std::exception {
+
+        public:
+
+            State_Exception(char const *const message) noexcept: std::exception(), what_message(message) {}
+
+            State_Exception(std::string message) noexcept
+                    : std::exception(), what_message(std::move(message)) {}
+
+            virtual char const *what() const noexcept {
+                return what_message.c_str();
+            }
+
+        private:
+
+            std::string what_message;
+        };
+    }
+}
+
+#endif //NGEN_BMI_MODEL_STATE_EXCEPTION_H
