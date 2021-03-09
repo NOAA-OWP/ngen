ea92de8945ec44b7c39f685f8e1c4abd96f5126f
Robert Bartel <robert.bartel@noaa.gov>
2021-03-09 10:13:21 -0600

    Fwd declare friend class for testing.

--- include/realizations/catchment/Bmi_Formulation.hpp
+++ include/realizations/catchment/Bmi_Formulation.hpp
@@ -23,6 +23,7 @@
 // Forward declaration to provide access to protected items in testing
 class Bmi_Formulation_Test;
 class Bmi_C_Formulation_Test;
+class Bmi_C_Cfe_IT;
 
 namespace realization {
 
