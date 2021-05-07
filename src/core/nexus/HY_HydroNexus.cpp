e99ab2a04893286e513c5fb9ef50ee2b14acd2e9
Nels Frazier <nels.frazier@noaa.gov>
2021-05-07 12:27:46 -0500

    add some documentation for recieving/contributing and fix constructor semantics accordingly

--- src/core/nexus/HY_HydroNexus.cpp
+++ src/core/nexus/HY_HydroNexus.cpp
@@ -1,15 +1,15 @@
 #include "HY_HydroNexus.hpp"
 
-HY_HydroNexus::HY_HydroNexus(std::string nexus_id, Catchments contributing_catchments):
-id(nexus_id), realization(), contributing_catchments(std::move(contributing_catchments)), receiving_catchments()
+HY_HydroNexus::HY_HydroNexus(std::string nexus_id, Catchments receiving_catchments):
+id(nexus_id), realization(), receiving_catchments(std::move(receiving_catchments)), contributing_catchments()
 {
    this->id_number = std::stoi(nexus_id.substr(4)); //FIXME brittle
    this->id = nexus_id;
-   this->number_of_downstream_catchments = contributing_catchments.size();
+   this->number_of_downstream_catchments = receiving_catchments.size();
 }
 
 HY_HydroNexus::HY_HydroNexus(std::string nexus_id, HydroLocation location, Catchments receiving_catchments, Catchments contributing_catchments):
-id(nexus_id), realization(std::move(location)), contributing_catchments(std::move(contributing_catchments)), receiving_catchments(std::move(contributing_catchments))
+id(nexus_id), realization(std::move(location)), receiving_catchments(std::move(contributing_catchments)), contributing_catchments(std::move(contributing_catchments))
 {}
 
 HY_HydroNexus::~HY_HydroNexus()
