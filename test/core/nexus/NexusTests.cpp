6a89aed9a42c07c79019195eba54b5f37059befc
Nels Frazier <nels.frazier@noaa.gov>
2021-05-07 08:53:31 -0500

    get nexus test to test a nexus

--- test/core/nexus/NexusTests.cpp
+++ test/core/nexus/NexusTests.cpp
@@ -1,8 +1,12 @@
 #include "gtest/gtest.h"
 
+#include "HY_PointHydroNexus.hpp"
 #include "HY_HydroLocation.hpp"
+#include "HY_IndirectPosition.hpp"
+
 #include <vector>
 #include <memory>
+using namespace hy_features::hydrolocation;
 
 class Nexus_Test : public ::testing::Test {
 
@@ -39,12 +43,12 @@ void Nexus_Test::TearDown() {
 //! Test that a HY_Hydrolocation can be created.
 TEST_F(Nexus_Test, TestInit0)
 {
-    HY_HydroLocation::point_t point(50,30);                                                  //!< Test data point
-    HY_HydroLocation::polygon_t polygon;                                                     //!< Test data location
-    HY_Features::HY_HydroLocationType type(HY_Features::HY_HydroLocationType::undefined);    //!< Test data type
-
-    std::shared_ptr<HY_HydroLocation> location = std::make_shared<HY_HydroLocation>(polygon, type, point);
-
-    ASSERT_TRUE(location != nullptr);
+    point_t point(50,30);                                                   //!< Test data location
+    //HY_HydroLocationType type(HY_HydroLocationType::undefined);    //!< Test data type
+    //HY_IndirectPosition pos;
+    //std::shared_ptr<HY_HydroLocation>location = std::make_shared<HY_HydroLocation>(point, type, pos);
+    std::vector<string> contrib = {"cat-1"};
+    //HY_PointHydroNexus("nex-0", location, std::vector<string>(), contrib);
+    HY_PointHydroNexus("nex-0", contrib);
+    ASSERT_TRUE( true );
 }
-
