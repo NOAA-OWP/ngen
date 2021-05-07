d33771b3c3e64b29de3c1ea8978688ab69919b67
Nels Frazier <nels.frazier@noaa.gov>
2021-05-07 08:48:06 -0500

    Move geometry definitions to GM_Object

--- include/core/catchment/HY_CatchmentArea.hpp
+++ include/core/catchment/HY_CatchmentArea.hpp
@@ -5,19 +5,12 @@
 
 #include "GM_Object.hpp"
 
-#include <boost/geometry.hpp>
-#include <boost/geometry/geometries/geometries.hpp>
-
 #include "StreamHandler.hpp"
 
-namespace bg = boost::geometry;
 
 class HY_CatchmentArea : public HY_CatchmentRealization, public GM_Object
 {
     public:
-
-    typedef bg::model::d2::point_xy<double> point_t;
-    typedef bg::model::polygon<point_t> polygon_t;
 
     HY_CatchmentArea();
     HY_CatchmentArea(forcing_params forcing_config, utils::StreamHandler output_stream); //TODO not sure I like this pattern
