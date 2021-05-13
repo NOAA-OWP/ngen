49a61a46e1aa2d9ef54423c27f6e11519a2e1413
Nels Frazier <nels.frazier@noaa.gov>
2021-05-13 13:01:12 -0500

    Expand implementation of HY_Catchment to hold proper identities per spec

--- include/core/catchment/HY_Catchment.hpp
+++ include/core/catchment/HY_Catchment.hpp
@@ -12,26 +12,40 @@ class HY_HydroNexus;
 
 class HY_Catchment : public HY_HydroFeature
 {
+    //using Nexuses = std::vector< std::shared_ptr<HY_HydroNexus> >;
+    using Nexus = std::string;
+    using Nexuses = std::vector< Nexus >;
+    //using Catchments = std::vector< std::shared_ptr<HY_Catchment> >;
+    using Catchment  = std::string;
+    using Catchments = std::vector< Catchment >;
     public:
 
     HY_Catchment();
+    HY_Catchment(std::string id, Nexuses inflows, Nexuses outflows, std::shared_ptr<HY_CatchmentRealization> realization):
+    id(std::move(id)),
+    inflows(std::move(inflows)),
+    outflows(std::move(outflows)),
+    contained_catchments(Catchments()),
+    containing_catchment({}),
+    conjoint_catchment({}),
+    upper_catchment({}),
+    lower_catchment({}),
+    realization(realization){}
     virtual ~HY_Catchment();
-
+    const Nexuses& get_outflow_nexuses(){ return outflows; }
+    std::shared_ptr<HY_CatchmentRealization> realization;
     protected:
-
-
 
     private:
 
-    std::shared_ptr<HY_CatchmentRealization> realization;
-    std::vector< std::shared_ptr<HY_HydroNexus> > inflows;
-    std::vector< std::shared_ptr<HY_HydroNexus> > outflows;
-    std::vector< std::shared_ptr<HY_Catchment> > recieving_catchments;
-    std::vector< std::shared_ptr<HY_Catchment> > contained_catchments;
-    std::shared_ptr<HY_Catchment> containing_catchment;
-
-    unsigned long id_number;
     std::string id;
+    Nexuses inflows;
+    Nexuses outflows;
+    Catchments contained_catchments;
+    Catchment containing_catchment;
+    Catchment conjoint_catchment;
+    Catchment upper_catchment;
+    Catchment lower_catchment;
 
 };
 
