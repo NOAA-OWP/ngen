679b923486b02e9a9b6f15e5a3c8668b1953a49a
Nels Frazier <nels.frazier@noaa.gov>
2021-05-13 13:00:22 -0500

    Hook nexus and catchment/realization into HY_Features collection

--- src/core/HY_Features.cpp
+++ src/core/HY_Features.cpp
@@ -1,31 +1,50 @@
 #include <HY_Features.hpp>
+#include <HY_PointHydroNexus.hpp>
 
 using namespace hy_features;
 
-HY_Features::HY_Features( geojson::GeoJSON fabric ){
-  HY_Features(network::Network(fabric));
+HY_Features::HY_Features( geojson::GeoJSON fabric, std::shared_ptr<Formulation_Manager> formulations)
+  :HY_Features(network::Network(fabric), formulations)
+{
+  /* If you nest the constuctor calls like this, then the network, while valid in the next constructor,
+     is empty once the collection is fully instansiated...
+    HY_Features(network::Network(fabric));
+  */
 }
 
-HY_Features::HY_Features(network::Network network):network(network){
+HY_Features::HY_Features(network::Network network, std::shared_ptr<Formulation_Manager> formulations)
+  :network(network), formulations(formulations)
+{
       std::string feat_id;
       std::string feat_type;
       std::vector<std::string> origins, destinations;
+
       for(const auto& feat_idx : network){
         feat_id = network.get_id(feat_idx);//feature->get_id();
         feat_type = feat_id.substr(0, 3);
 
+        destinations  = network.get_destination_ids(feat_id);
         if(feat_type == "cat")
         {
+          //Find and prepare formulation
+          auto formulation = formulations->get_formulation(feat_id);
+          formulation->set_output_stream(feat_id+".csv");
+          // TODO: add command line or config option to have this be omitted
+          //FIXME why isn't default param working here??? get_output_header_line() fails.
+          formulation->write_output("Time Step,""Time,"+formulation->get_output_header_line(",")+"\n");
+          //Find upstream nexus ids
           origins = network.get_origination_ids(feat_id);
-          destinations  = network.get_destination_ids(feat_id);
-
+          //Create the HY_Catchment with the formulation realization
           std::shared_ptr<HY_Catchment> c = std::make_shared<HY_Catchment>(
-              HY_Catchment(feat_id, origins, destinations)
+              HY_Catchment(feat_id, origins, destinations, formulation)
             );
+
+          _catchments.emplace(feat_id, c);
         }
         else if(feat_type == "nex")
         {
-
+            _nexuses.emplace(feat_id, std::make_unique<HY_PointHydroNexus>(
+                                          HY_PointHydroNexus(feat_id, destinations) ));
         }
         else
         {
@@ -33,9 +52,10 @@ HY_Features::HY_Features(network::Network network):network(network){
                    <<" Skipping feature"<<std::endl;
         }
       }
+
 }
 
-HY_Features::HY_Features( geojson::GeoJSON catchments, geojson::GeoJSON nexuses, std::string* link_key):
-    network(catchments, link_key){
+HY_Features::HY_Features( geojson::GeoJSON catchments, std::string* link_key, std::shared_ptr<Formulation_Manager> formulations):
+    HY_Features(network::Network(catchments, link_key), formulations){
 
 }
