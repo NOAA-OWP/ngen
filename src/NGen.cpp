1a1977d6ade66a1b08cbc91cd37b7ed923af88b5
Nels Frazier <nels.frazier@noaa.gov>
2021-05-13 13:25:55 -0500

    Driver cleanup

--- src/NGen.cpp
+++ src/NGen.cpp
@@ -3,18 +3,9 @@
 #include <string>
 #include <unordered_map>
 
-#include <FeatureBuilder.hpp>
-#include <FeatureVisitor.hpp>
-#include "features/Features.hpp"
-
 #include "realizations/catchment/Formulation_Manager.hpp"
-
-#include <HY_HydroNexus.hpp>
-#include <HY_Catchment.hpp>
-#include <Formulation.hpp>
-#include <HY_PointHydroNexus.hpp>
-
-#include <network.hpp>
+#include <Catchment_Formulation.hpp>
+#include <HY_Features.hpp>
 
 #include "NGenConfig.h"
 #include "tshirt_params.h"
@@ -26,58 +17,7 @@ std::string catchmentDataFile = "";
 std::string nexusDataFile = "";
 std::string REALIZATION_CONFIG_PATH = "";
 
-// TODO: Avoid having to call this in favor of having this being a core part of the creation of both the initial collections
-void prepare_features(geojson::GeoJSON& nexus, geojson::GeoJSON& catchments, bool validate=false)
-{
-  for(auto& feature: *nexus){
-    feature->set_id(feature->get_id());
-    //std::cout << "Got Nexus Feature " << feature->get_id() << std::endl;
-  }
-  nexus->update_ids();
-  //Now read the collection of catchments, iterate it and add them to the nexus collection
-  //also link them by to->id
-  //std::cout << "Iterating Catchment Features" << std::endl;
-  for(auto& feature: *catchments){
-    feature->set_id(feature->get_id());
-    nexus->add_feature(feature);
-    //std::cout<<"Catchment "<<feature->get_id()<<" -> Nexus "<<feature->get_property("toID").as_string()<<std::endl;
-  }
-  std::string linkage = "toid";
-  nexus->link_features_from_property(nullptr, &linkage);
-
-  if(validate){
-    //Convience check on linkage
-    for(auto& feature: *nexus){
-      //feature->set_id(feature->get_property("ID").as_string());
-      //std::cout << "Got Nexus Feature " << feature->get_id() << std::endl;
-      if( feature->get_id().substr(0, 3) == "cat") {
-        auto downstream = feature->destination_features();
-        if(downstream.size() > 1) {
-          std::cerr << "catchment " << feature->get_id() << " has more than one downstream connection" << std::endl;
-        }
-        else if(downstream.size() == 0) {
-            std::cerr << "catchment " << feature->get_id() << " has NO downstream connection" << std::endl;
-        }
-        else {
-          std::cout <<"catchment feature " << feature->get_id() << " to nexus feature " << downstream[0]->get_id() << std::endl;
-        }
-       }
-     }//end for(feature: nexus)
-   }//end if(validate)
-}
-
-std::unordered_map<std::string, std::unique_ptr<HY_HydroNexus>> nexus_realizations;
-std::unordered_map<std::string, std::string> catchment_to_nexus;
-std::unordered_map<std::string, std::string> nexus_to_catchment;
-std::unordered_map<std::string, std::string> nexus_from_catchment;
-std::unordered_map<std::string, std::vector<double>> output_map;
-
-std::unordered_map<std::string, std::ofstream> catchment_outfiles;
 std::unordered_map<std::string, std::ofstream> nexus_outfiles;
-
-//TODO move catchment int identity to relization, and update nexus to use string id
-std::unordered_map<std::string, int> catchment_id;
-
 
 pdm03_struct get_et_params() {
   // create the struct used for ET
@@ -90,9 +30,6 @@ pdm03_struct get_et_params() {
                                             (1.0 + pdm_et_data.scaled_distribution_fn_shape_parameter);
     return pdm_et_data;
 }
-
-typedef Simple_Lumped_Model_Realization _hymod;
-typedef realization::Tshirt_Realization _tshirt;
 
 int main(int argc, char *argv[]) {
     std::cout << "Hello there " << ngen_VERSION_MAJOR << "."
@@ -165,137 +102,68 @@ int main(int argc, char *argv[]) {
     // TODO: Instead of iterating through a collection of FeatureBase objects mapping to catchments, we instead want to iterate through HY_Catchment objects
     geojson::GeoJSON catchment_collection = geojson::read(catchmentDataFile, catchment_subset_ids);
 
-    prepare_features(nexus_collection, catchment_collection, !true);
-
-    // TODO: Have these formulations attached to the prior HY_Catchment objects
-    realization::Formulation_Manager manager = realization::Formulation_Manager(REALIZATION_CONFIG_PATH);
-    manager.read(catchment_collection, utils::getStdOut());
-
-    network::Network network(nexus_collection);
+    std::shared_ptr<realization::Formulation_Manager> manager = std::make_shared<realization::Formulation_Manager>(REALIZATION_CONFIG_PATH);
+    manager->read(catchment_collection, utils::getStdOut());
+    std::string link_key = "toid";
+    hy_features::HY_Features features = hy_features::HY_Features(catchment_collection, &link_key, manager);
+    //validate dendridic connections
+    features.validate_dendridic();
     //TODO don't really need catchment_collection once catchments are added to nexus collection
-    catchment_collection.reset();
-    for(auto& feature : *nexus_collection)
+    //Still using  catchments for geometry at the moment, fix this later
+    //catchment_collection.reset();
+    nexus_collection.reset();
+
+    //Still hacking nexus output  for the moment
+    for(const auto& id : features.nexuses())
     {
-      std::string feat_id = feature->get_id();
-      //FIXME rework how we use IDs to NOT force parsing???
-      //Skipping IDs that aren't "real" i.e. have a  NA id
-      if (feat_id.substr(4) == "NA") continue;
-
-      // We need a better way to identify catchments vs nexi
-      if( feat_id.substr(0, 3) == "cat" ){
-        catchment_outfiles[feat_id].open(feature->get_id()+"_output.csv", std::ios::trunc);
-
-        if(feature->get_number_of_destination_features() == 1)
-        {
-          catchment_to_nexus[feat_id] = feature->destination_features()[0]->get_id();
-        }
-        else
-        {
-          //TODO
-        }
-        catchment_id[feat_id] = std::stoi(feat_id.substr(4));
-      }else{
-        //Create nexus realization, add to map
-        int num = std::stoi( feat_id.substr(4) );
-        nexus_outfiles[feat_id].open("./"+feature->get_id()+"_output.csv", std::ios::trunc);
-        std::vector<std::string> downstreams;
-        for(const auto& f : feature->destination_features()){
-          downstreams.push_back(f->get_id());
-        }
-        nexus_realizations[feat_id] = std::make_unique<HY_PointHydroNexus>(
-                                      HY_PointHydroNexus(feat_id, downstreams) );
-       if(feature->get_number_of_destination_features() == 1)
-       {
-         nexus_to_catchment[feat_id] = feature->destination_features()[0]->get_id();
-       }
-       else if(feature->get_number_of_origination_features() == 1)
-       {
-         nexus_from_catchment[feat_id] = feature->origination_features()[0]->get_id();
-       }
-       else{
-         //TODO
-       }
-       output_map[feat_id] = std::vector<double>();
-      }
-
+      nexus_outfiles[id].open("./"+id+"_output.csv", std::ios::trunc);
     }
+
     std::cout<<"Running Models"<<std::endl;
 
     std::shared_ptr<pdm03_struct> pdm_et_data = std::make_shared<pdm03_struct>(get_et_params());
 
     //Now loop some time, iterate catchments, do stuff for total number of output times
-    for(int output_time_index = 0; output_time_index < manager.Simulation_Time_Object->get_total_output_times(); output_time_index++)
+    for(int output_time_index = 0; output_time_index < manager->Simulation_Time_Object->get_total_output_times(); output_time_index++)
     {
-      std::cout<<"Output Time Index: "<<output_time_index<<std::endl;
-
-      std::string current_timestamp = manager.Simulation_Time_Object->get_timestamp(output_time_index);
-      for(auto&  feature: network)
+      //std::cout<<"Output Time Index: "<<output_time_index<<std::endl;
+      if(output_time_index%100 == 0) std::cout<<"Running timestep "<<output_time_index<<std::endl;
+      std::string current_timestamp = manager->Simulation_Time_Object->get_timestamp(output_time_index);
+      for(const auto& id : features.catchments())
       {
-
-      }
-      /*
-      for (std::pair<std::string, std::shared_ptr<realization::Formulation>> formulation_pair : manager ) {
-        formulation_pair.second->set_et_params(pdm_et_data);
-
-        //get the catchment response
-        double response = formulation_pair.second->get_response(output_time_index, 3600.0);
-        //dump the output
-        std::cout<<"\tCatchment "<<formulation_pair.first<<" contributing "<<response<<" m/s to "<<catchment_to_nexus[formulation_pair.first]<<std::endl;
-
-      // If the timestep is 0, also write the header line to the file
-        // TODO: add command line or config option to have this be omitted
-        if (output_time_index == 0) {
-            // Append "Time Step" to header string provided by formulation, since we'll also add time step to output
-            std::string header_str = formulation_pair.second->get_output_header_line();
-            catchment_outfiles[formulation_pair.first] << "Time Step," << "Time," << header_str <<std::endl;
-        }
-        std::string output_str = formulation_pair.second->get_output_line_for_timestep(output_time_index);
-
-        catchment_outfiles[formulation_pair.first] << output_time_index << "," << current_timestamp << "," << output_str << std::endl;
-
-        response = response * boost::geometry::area(nexus_collection->get_feature(formulation_pair.first)->geometry<geojson::multipolygon_t>());
-
-        std::cout << "\t\tThe modified response is: " << response << std::endl;
+        //std::cout<<"Running cat "<<id<<std::endl;
+        auto r = features.catchment_at(id);
+        //TODO redesign to avoid this cast
+        auto r_c = dynamic_pointer_cast<realization::Catchment_Formulation>(r);
+        r_c->set_et_params(pdm_et_data);
+        double response = r_c->get_response(output_time_index, 3600.0);
+        std::string output = std::to_string(output_time_index)+","+current_timestamp+","+
+                             r_c->get_output_line_for_timestep(output_time_index)+"\n";
+        r_c->write_output(output);
+        //TODO put this somewhere else.  For now, just trying to ensure we get m^3/s into nexus output
+        response = response * boost::geometry::area(catchment_collection->get_feature(id)->geometry<geojson::multipolygon_t>());
         //update the nexus with this flow
-        //nexus_realizations[ catchment_to_nexus[formulation_pair.first] ]->add_upstream_flow(response, catchment_id[formulation_pair.first], output_time_index);
-        nexus_realizations[ catchment_to_nexus[formulation_pair.first] ]->add_upstream_flow(response, formulation_pair.first, output_time_index);
-      }
-
-      for(auto &nexus: nexus_realizations)
-      {
-        //TODO this ID isn't all that important is it?  And really it should connect to
-        //the downstream waterbody the way we are using it, so consider if this is needed
-        //it works for now though, so keep it
-        //int id = catchment_id[nexus_from_catchment[nexus.first]];
-        std::string id = nexus_from_catchment[nexus.first];
-        std::cout<<"nexusID: "<<id<<std::endl;
-        double contribution_at_t = nexus_realizations[nexus.first]->get_downstream_flow(id, output_time_index, 100.0);
-        if(nexus_outfiles[nexus.first].is_open())
+        for(auto& nexus : features.destination_nexuses(id))
         {
-          nexus_outfiles[nexus.first] << output_time_index << ", " << current_timestamp << ", " << contribution_at_t << std::endl;
+          //TODO in a DENDRIDIC network, only one destination nexus per catchment
+          //If there is more than one, some form of catchment partitioning will be required.
+          //for now, only contribute to the first one in the list
+          nexus->add_upstream_flow(response, id, output_time_index);
+          break;
         }
-        std::cout<<"\tNexus "<<nexus.first<<" has "<<contribution_at_t<<" m^3/s"<<std::endl;
-        output_map[nexus.first].push_back(contribution_at_t);
-      }
-      */
-    }
-
-    /*
-        The basic driving algorithm looks something like this:
-
-        Iteration 1: Only read catchments and nexus
-        Read configuration;
-        From configuration, read inputs
-        inputs: id, realization enum, input file (geojson), forcing provider (enum)
-
-        loop:
-          id -> read geojson -> lookup by id -> construct catchment realization and nexus
-        TODO catchment realizations will need to construct forcing objects (implement forcing provider interface) upon constructuion
-        One INDEPDENT timestep
-        run catchment A -> forcing.get(time, dt, <grid?> ) -> run_model(time, dt, forcing)
-        run catchment B
-
-        Iteration 2: Read waterbodies, build linkage between nexus/waterbody, apply routing, multiple timesteps
-
-    */
+      } //done catchments
+      //At this point, could make an internal routing pass, extracting flows from nexuses and routing
+      //across the flowpath to the next nexus.
+      //Once everything is updated for this timestep, dump the nexus output
+      for(const auto& id : features.nexuses())
+      {
+        double contribution_at_t = features.nexus_at(id)->get_downstream_flow("id-consuming-flow", output_time_index, 100.0);
+        if(nexus_outfiles[id].is_open())
+        {
+          nexus_outfiles[id] << output_time_index << ", " << current_timestamp << ", " << contribution_at_t << std::endl;
+        }
+        //std::cout<<"\tNexus "<<id<<" has "<<contribution_at_t<<" m^3/s"<<std::endl;
+      } //done nexuses
+    } //done time
+    std::cout<<"Finished "<<manager->Simulation_Time_Object->get_total_output_times()<<" timesteps."<<std::endl;
 }
