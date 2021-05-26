cd53829a5a2b0d515967580319077fdd1ecd8774
David Mattern <dmattern@cheyenne1.cheyenne.ucar.edu>
2021-05-26 16:05:02 -0600

    basic build of routing py adapter

---
+++ include/routing/Routing_Py_Adapter.hpp
@@ -0,0 +1,25 @@
+#ifndef NGEN_ROUTING_PY_ADAPTER_H
+#define NGEN_ROUTING_PY_ADAPTER_H
+
+#ifdef ACTIVATE_PYTHON
+
+#include <exception>
+#include <memory>
+#include <string>
+//#include "bmi.hxx"
+#include "pybind11/pybind11.h"
+#include "pybind11/pytypes.h"
+#include "pybind11/numpy.h"
+//#include "JSONProperty.hpp"
+//#include "StreamHandler.hpp"
+
+
+
+
+
+
+
+
+#endif //ACTIVATE_PYTHON
+
+#endif //NGEN_ROUTING_PY_ADAPTER_H
