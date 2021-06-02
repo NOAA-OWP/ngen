54bb3e984dfa3584b7be9ef536daf5ae2fbc35c6
David Mattern <dmattern@cheyenne6.cheyenne.ucar.edu>
2021-06-02 11:58:38 -0600

    updated t-route input paths in routing py bind test

--- test/routing/Routing_Py_Bind_Test.cpp
+++ test/routing/Routing_Py_Bind_Test.cpp
@@ -1,9 +1,12 @@
 #ifdef ROUTING_PYBIND_TESTS_ACTIVE
 #include "gtest/gtest.h"
 #include "routing/Routing_Py_Adapter.hpp"
-
+#include <string>
 #include <pybind11/embed.h>
 namespace py = pybind11;
+
+using namespace std;
+
 
 class RoutingPyBindTest : public ::testing::Test {
 
@@ -23,52 +26,54 @@ TEST_F(RoutingPyBindTest, TestRoutingPyBind)
   // Start Python interpreter and keep it alive
   py::scoped_interpreter guard{};
 
-
   py::print("Hello, World!"); // use the Python API
 
   py::module_ sys = py::module_::import("sys");
+
   py::print(sys.attr("path"));
 
   py::object sys_path = sys.attr("path"); 
-  //py::print(sys_path);
 
   py::object sys_path_append = sys_path.attr("append");
 
-  sys_path_append("/glade/work/dmattern/t-route/src/external_connections");
-  //sys_path_append("/glade/work/dmattern/t-route");
 
-  sys_path_append("/glade/work/dmattern/t-route/src/python_framework_v02");
-  sys_path_append("/glade/work/dmattern/t-route/src/python_routing_v02");
-  sys_path_append("/glade/work/dmattern/t-route/test/input/next_gen");
-
- 
-  //py::exec(sys_path.attr("append('/glade/work/dmattern/t-route/src/external_connections')"));
-  //py::exec(sys_path.attr("append('/glade/work/dmattern/t-route/src/external_connections')"));
+  ////////////////
+  //SET THESE AS INPUTS
+  string t_route_connection_path = "../../t-route/src/external_connections";
+  string input_path = "../../t-route/test/input/next_gen";
 
 
-
- //py::exec(sys.attr("path").attr("append('/glade/work/dmattern/t-route/src/external_connections')"));
-
-  //py::print(sys.insert(1, "/glade/work/dmattern/t-route/src/external_connections"));
-  //py::print(sys.attr("path")(1, "/glade/work/dmattern/t-route/src/external_connections"));
-
-  //py::eval("sys.path.append('/glade/work/dmattern/t-route/src/external_connections')");
-  //py::eval("sys.path.append('/glade/work/dmattern/t-route/src/external_connections')");
-  
-
-  //py::sys.path.insert(1, "/glade/work/dmattern/t-route/src/external_connections");
-  //py::exec(sys.path.insert(1, "/glade/work/dmattern/t-route/src/external_connections"));
-
-  //py::module_ t_route = py::module_::import("/glade/work/dmattern/t-route/src/external_connections/next_gen_network_module");
- 
-  
+  //USE THIS
+  sys_path_append(t_route_connection_path);
+  sys_path_append(input_path);
+  ////////////////////
 
   //Use this
   py::module_ t_route_module = py::module_::import("next_gen_network_module");
  
   py::print(sys.attr("path"));
 
-  
+  py::object test_add = t_route_module.attr("test_add"); 
+
+  ////////////////////
+  int c = 1;
+  int d = 2;
+  int e = 0;
+  //e = test_add("c,d");
+  //e = test_add(c,d);
+  //py::print(e);
+  py::print(test_add(c,d)); //WORKS
+  ////////////////
+
+  py::object set_paths = t_route_module.attr("set_paths"); 
+
+  set_paths(t_route_connection_path);
+
+
+
+
+
+ 
   ASSERT_TRUE(true);
 
 
