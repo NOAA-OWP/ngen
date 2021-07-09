a4280dcec76532f3038491005ae5f774075323a3
Robert Bartel <robert.bartel@noaa.gov>
2021-07-09 10:51:03 -0500

    Add suggested routing_param default constructor.

--- include/routing/Routing_Params.h
+++ include/routing/Routing_Params.h
@@ -11,6 +11,11 @@ struct routing_params
     std::string t_route_connection_path;
     std::string input_path;
 
+    /**
+     * Default constructor, using empty strings for both member values
+     */
+    routing_params() : t_route_connection_path(""), input_path("") {}
+
     /*
      * @brief Constructor for routing_params
      * @param t_route_connection_path
