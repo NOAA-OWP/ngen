8ab74cd9eb407d3b13445ff38eb6ed4649de2fd9
Robert Bartel <robert.bartel@noaa.gov>
2021-07-15 10:37:44 -0500

    Let BMI formulation init with Forcing.
    Adding constructors to existing types and super types of BMI
    formulations that accept already created forcing object instead of a
    forcing config.

--- src/core/catchment/HY_CatchmentArea.cpp
+++ src/core/catchment/HY_CatchmentArea.cpp
@@ -5,6 +5,8 @@ HY_CatchmentArea::HY_CatchmentArea()
     //ctor
 }
 
+HY_CatchmentArea::HY_CatchmentArea(Forcing forcing, utils::StreamHandler output_stream) : HY_CatchmentRealization(forcing), output(output_stream) { }
+
 HY_CatchmentArea::HY_CatchmentArea(forcing_params forcing_config, utils::StreamHandler output_stream) : HY_CatchmentRealization(forcing_config), output(output_stream)
 {
     //ctor
