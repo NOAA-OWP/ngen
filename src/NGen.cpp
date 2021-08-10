2a65520a37f832ebec7abb96aeb42383e6d769c6
Robert Bartel <robert.bartel@noaa.gov>
2021-08-10 14:29:15 -0500

    Add hydrofabric subdividing funcs to driver.
    Adding helper functions for subdividing hydrofabrics per partition over
    MPI to NGen.cpp.

--- src/NGen.cpp
+++ src/NGen.cpp
@@ -27,9 +27,19 @@ std::string nexusDataFile = "";
 std::string REALIZATION_CONFIG_PATH = "";
 
 #ifdef NGEN_MPI_ACTIVE
+
+#ifndef MPI_HF_SUB_CODE_GOOD
+#define MPI_HF_SUB_CODE_GOOD 0
+#endif
+
+#ifndef MPI_HF_SUB_CODE_BAD
+#define MPI_HF_SUB_CODE_BAD 1
+#endif
+
 #include <mpi.h>
 #include "core/Partition_Parser.hpp"
 #include <HY_Features_MPI.hpp>
+#include "PyHydrofabricSubsetter.hpp"
 
 std::string PARTITION_PATH = "";
 int mpi_rank;
@@ -53,21 +63,137 @@ pdm03_struct get_et_params() {
     return pdm_et_data;
 }
 
+#ifdef NGEN_MPI_ACTIVE
+
 /**
- * Check whether a given file passed as a runtime exec parameter exists, printing a message when it does not.
+ * Perform an "AND" type sync across all ranks to synchronize whether all are in a 'success' or 'ready' state.
  *
- * @param fileName The name of the file to check.
- * @param description A description of why file was passed as a parameter, used in error messages when a file doesn't
- *                    exist.
- * @return Whether the given file exists.
+ * Function should be called at the same time by all ranks to sync on some 'success' or 'ready' state for all the ranks.
+ * It handles the required MPI communication and the processing of received message content.
+ *
+ * Function accepts a value for some boolean status property.  This initial value is applicable only to the local MPI
+ * rank, with all MPI ranks having the status property and each having its own independent value.  The function has all
+ * ranks (except ``0``) send their local status to rank ``0``.  Rank ``0`` then applies a Boolean AND to the statuses
+ * (including its own) to produce a global status value.  The global status is then broadcast back to the other ranks.
+ * Finally, the value indicated by this global status is returned.
+ *
+ * @param status The initial individual state for the current MPI rank.
+ * @param taskDesc A description of the related task, used by rank 0 to print a message when included for any rank that
+ *                 is not in the success/ready state.
+ * @return Whether all ranks coordinating status had a success/ready status value.
  */
-bool check_param_file_exists(const std::string &fileName, const std::string &description) {
-    if( !utils::FileChecker::file_is_readable(fileName) ) {
-        std::cout << description << " path " << fileName << " not readable" << std::endl;
+bool mpiSyncStatusAnd(bool status, const std::string &taskDesc) {
+    // Expect 0 is good and 1 is no good for goodCode
+    // TODO: assert this in constructor or somewhere, or maybe just in a unit test
+    unsigned short codeBuffer;
+    bool printMessage = !taskDesc.empty();
+    // For the other ranks, start by properly setting the status code value in the buffer and send to rank 0
+    if (mpi_rank != 0) {
+        codeBuffer = status ? MPI_HF_SUB_CODE_GOOD : MPI_HF_SUB_CODE_BAD;
+        MPI_Send(&codeBuffer, 1, MPI_UNSIGNED_SHORT, 0, 0, MPI_COMM_WORLD);
+    }
+    // In rank 0, the first step is to receive and process codes from the other ranks into unified global status
+    else {
+        for (int i = 1; i < mpi_num_procs; ++i) {
+            MPI_Recv(&codeBuffer, 1, MPI_UNSIGNED_SHORT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // If any is ever "not good", overwrite status to be "false"
+            if (codeBuffer != MPI_HF_SUB_CODE_GOOD) {
+                if (printMessage) {
+                    std::cout << "Rank " << i << " not successful/ready after " << taskDesc << std::endl;
+                }
+                status = false;
+            }
+        }
+        // Rank 0 must also now prepare the codeBuffer value for broadcasting the global status
+        codeBuffer = status ? MPI_HF_SUB_CODE_GOOD : MPI_HF_SUB_CODE_BAD;
+    }
+    // Execute broadcast of global status rooted at rank 0
+    MPI_Bcast(&codeBuffer, mpi_num_procs - 1, MPI_UNSIGNED_SHORT, 0, MPI_COMM_WORLD);
+    return codeBuffer == MPI_HF_SUB_CODE_GOOD;
+}
+
+/**
+ * Convenience method for overloaded function when no message is needed, and thus no description param provided.
+ *
+ * @param status The initial individual state for the current MPI rank.
+ * @return Whether all ranks coordinating status had a success/ready status value.
+ * @see mpiSyncStatusAnd(bool, const std::string&)
+ */
+bool mpiSyncStatusAnd(bool status) {
+    return mpiSyncStatusAnd(status, "");
+}
+
+/**
+ * Check whether the parameter hydrofabric files have been subdivided into appropriate per partition files.
+ *
+ * Checks to see if partition specific subfiles corresponding to each partition/process already exist.  E.g., for a file
+ * at ``/dirname/catchment_data.geojson`` and two MPI processes, checks if both ``/dirname/catchment_data.geojson.0``
+ * and ``/dirname/catchment_data.geojson.1 `` exist.
+ *
+ * This check is performed for both the catchment and nexus hydrofabric base file names, as stored in the global
+ * ``catchmentDataFile`` and ``nexusDataFile`` variables respectively.  The number of MPI processes is obtained from the
+ * global ``mpi_rank`` variable.
+ *
+ * @return Whether proprocessing has already been performed to divide the main hydrofabric into existing, individual
+ *         sub-hydrofabric files for each partition/process.
+ */
+bool is_hydrofabric_subdivided() {
+    std::string name = catchmentDataFile + "." + std::to_string(mpi_rank);
+    // Initialize isGood based on local state.  Here, local file is "good" when it already exists.
+    // TODO: this isn't actually checking whether the files are right (just that they are present) so do we need to?
+    bool isGood = utils::FileChecker::file_is_readable(name);
+    return mpiSyncStatusAnd(isGood);
+}
+
+/**
+ * Attempt to subdivide the passed hydrofabric files into a series of per-partition files.
+ *
+ * This function assumes that, when it is called, the intent is for it to produce a freshly subdivided hydrofabric and
+ * associated files.  As a result, if there are any other subdivided hydrofabric files present having the same names
+ * as the files the function will write, then those preexisting files are considered stale and overwritten.
+ *
+ * @return Whether subdividing was successful.
+ */
+bool subdivide_hydrofabric() {
+    unsigned short codeBuffer;
+    // Track whether things are good, meaning ok to continue and, at the end, whether successful
+    // Start with a value of true
+    bool isGood = true;
+
+    // For now just have this be responsible for its own rank file
+    // Later consider whether it makes more sense for one rank (per host) to write all files
+    std::vector<int> indices = {mpi_rank};
+    std::unique_ptr<utils::PyHydrofabricSubsetter> subdivider;
+    try {
+        subdivider = std::make_unique<utils::PyHydrofabricSubsetter>(catchmentDataFile, nexusDataFile, PARTITION_PATH);
+    }
+    catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        // Set not good if the subdivider object couldn't be instantiated
+        isGood = false;
+    }
+    // Sync with the rest of the ranks and bail if any aren't ready to proceed for any reason
+    if (!mpiSyncStatusAnd(isGood, "initializing hydrofabric subdivider")) {
         return false;
     }
-    return true;
+
+    // Try to perform the subdividing (for now, have each rank handle its own file)
+    try {
+        isGood = subdivider->execSubdivision(mpi_rank);
+    }
+    catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        // Set not good if the subdivider object couldn't be instantiated
+        isGood = false;
+    }
+
+    // Now sync ranks on whether the subdividing function was executed successfully, and return
+    return mpiSyncStatusAnd(isGood, "executing hydrofabric subdivision");
 }
+
+
+
+#endif // NGEN_MPI_ACTIVE
 
 int main(int argc, char *argv[]) {
     std::cout << "Hello there " << ngen_VERSION_MAJOR << "."
