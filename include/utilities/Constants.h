d6070d43918147fbe745464c90e6fedee4a6edd2
Robert Bartel <robert.bartel@noaa.gov>
2021-08-20 11:15:46 -0500

    Fix remote get_downstream_flow with sentinel.
    Fixing HY_PointHydroNexusRemote get_downstream_flow to ensure it returns
    in control branch when it sends to a remote receiver, and specifically
    returns a defined sentinel constant.

--- include/utilities/Constants.h
+++ include/utilities/Constants.h
@@ -7,4 +7,13 @@
 // Units for water specific weight value: Newtons / meters^3
 #define WATER_SPECIFIC_WEIGHT 9810
 
+/**
+ * Sentinel for indicating something that could be handled locally or remotely is remote.
+ *
+ * A sentinel that indicates that some value that could potentially be the responsibility of local (in the context of
+ * an MPI rank) entity is actually the responsibility of a remote entity in a different rank.  As such, generally the
+ * value itself should not be directly used as it typically would.
+ */
+const int SENTINEL_IS_REMOTELY_HANDLED = -9999;
+
 #endif //NGEN_CONSTANTS_H
