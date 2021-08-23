6f2408d69138241d901c964438ab95a5b45ab055
Robert Bartel <robert.bartel@noaa.gov>
2021-08-23 10:38:23 -0500

    Fix "inline" virtual BMI C formulation funcs.

--- include/realizations/catchment/Bmi_C_Formulation.hpp
+++ include/realizations/catchment/Bmi_C_Formulation.hpp
@@ -91,9 +91,9 @@ namespace realization {
          */
         double get_response(time_step_t t_index, time_step_t t_delta) override;
 
-        inline bool is_bmi_input_variable(const std::string &var_name) override;
+        bool is_bmi_input_variable(const std::string &var_name) override;
 
-        inline bool is_bmi_output_variable(const std::string &var_name) override;
+        bool is_bmi_output_variable(const std::string &var_name) override;
 
     protected:
 
