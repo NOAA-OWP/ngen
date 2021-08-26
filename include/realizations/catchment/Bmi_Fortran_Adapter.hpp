d350ee52abdca9d3341c875229c659dc68e9efd9
Robert Bartel <robert.bartel@noaa.gov>
2021-08-26 08:40:29 -0500

    Add func defs to Fortran adapter for BMI.
    Adding required outstanding BMI function definitions to
    Bmi_Fortran_Adapter.

--- include/realizations/catchment/Bmi_Fortran_Adapter.hpp
+++ include/realizations/catchment/Bmi_Fortran_Adapter.hpp
@@ -56,9 +56,350 @@ namespace models {
                 }
             }
 
+            string GetComponentName() override;
+
+            /**
+             * Get the backing model's current time.
+             *
+             * Get the backing model's current time, relative to the start time, in the units expressed by
+             * `GetTimeUnits()`.
+             *
+             * @return The backing model's current time.
+             */
+            double GetCurrentTime() override;
+
+            double GetEndTime() override;
+
+            /**
+             * The number of BMI input variables the model makes available.
+             *
+             * @return The number of BMI input variables the model makes available.
+             * @see inner_get_input_item_count
+             */
+            int GetInputItemCount() override {
+                return inner_get_input_item_count();
+            }
+
+            /**
+             * Get input variable names for the backing model.
+             *
+             * @return A vector of BMI input variable names for the backing model.
+             */
+            std::vector<std::string> GetInputVarNames() override;
+
+            /**
+             * The number of BMI output variables the model makes available.
+             *
+             * @return The number of BMI output variables the model makes available.
+             * @see inner_get_output_item_count
+             */
+            int GetOutputItemCount() override {
+                return inner_get_output_item_count();
+            }
+
+            std::vector<std::string> GetOutputVarNames() override;
+
+            /**
+             * Get the backing model's starting time.
+             *
+             * Get the backing model's starting time, in the units expressed by `GetTimeUnits()`.
+             *
+             * The BMI standard start time is typically defined to be `0.0`.
+             *
+             * @return
+             */
+            double GetStartTime() override;
+
+            /**
+             * Get the time step used in the model.
+             *
+             * Get the time step size used in the backing model, expressed as a `double` type value.
+             *
+             * This function defers to the behavior of the analogous function `get_time_step` of the backing model.  As
+             * such, the the model-specific documentation for this function should be consulted for utilized models.
+             *
+             * @return The time step size of the model.
+             */
+            double GetTimeStep() override;
+
+            /**
+             * Get the units of time for the backing model.
+             *
+             * Get the units of time for the backing model, as a standard convention string representing the unit
+             * obtained directly from the model.
+             *
+             * @return A conventional string representing the time units for the model.
+             */
+            std::string GetTimeUnits() override;
+
+            /**
+             * Get value(s) for a variable.
+             *
+             * Implementation of virtual function in BMI interface.
+             *
+             * @param name
+             * @param dest
+             * @see get_value
+             */
+            void GetValue(std::string name, void *dest) override {
+                inner_get_value(name, dest);
+            }
+
+            /**
+             * Get a copy of values of a given variable.
+             *
+             * @tparam T
+             * @param name
+             * @see get_value
+             */
+            template <typename T>
+            std::vector<T> GetValue(const std::string& name) {
+                std::string type = GetVarType(name);
+                int total_mem = GetVarNbytes(name);
+                int item_size = GetVarItemsize(name);
+                int num_items = total_mem/item_size;
+
+                void* dest = malloc(total_mem);
+
+                inner_get_value(name, dest);
+
+                std::vector<T> retrieved_results(num_items);
+                T* d_results_ptr;
+                d_results_ptr = (T*) dest;
+
+                for (int i = 0; i < num_items; i++)
+                    retrieved_results[i] = d_results_ptr[i];
+
+                /*
+                std::vector<T> retrieved_results;
+
+                if (type == "double") {
+                    retrieved_results = std::vector<double>(num_items);
+                    double d_results[num_items];
+                    double* d_results_ptr = d_results;
+                    d_results_ptr = (double*) dest;
+                    for (int i = 0; i < num_items; i++)
+                        retrieved_results[i] = d_results_ptr[i];
+                }
+                */
+
+                free(dest);
+                return retrieved_results;
+            }
+
+            void GetValueAtIndices(std::string name, void *dest, int *inds, int count) override;
+
+            void GetGridShape(const int grid, int *shape) override;
+
+            void GetGridSpacing(const int grid, double *spacing) override;
+
+            void GetGridOrigin(const int grid, double *origin) override;
+
+            void GetGridX(const int grid, double *x) override;
+
+            void GetGridY(const int grid, double *y) override;
+
+            void GetGridZ(const int grid, double *z) override;
+
+            int GetGridNodeCount(const int grid) override;
+
+            int GetGridEdgeCount(const int grid) override;
+
+            int GetGridFaceCount(const int grid) override;
+
+            void GetGridEdgeNodes(const int grid, int *edge_nodes) override;
+
+            void GetGridFaceEdges(const int grid, int *face_edges) override;
+
+            void GetGridFaceNodes(const int grid, int *face_nodes) override;
+
+            void GetGridNodesPerFace(const int grid, int *nodes_per_face) override;
+
             void *GetValuePtr(std::string name) override {
                 throw std::runtime_error(model_name + " cannot currently get pointers for Fortran-based BMI modules.");
             }
+
+            /**
+             * Get a reference to the value(s) of a given variable.
+             *
+             * This function gets and returns the reference (i.e., pointer) to the backing model's variable with the
+             * given name.  Unlike what is returned by `GetValue()`, this will point to the current value(s) of the
+             * variable, even if the model's state has changed.
+             *
+             * Because of how C-based BMI models are implemented, raw pointers are returned.  The pointers are "owned"
+             * by the model implementation, though, so there should be no risk of memory leaks (i.e., strictly as a
+             * result of obtaining a reference via this function.)
+             *
+             * @tparam T   The type of the variable to which a reference will be returned.
+             * @param name The name of the variable to which a reference will be returned.
+             * @return A reference to the value(s) of a given variable
+             */
+            template<class T>
+            T *GetValuePtr(const std::string &name) {
+                int nbytes;
+                if (get_var_nbytes(bmi_model->handle, name.c_str(), &nbytes) != BMI_SUCCESS)
+                    throw std::runtime_error(model_name + " failed to get pointer for BMI variable " + name + ".");
+                void *dest = GetValuePtr(name);
+                T *ptr = (T *) dest;
+                return ptr;
+            }
+
+            /**
+             * Get the size (in bytes) of one item of a variable.
+             *
+             * @param name
+             * @return
+             */
+            int GetVarItemsize(std::string name) override;
+
+            /**
+             * Get the total size (in bytes) of a variable.
+             *
+             * This function provides the total amount of memory used to store a variable; i.e., the number of items
+             * multiplied by the size of each item.
+             *
+             * @param name
+             * @return
+             */
+            int GetVarNbytes(std::string name) override;
+
+            /**
+             * Get the data type of a variable.
+             *
+             * @param name
+             * @return
+             */
+            int GetVarGrid(std::string name) override;
+
+            /**
+             * Get the grid (id) of a variable.
+             *
+             * @param name
+             * @return
+             */
+            std::string GetVarType(std::string name) override;
+
+            /**
+             * Get the units for a variable.
+             *
+             * @param name
+             * @return
+             */
+            std::string GetVarUnits(std::string name) override;
+
+            /**
+             * Get location for a variable.
+             *
+             * @param name
+             * @return
+             */
+            std::string GetVarLocation(std::string name) override;
+
+            /**
+             * Get grid type for a grid-id.
+             *
+             * @param name
+             * @return
+             */
+            std::string GetGridType(int grid_id) override;
+
+            /**
+             * Get grid rank for a grid-id.
+             *
+             * @param name
+             * @return
+             */
+            int GetGridRank(int grid_id) override;
+
+            /**
+             * Get grid size for a grid-id.
+             *
+             * @param name
+             * @return
+             */
+            int GetGridSize(int grid_id) override;
+
+            /**
+             * Whether the backing model has been initialized yet.
+             *
+             * @return Whether the backing model has been initialized yet.
+             */
+            bool is_model_initialized();
+
+            void SetValue(std::string name, void *src) override;
+
+            template<class T>
+            void SetValue(std::string name, std::vector<T> src) {
+                size_t item_size;
+                try {
+                    item_size = (size_t) GetVarItemsize(name);
+                }
+                catch (std::runtime_error &e) {
+                    throw std::runtime_error("Cannot set " + name + " variable of " + model_name +
+                                             "; unable to test item sizes are equal (does variable exist for model?)");
+                }
+                if (item_size != sizeof(src[0])) {
+                    throw std::runtime_error("Cannot set " + name + " variable of " + model_name +
+                                             " with values of different item size");
+                }
+                SetValue(std::move(name), static_cast<void *>(src.data()));
+            }
+
+            void SetValueAtIndices(std::string name, int *inds, int count, void *src) override;
+
+            /**
+             *
+             * @tparam T
+             * @param name
+             * @param inds Collection of indexes within the model for which this variable should have a value set.
+             * @param src Values that should be set for this variable at the corresponding index in `inds`.
+             */
+
+            template<class T>
+            void SetValueAtIndices(const std::string &name, std::vector<int> inds, std::vector<T> src) {
+                if (inds.size() != src.size()) {
+                    throw std::runtime_error("Cannot set specified indexes for " + name + " variable of " + model_name +
+                                             " when index collection is different size than collection of values.");
+                }
+                if (inds.empty()) {
+                    return;
+                }
+                size_t item_size;
+                try {
+                    item_size = (size_t) GetVarItemsize(name);
+                }
+                catch (std::runtime_error &e) {
+                    throw std::runtime_error("Cannot set specified indexes for " + name + " variable of " + model_name +
+                                             "; unable to test item sizes are equal (does variable exist for model?)");
+                }
+                if (item_size != sizeof(src[0])) {
+                    throw std::runtime_error("Cannot set specified indexes for " + name + " variable of " + model_name +
+                                             " with values of different item size");
+                }
+                SetValueAtIndices(name, inds.data(), inds.size(), static_cast<void *>(src.data()));
+            }
+
+            /**
+             * Have the backing model update to next time step.
+             *
+             * Have the backing BMI model perform an update to the next time step according to its own internal time
+             * keeping.
+             */
+            void Update() override;
+
+            /**
+             * Have the backing BMI model update to the specified time.
+             *
+             * Update the backing BMI model to some desired model time, specified either explicitly or implicitly as a
+             * non-integral multiple of time steps.  Note that the former is supported, but not required, by the BMI
+             * specification.  The same is true for negative argument values.
+             *
+             * This function does not attempt to determine whether the particular backing model will consider the
+             * provided parameter valid.
+             *
+             * @param time Time to update model to, either as literal model time or non-integral multiple of time steps.
+             */
+            void UpdateUntil(double time) override;
 
         protected:
 
---
+++ src/realizations/catchment/Bmi_Fortran_Adapter.cpp
@@ -0,0 +1,291 @@
+#include "Bmi_Fortran_Adapter.hpp"
+
+using namespace models::bmi;
+
+string Bmi_Fortran_Adapter::GetComponentName() {
+    char component_name[BMI_MAX_COMPONENT_NAME];
+    if (get_component_name(bmi_model->handle, component_name) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get model component name.");
+    }
+    return {component_name};
+}
+
+double Bmi_Fortran_Adapter::GetCurrentTime() {
+    double current_time;
+    if (get_current_time(bmi_model->handle, &current_time) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get current model time.");
+    }
+    return current_time;
+}
+
+double Bmi_Fortran_Adapter::GetEndTime() {
+    double end_time;
+    if (get_end_time(bmi_model->handle, &end_time) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get model end time.");
+    }
+    return end_time;
+}
+
+std::vector<std::string> Bmi_Fortran_Adapter::GetInputVarNames() {
+    if (input_var_names == nullptr) {
+        input_var_names = inner_get_variable_names(true);
+    }
+    return *input_var_names;
+}
+
+std::vector<std::string> Bmi_Fortran_Adapter::GetOutputVarNames() {
+    if (output_var_names == nullptr) {
+        output_var_names = inner_get_variable_names(false);
+    }
+    return *output_var_names;
+}
+
+double Bmi_Fortran_Adapter::GetStartTime() {
+    double start_time;
+    if (get_start_time(bmi_model->handle, &start_time) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get model start time.");
+    }
+    return start_time;
+}
+
+double Bmi_Fortran_Adapter::GetTimeStep() {
+    // TODO: go back and revisit this
+    //return *get_bmi_model_time_step_size_ptr();
+    double ts;
+    if (get_time_step(bmi_model->handle, &ts) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get model time step size.");
+    }
+}
+
+std::string Bmi_Fortran_Adapter::GetTimeUnits() {
+    char time_units_cstr[BMI_MAX_UNITS_NAME];
+    if (get_time_units(bmi_model->handle, time_units_cstr) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to read time units from model.");
+    }
+    return {time_units_cstr};
+}
+
+void Bmi_Fortran_Adapter::GetValueAtIndices(std::string name, void *dest, int *inds, int count) {
+    if (get_value_at_indices(bmi_model->handle, name.c_str(), dest, inds, count) != BMI_SUCCESS) {
+        throw models::external::State_Exception(
+                model_name + " failed to get variable " + name + " for " + std::to_string(count) +
+                " indices.");
+    }
+}
+
+int Bmi_Fortran_Adapter::GetVarItemsize(std::string name) {
+    int size;
+    if (get_var_itemsize(bmi_model->handle, name.c_str(), &size) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get variable item size for " + name + ".");
+    }
+    return size;
+}
+
+int Bmi_Fortran_Adapter::GetVarNbytes(std::string name) {
+    int size;
+    int success = get_var_nbytes(bmi_model->handle, name.c_str(), &size);
+    if (success != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get variable array size (i.e., nbytes) for " + name + ".");
+    }
+    return size;
+}
+
+std::string Bmi_Fortran_Adapter::GetVarType(std::string name) {
+    char type_c_str[BMI_MAX_TYPE_NAME];
+    int success = get_var_type(bmi_model->handle, name.c_str(), type_c_str);
+    if (success != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get variable type for " + name + ".");
+    }
+    return std::string(type_c_str);
+}
+
+std::string Bmi_Fortran_Adapter::GetVarUnits(std::string name) {
+    char units_c_str[BMI_MAX_UNITS_NAME];
+    int success = get_var_units(bmi_model->handle, name.c_str(), units_c_str);
+    if (success != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get variable units for " + name + ".");
+    }
+    return std::string(units_c_str);
+}
+
+std::string Bmi_Fortran_Adapter::GetVarLocation(std::string name) {
+    char location_c_str[BMI_MAX_LOCATION_NAME];
+    int success = get_var_location(bmi_model->handle, name.c_str(), location_c_str);
+    if (success != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get variable location for " + name + ".");
+    }
+    return std::string(location_c_str);
+}
+
+int Bmi_Fortran_Adapter::GetVarGrid(std::string name) {
+    int grid;
+    int success = get_var_grid(bmi_model->handle, name.c_str(), &grid);
+    if (success != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get variable grid for " + name + ".");
+    }
+    return grid;
+}
+
+std::string Bmi_Fortran_Adapter::GetGridType(int grid_id) {
+    char gridtype_c_str[BMI_MAX_TYPE_NAME];
+    int success = get_grid_type(bmi_model->handle, grid_id, gridtype_c_str);
+    if (success != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid type for grid ID " + to_string(grid_id) + ".");
+    }
+    return std::string(gridtype_c_str);
+}
+
+int Bmi_Fortran_Adapter::GetGridRank(int grid_id) {
+    int gridrank;
+    int success = get_grid_rank(bmi_model->handle, grid_id, &gridrank);
+    if (success != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid rank for grid ID " + to_string(grid_id) + ".");
+    }
+    return gridrank;
+}
+
+int Bmi_Fortran_Adapter::GetGridSize(int grid_id) {
+    int gridsize;
+    int success = get_grid_size(bmi_model->handle, grid_id, &gridsize);
+    if (success != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid size for grid ID " + to_string(grid_id) + ".");
+    }
+    return gridsize;
+}
+
+void Bmi_Fortran_Adapter::SetValue(std::string name, void *src) {
+    int result = set_value(bmi_model->handle, name.c_str(), src);
+    if (result != BMI_SUCCESS) {
+        throw models::external::State_Exception("Failed to set values of " + name + " variable for " + model_name);
+    }
+}
+
+bool Bmi_Fortran_Adapter::is_model_initialized() {
+    return model_initialized;
+}
+
+void Bmi_Fortran_Adapter::SetValueAtIndices(std::string name, int *inds, int count, void *src) {
+    int result = set_value_at_indices(bmi_model->handle, name.c_str(), inds, count, src);
+    if (result != BMI_SUCCESS) {
+        throw models::external::State_Exception(
+                "Failed to set specified indexes for " + name + " variable of " + model_name);
+    }
+}
+
+/**
+ * Have the backing model update to next time step.
+ *
+ * Have the backing BMI model perform an update to the next time step according to its own internal time keeping.
+ */
+void Bmi_Fortran_Adapter::Update() {
+    int result = update(bmi_model->handle);
+    if (result != BMI_SUCCESS) {
+        throw models::external::State_Exception("BMI C model execution update failed for " + model_name);
+    }
+}
+
+/**
+ * Have the backing BMI model update to the specified time.
+ *
+ * Update the backing BMI model to some desired model time, specified either explicitly or implicitly as a
+ * non-integral multiple of time steps.  Note that the former is supported, but not required, by the BMI
+ * specification.  The same is true for negative argument values.
+ *
+ * This function does not attempt to determine whether the particular backing model will consider the
+ * provided parameter valid.
+ *
+ * @param time Time to update model to, either as literal model time or non-integral multiple of time steps.
+ */
+void Bmi_Fortran_Adapter::UpdateUntil(double time) {
+    int result = update_until(bmi_model->handle, time);
+    if (result != BMI_SUCCESS) {
+        throw models::external::State_Exception("Model execution update to specified time failed for " + model_name);
+    }
+}
+
+void Bmi_Fortran_Adapter::GetGridShape(const int grid, int *shape) {
+    if (get_grid_shape(bmi_model->handle, grid, shape) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " shape.");
+    }
+}
+
+void Bmi_Fortran_Adapter::GetGridSpacing(const int grid, double *spacing) {
+    if (get_grid_spacing(bmi_model->handle, grid, spacing) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " spacing.");
+    }
+}
+
+void Bmi_Fortran_Adapter::GetGridOrigin(const int grid, double *origin) {
+    if (get_grid_origin(bmi_model->handle, grid, origin) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " origin.");
+    }
+}
+
+void Bmi_Fortran_Adapter::GetGridX(const int grid, double *x) {
+    if (get_grid_x(bmi_model->handle, grid, x) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " x.");
+    }
+}
+
+void Bmi_Fortran_Adapter::GetGridY(const int grid, double *y) {
+    if (get_grid_y(bmi_model->handle, grid, y) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " y.");
+    }
+}
+
+void Bmi_Fortran_Adapter::GetGridZ(const int grid, double *z) {
+    if (get_grid_z(bmi_model->handle, grid, z) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " z.");
+    }
+}
+
+int Bmi_Fortran_Adapter::GetGridNodeCount(const int grid) {
+    int count;
+    if (get_grid_node_count(bmi_model->handle, grid, &count) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " node count.");
+    }
+    return count;
+}
+
+int Bmi_Fortran_Adapter::GetGridEdgeCount(const int grid) {
+    int count;
+    if (get_grid_edge_count(bmi_model->handle, grid, &count) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " edge count.");
+    }
+    return count;
+}
+
+int Bmi_Fortran_Adapter::GetGridFaceCount(const int grid) {
+    int count;
+    if (get_grid_face_count(bmi_model->handle, grid, &count) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " face count.");
+    }
+    return count;
+}
+
+void Bmi_Fortran_Adapter::GetGridEdgeNodes(const int grid, int *edge_nodes) {
+    if (get_grid_edge_nodes(bmi_model->handle, grid, edge_nodes) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " edge nodes.");
+    }
+}
+
+void Bmi_Fortran_Adapter::GetGridFaceEdges(const int grid, int *face_edges) {
+    if (get_grid_face_edges(bmi_model->handle, grid, face_edges) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " face edges.");
+    }
+}
+
+void Bmi_Fortran_Adapter::GetGridFaceNodes(const int grid, int *face_nodes) {
+    if (get_grid_face_nodes(bmi_model->handle, grid, face_nodes) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " face nodes.");
+    }
+}
+
+void Bmi_Fortran_Adapter::GetGridNodesPerFace(const int grid, int *nodes_per_face) {
+    if (get_grid_nodes_per_face(bmi_model->handle, grid, nodes_per_face) != BMI_SUCCESS) {
+        throw std::runtime_error(model_name + " failed to get grid " + std::to_string(grid) + " nodes per face.");
+    }
+}
+
+
+
