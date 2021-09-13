7ca069552e43d63a37668cdeca297fa52f34af49
Robert Bartel <robert.bartel@noaa.gov>
2021-09-13 09:48:32 -0500

    Start fix parallel hydrofabric subdivide, tools.
    Adding MPI send and recv funcs for files, and starting work to improve
    the hydrofabric subdividing to have all files generated by one rank,
    because otherwise the subdividing routing (in Python) has to load the
    hydrofabric in each processs again, defeating the purpose.

--- include/utilities/parallel_utils.h
+++ include/utilities/parallel_utils.h
@@ -11,8 +11,10 @@
 #define MPI_HF_SUB_CODE_BAD 1
 #endif
 
+#include <cstring>
 #include <mpi.h>
 #include <string>
+#include <set>
 #ifdef ACTIVATE_PYTHON
 #include "PyHydrofabricSubsetter.hpp"
 #endif // ACTIVATE_PYTHON
@@ -129,6 +131,75 @@ namespace parallel {
         return is_hydrofabric_subdivided(mpi_rank, mpi_num_procs, false);
     }
 
+    bool distribute_subdivided_hydrofabric_files() {
+
+    }
+
+    /**
+     * Set each rank's host "id" value in a provided host array.
+     *
+     * This function has the ranks communicate their hostname back to rank 0.  Rank 0 then builds an array of unique
+     * host id values.  The value at index ``x`` in this array is the id for the host of rank ``x``.
+     *
+     * The id of the host of rank 0 is ``0``, but otherwise there is no deterministic ordering of id values for hosts.
+     *
+     * This array is then broadcast by rank 0 back to to the other ranks, allowing them to set their analogous arrays.
+     * It is expected all ranks run this function at the same time.
+     *
+     * @param mpi_rank The current rank.
+     * @param mpi_num_procs The number of ranks.
+     * @param host_array A pointer to an allocated array of size ``mpi_num_procs``.
+     */
+    void get_hosts_array(int mpi_rank, int mpi_num_procs, int *host_array) {
+        // Things should never be longer than this
+        int hostnameBufferSize = 256;
+        char hostnames[mpi_num_procs][hostnameBufferSize];
+        // These are the lengths of the (trimmed) C-string representations of the hostname for each rank
+        int actualHostnameCStrLength[mpi_num_procs];
+        // Initialize to -1 to represent unknown
+        for (int i = 0; i < mpi_num_procs; ++i) {
+            actualHostnameCStrLength[i] = -1;
+        }
+
+        // Get this rank's hostname
+        gethostname(hostnames[mpi_rank], hostnameBufferSize);
+        // Set the one for this rank
+        actualHostnameCStrLength[mpi_rank] = std::strlen(hostnames[mpi_rank]);
+
+        if (mpi_rank != 0) {
+            // First, send how long the string is
+            MPI_Send(&actualHostnameCStrLength[mpi_rank], 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(hostnames[mpi_rank], actualHostnameCStrLength[mpi_rank], MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+        }
+        // In rank 0, get the other hostnames
+        else {
+            host_array[0] = 0;
+            int next_host_id = 1;
+
+            for (int recv_rank = 1; recv_rank < mpi_num_procs; ++recv_rank) {
+                MPI_Recv(&actualHostnameCStrLength[recv_rank], 1, MPI_INT, recv_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(hostnames[recv_rank], actualHostnameCStrLength[recv_rank], MPI_CHAR, recv_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                int matches_host_at = -1;
+
+                for (int known_rank = 0; known_rank < recv_rank; ++known_rank) {
+                    if (std::strcmp(hostnames[known_rank], hostnames[recv_rank]) == 0) {
+                        matches_host_at = known_rank;
+                        break;
+                    }
+                }
+                if (matches_host_at == -1) {
+                    // Assign new host id, then increment what the next id will be
+                    host_array[recv_rank] = next_host_id++;
+                }
+                else {
+                    host_array[recv_rank] = host_array[matches_host_at];
+                }
+            }
+        }
+        // Now, broadcast the results out
+        MPI_Bcast(host_array, mpi_num_procs, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+
     /**
      * Attempt to subdivide the passed hydrofabric files into a series of per-partition files.
      *
@@ -151,45 +222,179 @@ namespace parallel {
         bool isGood = true;
 
         #ifdef ACTIVATE_PYTHON
+        // Have rank 0 handle the generation task for all files/partitions
+        if (mpi_rank == 0) {
+            std::unique_ptr<utils::PyHydrofabricSubsetter> subdivider;
+            try {
+                subdivider = std::make_unique<utils::PyHydrofabricSubsetter>(catchmentDataFile, nexusDataFile,
+                                                                             partitionConfigFile);
+            }
+            catch (const std::exception &e) {
+                std::cerr << e.what() << std::endl;
+                // Set not good if the subdivider object couldn't be instantiated
+                isGood = false;
+            }
+            // Sync with the rest of the ranks and bail if any aren't ready to proceed for any reason
+            if (!mpiSyncStatusAnd(isGood, mpi_rank, mpi_num_procs, "initializing hydrofabric subdivider")) {
+                return false;
+            }
 
-        // For now just have this be responsible for its own rank file
-        // Later consider whether it makes more sense for one rank (per host) to write all files
-        std::unique_ptr<utils::PyHydrofabricSubsetter> subdivider;
-        try {
-            subdivider = std::make_unique<utils::PyHydrofabricSubsetter>(catchmentDataFile, nexusDataFile,
-                                                                         partitionConfigFile);
-        }
-        catch (const std::exception &e) {
-            std::cerr << e.what() << std::endl;
-            // Set not good if the subdivider object couldn't be instantiated
-            isGood = false;
-        }
-        // Sync with the rest of the ranks and bail if any aren't ready to proceed for any reason
-        if (!mpiSyncStatusAnd(isGood, mpi_rank, mpi_num_procs, "initializing hydrofabric subdivider")) {
-            return false;
-        }
-
-        // Try to perform the subdividing (for now, have each rank handle its own file)
-        try {
-            isGood = subdivider->execSubdivision(mpi_rank);
-        }
-        catch (const std::exception &e) {
-            std::cerr << e.what() << std::endl;
-            // Set not good if the subdivider object couldn't be instantiated
-            isGood = false;
+            // Try to perform the subdividing
+            try {
+                isGood = subdivider->execSubdivision();
+            }
+            catch (const std::exception &e) {
+                std::cerr << e.what() << std::endl;
+                // Set not good if the subdivider object couldn't be instantiated
+                isGood = false;
+            }
         }
 
         #else // i.e., ifndef ACTIVATE_PYTHON
         // Without Python available, there is no way external subdivide package can be used (i.e., not good)
         isGood = false;
-        std::cerr
-                << "Driver is unable to perform required hydrofabric subdividing when Python integration is not active."
-                << std::endl;
+        std::cerr << "Driver is unable to perform required hydrofabric subdividing when Python integration is not active." << std::endl;
         #endif // ACTIVATE_PYTHON
 
-        // Now sync ranks on whether the subdividing function was executed successfully, and return
-        return mpiSyncStatusAnd(isGood, mpi_rank, mpi_num_procs, "executing hydrofabric subdivision");
+        // Now sync ranks on whether the subdividing function was executed successfully
+        // If not, meaning rank 0 couldn't do subdividing, then have all ranks exit at this point
+        if (!mpiSyncStatusAnd(isGood, mpi_rank, mpi_num_procs, "executing hydrofabric subdivision")) {
+            return false;
+        }
+        // But if the subdividing went fine, then we need to work out (possibly) transferring files around
+        else {
+            // Figure out which ranks are and are not on the same host as rank 0
+            int hostArray[mpi_num_procs];
+            get_hosts_array(mpi_rank, mpi_num_procs, hostArray);
+            // FIXME: For now, just have rank 0 send everything, but optimize with multiple procs or threads later
+            // Only the transmitting (i.e., rank 0) and receiving ranks (not the same host as rank 0) need to do this
+            if (mpi_rank == 0 || hostArray[mpi_rank] != hostArray[0]) {
+                int bufSize = 4096;
+                char buf[bufSize];
+
+                if (mpi_rank == 0) {
+                    for (int i = 1; i < mpi_num_procs; ++i) {
+                        if (hostArray[i] != hostArray[0]) {
+                            // TODO: transmit from rank 0 to rank i the i-th catchment and nexus files
+                            asdf
+                        }
+                    }
+                }
+                else {
+                    // TODO: receive the transmitted file
+                }
+            }
+
+            // TODO: barrier for all
+
+        }
     }
+}
+
+bool mpi_send_file(char *fileName, int mpi_rank, int destRank) {
+    int bufSize = 4096;
+    char buf[bufSize];
+    int code;
+    // How much has been transferred so far
+    int totalNumTransferred = 0;
+
+    FILE *file = fopen(fileName, "r");
+
+    // Transmit error code instead of expected size and return false if file can't be opened
+    if (file == NULL) {
+        // TODO: output error message
+        code = -1;
+        MPI_Send(&code, 1, MPI_INT, destRank, 0, MPI_COMM_WORLD);
+        return false;
+    }
+
+    // Send expected size to start
+    MPI_Send(&bufSize, 1, MPI_INT, destRank, 0, MPI_COMM_WORLD);
+
+    // Then get back expected size to infer other side is good to go
+    MPI_Recv(&code, 1, MPI_INT, destRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    if (code != bufSize) {
+        // TODO: output error message
+        fclose(file);
+        return false;
+    }
+
+    // Then while there is more of the file to read and send, read the next batch and ...
+    while (fgets(buf, bufSize, file) != NULL) {
+        // First send this batch
+        MPI_Send(buf, bufSize, MPI_CHAR, destRank, 0, MPI_COMM_WORLD);
+        // Then get back a code, which will be -1 if bad and need to exit and otherwise good
+        MPI_Recv(&code, 1, MPI_INT, destRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (code == -1) {
+            // TODO: output error message
+            fclose(file);
+            return false;
+        }
+    }
+    // Once there is no more file to read and send, send an empty string
+    MPI_Send("", bufSize, MPI_CHAR, destRank, 0, MPI_COMM_WORLD);
+    // Expect to get back a code of 0
+    MPI_Recv(&code, 1, MPI_INT, destRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    fclose(file);
+    return code == 0;
+}
+
+bool mpi_recv_file(char *fileName, int mpi_rank, int srcRank) {
+    int bufSize, strLength, writeCode;
+    // Receive expected buffer size to start
+    MPI_Recv(&bufSize, 1, MPI_INT, srcRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    // If the sending side couldn't open the file, then immediately return false
+    if (bufSize == -1) {
+        // TODO: output error
+        return false;
+    }
+
+    // Try to open recv file ...
+    FILE *file = fopen(fileName, "w");
+    // ... and let sending size know whether this was successful by sending error code if not ...
+    if (file == NULL) {
+        // TODO: output error message
+        bufSize = -1;
+        MPI_Send(&bufSize, 1, MPI_INT, srcRank, 0, MPI_COMM_WORLD);
+        return false;
+    }
+
+    // Send back the received buffer it if file opened, confirming things are good to go for transfer
+    MPI_Send(&bufSize, 1, MPI_INT, srcRank, 0, MPI_COMM_WORLD);
+
+    // How much has been transferred so far
+    int totalNumTransferred = 0;
+    char buf[bufSize];
+
+    // While the last received string batch contained something
+    while (true) {
+        MPI_Recv(buf, bufSize, MPI_CHAR, srcRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        strLength = std::strlen(buf);
+        // As long as the
+        if (strLength > 0) {
+
+            // Write to file
+            writeCode = fputs(buf, file);
+            if (writeCode >= 0) {
+                MPI_Send(&strLength, 1, MPI_INT, srcRank, 0, MPI_COMM_WORLD);
+            }
+            else {
+                MPI_Send(&writeCode, 1, MPI_INT, srcRank, 0, MPI_COMM_WORLD);
+                fclose(file);
+                return false;
+            }
+        }
+        else {
+            // When finished ...
+            fclose(file);
+            // Also send back understood finished code of 0
+            MPI_Send(&strLength, 1, MPI_INT, srcRank, 0, MPI_COMM_WORLD);
+            break;
+        }
+    }
+
+    return true;
 }
 
 #endif // NGEN_MPI_ACTIVE
