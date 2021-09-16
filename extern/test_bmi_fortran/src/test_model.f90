b883dac223c2c40a396bf858a52135346a9b11c9
Nels Frazier <nels.frazier@noaa.gov>
2021-09-16 09:52:06 -0500

    make sure output_var_3 in model gets some value, just in case

--- extern/test_bmi_fortran/src/test_model.f90
+++ extern/test_bmi_fortran/src/test_model.f90
@@ -24,9 +24,11 @@ module test_model
         if( dt == model%time_step_size) then
             model%output_var_1 = model%input_var_1
             model%output_var_2 = 2.0 * model%input_var_2
+            model%output_var_3 = 0
         else
             model%output_var_1 = model%input_var_1 * dt / model%time_step_size
             model%output_var_2 = 2.0 * model%input_var_2 * dt / model%time_step_size
+            model%output_var_3 = 0
         end if
 
         model%current_model_time = model%current_model_time + dt
