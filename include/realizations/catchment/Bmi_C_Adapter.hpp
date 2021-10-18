ff3747b8475ad83b0c328eea23b8d5ad3c109b7f
Robert Bartel <robert.bartel@noaa.gov>
2021-10-18 09:55:25 -0500

    Add Bmi_C_Adapter get_analogous_cxx_type.
    Adding C adapter's definition for this new interface function.

--- include/realizations/catchment/Bmi_C_Adapter.hpp
+++ include/realizations/catchment/Bmi_C_Adapter.hpp
@@ -387,6 +387,28 @@ namespace models {
             int GetGridSize(int grid_id) override;
 
             /**
+             * Get the name string for the C++ type analogous to the described type in the backing model.
+             *
+             * The supported languages for BMI modules support different types than C++ in general, so it is necessary
+             * to map "external" types to the appropriate C++ type for certain interactions; e.g., setting a variable
+             * value.
+             *
+             * However, for C language models, the types in C can be used directly, so this simply returns the original
+             * string for the external name.
+             *
+             * Note that the size of an individual item is also required, as this can vary in certain situations
+             * depending on the implementation language of a backing model.
+             *
+             * @param external_type_name The string name of the analog type in Python.
+             * @param item_size The particular size in bytes for items of the involved analogous types.
+             * @return The name string for the analogous C++ type, which is the same as given arg for C modules.
+             */
+            const std::string
+            get_analogous_cxx_type(const std::string &external_type_name, const size_t item_size) override {
+                return external_type_name;
+            }
+
+            /**
              * Whether the backing model has been initialized yet.
              *
              * @return Whether the backing model has been initialized yet.
