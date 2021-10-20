352131037bc385f813cad1dca0ddfedc83a18392
Matt Williamson <87771120+mattw-nws@users.noreply.github.com>
2021-10-20 18:34:40 +0000

    Improved tests + cleanup

--- test/forcing/CsvPerFeatureForcingProvider_Test.cpp
+++ test/forcing/CsvPerFeatureForcingProvider_Test.cpp
@@ -21,13 +21,14 @@ class CsvPerFeatureForcingProviderTest : public ::testing::Test {
 
     void setupForcing();
 
-    std::shared_ptr<CsvPerFeatureForcingProvider> Forcing_Object; //smart pointer to a Forcing object
+    std::shared_ptr<CsvPerFeatureForcingProvider> Forcing_Object;
+    std::shared_ptr<CsvPerFeatureForcingProvider> Forcing_Object_2;
 
     typedef struct tm time_type;
 
-    std::shared_ptr<time_type> start_date_time; //smart pointer to time struct
+    std::shared_ptr<time_type> start_date_time;
 
-    std::shared_ptr<time_type> end_date_time; //smart pointer to time struct
+    std::shared_ptr<time_type> end_date_time;
 
 };
 
@@ -47,15 +48,26 @@ void CsvPerFeatureForcingProviderTest::TearDown() {
 void CsvPerFeatureForcingProviderTest::setupForcing()
 {
     std::vector<std::string> forcing_file_names = { 
-        "test/forcing/cat-10_2015-12-01 00_00_00_2015-12-30 23_00_00.csv",
-        "../test/forcing/cat-10_2015-12-01 00_00_00_2015-12-30 23_00_00.csv",
-        "../../test/forcing/cat-10_2015-12-01 00_00_00_2015-12-30 23_00_00.csv"
+        "test/data/forcing/cat-10_2015-12-01 00_00_00_2015-12-30 23_00_00.csv",
+        "../test/data/forcing/cat-10_2015-12-01 00_00_00_2015-12-30 23_00_00.csv",
+        "../../test/data/forcing/cat-10_2015-12-01 00_00_00_2015-12-30 23_00_00.csv"
         };
     std::string forcing_file_name = utils::FileChecker::find_first_readable(forcing_file_names);
 
     forcing_params forcing_p(forcing_file_name, "2015-12-14 21:00:00", "2015-12-30 23:00:00");
 
     Forcing_Object = std::make_shared<CsvPerFeatureForcingProvider>(forcing_p);
+
+    forcing_file_names = { 
+        "test/data/forcing/cat-27115-nwm-aorc-variant-derived-format.csv",
+        "../test/data/forcing/cat-27115-nwm-aorc-variant-derived-format.csv",
+        "../../test/data/forcing/cat-27115-nwm-aorc-variant-derived-format.csv"
+        };
+    forcing_file_name = utils::FileChecker::find_first_readable(forcing_file_names);
+
+    forcing_params forcing_p_2(forcing_file_name, "2007-01-01 00:00:00", "2007-01-05 02:00:00");
+
+    Forcing_Object_2 = std::make_shared<CsvPerFeatureForcingProvider>(forcing_p_2);
 }
 
 ///Test AORC Forcing Object
@@ -90,4 +102,32 @@ TEST_F(CsvPerFeatureForcingProviderTest, TestForcingDataRead)
     current_precipitation = Forcing_Object->get_value(CSDMS_STD_NAME_RAIN_RATE, begin+(i*3600), 3600, "");
 
     EXPECT_NEAR(current_precipitation, 6.9999999999999996e-07, 0.00000005);
+}
+
+TEST_F(CsvPerFeatureForcingProviderTest, TestForcingDataReadAltFormat)
+{
+    double current_precipitation;
+
+    time_t begin = Forcing_Object_2->get_forcing_output_time_begin("");
+
+    int current_day_of_year;
+    int i = 8;
+    time_t t = begin+(i*3600);
+    std::cerr << std::ctime(&t) << std::endl;
+
+    current_precipitation = Forcing_Object_2->get_value(CSDMS_STD_NAME_RAIN_RATE, begin+(i*3600), 3600, "");
+
+    EXPECT_NEAR(current_precipitation, 0.00032685, 0.00000001);
+
+    double temp_k = Forcing_Object_2->get_value(CSDMS_STD_NAME_SURFACE_TEMP, begin+(i*3600), 3600, "");
+
+    EXPECT_NEAR(temp_k, 265.77, 0.00001);
+
+    int current_epoch;
+
+    i = 34;
+    current_precipitation = Forcing_Object_2->get_value(CSDMS_STD_NAME_RAIN_RATE, begin+(i*3600), 3600, "");
+
+    EXPECT_NEAR(current_precipitation, 0.00013539, 0.00000001);
+
 }
