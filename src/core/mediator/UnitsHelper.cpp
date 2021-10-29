ada1f3634b5f1f9bc50622ef104221134ec70ce4
Matt Williamson <87771120+mattw-nws@users.noreply.github.com>
2021-10-29 17:15:29 +0000

    Less naive more performant...and buld structure

---
+++ src/core/mediator/UnitsHelper.cpp
@@ -0,0 +1,32 @@
+#include "UnitsHelper.hpp"
+
+ut_system* UnitsHelper::unit_system;
+
+
+double UnitsHelper::get_converted_value(const std::string &in_units, double value, const std::string &out_units)
+{
+    init_unit_system();
+    ut_unit* from = ut_parse(unit_system, in_units.c_str(), UT_UTF8);
+    if (from == NULL)
+    {
+        throw std::runtime_error("Unable to parse in_units value " + in_units);
+    }
+    ut_unit* to = ut_parse(unit_system, out_units.c_str(), UT_UTF8);
+    if (to == NULL)
+    {
+        ut_free(from);
+        throw std::runtime_error("Unable to parse out_units value " + out_units);
+    }
+    cv_converter* conv = ut_get_converter(from, to);
+    if (conv == NULL)
+    {
+        ut_free(from);
+        ut_free(to);
+        throw std::runtime_error("Unable to convert " + in_units + " to " + out_units);
+    }
+    double r = cv_convert_double(conv, value);
+    ut_free(from);
+    ut_free(to);
+    cv_free(conv);
+    return r;
+}
