d966c70292721ca92a013359a5354d83c4f7791e
Matt Williamson <87771120+mattw-nws@users.noreply.github.com>
2021-11-09 08:10:26 -0500

    NGEN_UDUNITS2_XML_PATH no longer necessary

--- include/core/mediator/UnitsHelper.hpp
+++ include/core/mediator/UnitsHelper.hpp
@@ -16,7 +16,11 @@ class UnitsHelper {
     static ut_system* unit_system;
     static std::once_flag unit_system_inited;
     static void init_unit_system(){
+        #ifdef NGEN_UDUNITS2_XML_PATH
         unit_system = ut_read_xml(NGEN_UDUNITS2_XML_PATH);
+        #else
+        unit_system = ut_read_xml(NULL);
+        #endif
         if (unit_system == NULL) 
         {
             throw std::runtime_error("Unable to create UDUNITS2 Unit System.");
