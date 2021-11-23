1a71d17ac2883df71e425201208eaf87d2cd1a03
Robert Bartel <robert.bartel@noaa.gov>
2021-11-23 10:45:21 -0600

    Refactoring DeferredWrappedProvider.
    Changing use of "values" to "outputs" where appropriate.

--- include/forcing/DeferredWrappedProvider.hpp
+++ include/forcing/DeferredWrappedProvider.hpp
@@ -14,17 +14,18 @@ namespace forcing {
      * A specialized @WrappedForcingProvider that is created without first knowing the backing source it wraps.
      *
      * This type wraps another @ref ForcingProvider, similarly to its parent.  This is "optimistic," however, in that it
-     * is constructed without the backing data source it will wrap.  It only requires the data value names it expects to
-     * eventually be able to provide.
+     * is constructed without the backing data source it will wrap.  It only requires the data output names it expects
+     * to eventually be able to provide.
      *
-     * This type allows for deferring reconciling of whether a provider for some data property is available.  This is
+     * This type allows for deferring reconciling of whether a provider for some data output is available.  This is
      * useful for situations when a required provider is not current known, but is expected, and there will be ample
      * time to validate the expectation and make the necessary associations before the data must be provided.
      *
      * Note that this type does not alter the behavior of inherited functions, except for
-     * @ref get_available_forcing_outputs.  It will only list outputs guaranteed to be provideable by it, but it still
-     * defers to the wrapped instance completely for other functions.  This means the wrapped type will control behavior
-     * of cases when some unknown value name is given.
+     * @ref get_available_forcing_outputs.  That particular function is implemented to only return outputs guaranteed to
+     * be provideable by the instance.  Instances of this type otherwise defer to the wrapped, backing provider object
+     * and its function implementations.  This means the backing wrapped object's type control behavior of cases when
+     * some unknown value name is given.
      */
     class DeferredWrappedProvider : public WrappedForcingProvider {
     public:
@@ -32,17 +33,17 @@ namespace forcing {
         /**
          * Constructor for instance.
          *
-         * @param providedValues The collection of the names of values this instance will need to provide.
+         * @param providedOutputs The collection of the names of outputs this instance will need to provide.
          */
-        explicit DeferredWrappedProvider(vector<string> providedValues) : WrappedForcingProvider(nullptr), providedValues(std::move(providedValues)) { }
+        explicit DeferredWrappedProvider(vector<string> providedOutputs) : WrappedForcingProvider(nullptr), providedOutputs(std::move(providedOutputs)) { }
 
         /**
-         * Convenience constructor for when there is only one provided property name.
+         * Convenience constructor for when there is only one provided output name.
          *
-         * @param valueName The name of the single value this instance will need to provide.
+         * @param outputName The name of the single output this instance will need to provide.
          */
-        explicit DeferredWrappedProvider(const string& valueName) : DeferredWrappedProvider(vector<string>(1)) {
-            providedValues[0] = valueName;
+        explicit DeferredWrappedProvider(const string& outputName) : DeferredWrappedProvider(vector<string>(1)) {
+            providedOutputs[0] = outputName;
         }
 
         /**
@@ -51,21 +52,21 @@ namespace forcing {
          * @param provider_to_move
          */
         DeferredWrappedProvider(DeferredWrappedProvider &&provider_to_move) noexcept
-            : DeferredWrappedProvider(provider_to_move.providedValues)
+            : DeferredWrappedProvider(provider_to_move.providedOutputs)
         {
             wrapped_provider = provider_to_move.wrapped_provider;
             provider_to_move.wrapped_provider = nullptr;
         }
 
         /**
-         * Get the names of the outputs for which this instance is (or will be) able to provide values.
+         * Get the names of the outputs that this instance is (or will be) able to provide.
          *
          * Note that this function behaves the same regardless of whether the wrapped provider has been set.
          *
          * @return The names of the outputs for which this instance is (or will be) able to provide values.
          */
         const std::vector<std::string> &get_available_forcing_outputs() override {
-            return providedValues;
+            return providedOutputs;
         }
 
         /**
@@ -93,10 +94,10 @@ namespace forcing {
         }
 
         /**
-      * Get whether the backing provider this instance wraps has been set yet.
-      *
-      * @return Whether the backing provider this instance wraps has been set yet.
-      */
+         * Get whether the backing provider this instance wraps has been set yet.
+         *
+         * @return Whether the backing provider this instance wraps has been set yet.
+         */
         inline bool isWrappedProviderSet() {
             return wrapped_provider != nullptr;
         }
@@ -111,7 +112,7 @@ namespace forcing {
          * There are several invalid argument cases:
          *
          * - an arg that is actually a null pointer
-         * - an arg that points to a provider that does not itself provide all the required values this must provide
+         * - an arg that points to a provider that does not itself provide all the required outputs this must provide
          * - any arg if there has already been a valid provider set
          *
          * @param provider A pointer for the wrapped provider.
@@ -120,19 +121,19 @@ namespace forcing {
         virtual bool setWrappedProvider(ForcingProvider *provider) {
             // Disallow re-setting the provider if already validly set
             if (isWrappedProviderSet()) {
-                setMessage = "Cannot change wrapped provider after a valid provide has already been set";
+                setMessage = "Cannot change wrapped provider after a valid provider has already been set";
                 return false;
             }
 
             // Confirm this will provide anything
             if (provider == nullptr) {
-                setMessage = "Cannot set provider as null, as this cannot provide the values this type must proxy";
+                setMessage = "Cannot set provider as null, as this cannot provide the outputs this type must proxy";
                 return false;
             }
 
             // Confirm this will provide everything needed
             const vector<string> &available = provider->get_available_forcing_outputs();
-            for (const string &requiredName : providedValues) {
+            for (const string &requiredName : providedOutputs) {
                 if (std::find(available.begin(), available.end(), requiredName) == available.end()) {
                     setMessage = "Given provider does not provide the required " + requiredName;
                     return false;
@@ -146,8 +147,8 @@ namespace forcing {
         }
 
     protected:
-        /** The collection of names of the values this type can/will be able to provide from its wrapped source. */
-        vector<string> providedValues;
+        /** The collection of names of the outputs this type can/will be able to provide from its wrapped source. */
+        vector<string> providedOutputs;
 
         /**
          * A message providing information from the last @ref setWrappedProvider call.
