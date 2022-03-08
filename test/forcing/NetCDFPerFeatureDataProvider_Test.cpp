bf3838f562cd426459cfec59f94771206061b5e9
Donald Johnson <Donald.W.Johnson@noaa.gov>
2022-03-08 17:37:46 +0000

    Added code to build extrac ids from netcdf file and create the map of id->offset. Added stand alone test for netcdf forcing.

--- test/forcing/NetCDFPerFeatureDataProvider_Test.cpp
+++ test/forcing/NetCDFPerFeatureDataProvider_Test.cpp
@@ -58,6 +58,21 @@ void NetCDFPerFeatureDataProviderTest::setupForcing()
 TEST_F(NetCDFPerFeatureDataProviderTest, TestForcingDataRead)
 {
     std::shared_ptr<NetCDFPerFeatureDataProvider> nc_provider = std::make_shared<NetCDFPerFeatureDataProvider>("/local/ngen/data/huc01/huc_01/forcing/netcdf/huc01.nc");
+
+    // check to see that the variable "T2D" exists
+    auto var_names = nc_provider->get_avaliable_variable_names();
+    auto pos = std::find(var_names.begin(), var_names.end(), "T2D");
+
+    if ( pos != var_names.end() )
+    {
+        std::cout << "Found variable T2D\n";
+    }
+    else
+    {
+        std::cout << "The variable \"T2D\" is missing\n";
+        FAIL();
+    }
+
     
     //double val = Forcing_Object->get_value("cat-10095","RAINRATE",0,1,"mm/h");
     //EXPECT_NEAR(current_precipitation, 6.9999999999999996e-07, 0.00000005);
