2362422e1b0f5bcb667af3b615738cd0445b2c8c
Nels Frazier <nels.frazier@noaa.gov>
2022-03-09 15:13:29 +0000

    adjust C formulation unit tests for new input data

--- test/realizations/catchments/Bmi_C_Formulation_Test.cpp
+++ test/realizations/catchments/Bmi_C_Formulation_Test.cpp
@@ -275,7 +275,7 @@ TEST_F(Bmi_C_Formulation_Test, GetResponse_0_b) {
     for (int i = 0; i < 39; i++) {
         response = formulation.get_response(i, 3600);
     }
-    double expected = 4.866464273262429e-08;
+    double expected = 2.7809780039160068e-08;
     ASSERT_EQ(expected, response);
 }
 
@@ -336,7 +336,7 @@ TEST_F(Bmi_C_Formulation_Test, GetOutputLineForTimestep_1_b) {
         formulation.get_response(i++, 3600);
     formulation.get_response(i, 3600);
     std::string output = formulation.get_output_line_for_timestep(i, ",");
-    ASSERT_EQ(output, "0.000000,0.000002");
+    ASSERT_EQ(output, "0.000000,0.000001");
 }
 
 TEST_F(Bmi_C_Formulation_Test, determine_model_time_offset_0_a) {
