fa06a8bf33d95f27f11a649c6d1d6fe047143a70
Robert Bartel <robert.bartel@noaa.gov>
2022-03-14 12:56:20 -0500

    Add ConfigurationException.
    Add custom exception indicating realization config problem.

---
+++ include/utilities/ConfigurationException.hpp
@@ -0,0 +1,36 @@
+#ifndef NGEN_CONFIGURATIONEXCEPTION_HPP
+#define NGEN_CONFIGURATIONEXCEPTION_HPP
+
+#include <exception>
+#include <string>
+#include <utility>
+
+
+namespace realization {
+    /**
+     * Custom exception indicating a problem when integrating with the provided realization configuration.
+     */
+    class ConfigurationException : public std::exception {
+
+    public:
+
+        ConfigurationException(char const *const message) noexcept : ConfigurationException(std::string(message)) {}
+
+        ConfigurationException(std::string message) noexcept : std::exception(), what_message(std::move(message)) {}
+
+        ConfigurationException(ConfigurationException &exception) noexcept : ConfigurationException(exception.what_message) {}
+
+        ConfigurationException(ConfigurationException &&exception) noexcept
+                : ConfigurationException(std::move(exception.what_message)) {}
+
+        virtual char const *what() const noexcept {
+            return what_message.c_str();
+        }
+
+    private:
+
+        std::string what_message;
+    };
+}
+
+#endif //NGEN_CONFIGURATIONEXCEPTION_HPP
