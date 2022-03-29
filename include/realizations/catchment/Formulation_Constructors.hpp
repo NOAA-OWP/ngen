11c95cb4039effde5d1a38f04b278478a1e10abf
Donald Johnson <Donald.W.Johnson@noaa.gov>
2022-03-29 19:22:15 +0000

    Starting to integrate new data provider API.

--- include/realizations/catchment/Formulation_Constructors.hpp
+++ include/realizations/catchment/Formulation_Constructors.hpp
@@ -17,7 +17,8 @@
 #include "Bmi_Fortran_Formulation.hpp"
 #include "Bmi_Multi_Formulation.hpp"
 #include "Bmi_Py_Formulation.hpp"
-#include <ForcingProvider.hpp>
+#include "ForcingProvider.hpp"
+#include <NetCDFPerFeatureDataProvider.hpp>
 #include "CsvPerFeatureForcingProvider.hpp"
 
 #ifdef NGEN_LSTM_TORCH_LIB_ACTIVE
@@ -25,38 +26,56 @@
 #endif
 
 namespace realization {
-    typedef std::shared_ptr<Catchment_Formulation> (*constructor)(std::string, unique_ptr<forcing::ForcingProvider>, utils::StreamHandler);
-
-    template<class T>
-    static constructor create_formulation_constructor() {
-        return [](std::string id, std::unique_ptr<forcing::ForcingProvider> forcing_provider, utils::StreamHandler output_stream) -> std::shared_ptr<Catchment_Formulation>{
+    /*template<class T, class DataProviderType>
+    auto create_formulation_constructor() {
+        return [](std::string id, std::shared_ptr<DataProviderType> forcing_provider, utils::StreamHandler output_stream) -> std::shared_ptr<Catchment_Formulation>{
             return std::make_shared<T>(id, std::move(forcing_provider), output_stream);
         };
-    };
 
-    static std::map<std::string, constructor> formulations = {
+        typedef std::shared_ptr<Catchment_Formulation> (*constructor)(std::string, shared_ptr<DataProviderType>, utils::StreamHandler);
+    };*/
+
+    /*static std::map<std::string, constructor> formulations = {
         {"bmi_c++", create_formulation_constructor<Bmi_Cpp_Formulation>()},
 #ifdef NGEN_BMI_C_LIB_ACTIVE
-        {"bmi_c", create_formulation_constructor<Bmi_C_Formulation>()},
+        {"bmi_c", create_formulation_constructor<Bmi_C_Formulation, CsvPerFeatureForcingProvider>()},
 #endif // NGEN_BMI_C_LIB_ACTIVE
 #ifdef NGEN_BMI_FORTRAN_ACTIVE
-        {"bmi_fortran", create_formulation_constructor<Bmi_Fortran_Formulation>()},
+        {"bmi_fortran", create_formulation_constructor<Bmi_Fortran_Formulation, CsvPerFeatureForcingProvider>()},
 #endif // NGEN_BMI_FORTRAN_ACTIVE
-        {"bmi_multi", create_formulation_constructor<Bmi_Multi_Formulation>()},
+        {"bmi_multi", create_formulation_constructor<Bmi_Multi_Formulation, CsvPerFeatureForcingProvider>()},
 #ifdef ACTIVATE_PYTHON
-        {"bmi_python", create_formulation_constructor<Bmi_Py_Formulation>()},
+        {"bmi_python", create_formulation_constructor<Bmi_Py_Formulation, CsvPerFeatureForcingProvider>()},
 #endif // ACTIVATE_PYTHON
-        {"tshirt", create_formulation_constructor<Tshirt_Realization>()},
+        {"tshirt", create_formulation_constructor<Tshirt_Realization, >()},
         {"tshirt_c", create_formulation_constructor<Tshirt_C_Realization>()},
         {"simple_lumped", create_formulation_constructor<Simple_Lumped_Model_Realization>()}
 #ifdef NGEN_LSTM_TORCH_LIB_ACTIVE
         ,
         {"lstm", create_formulation_constructor<LSTM_Realization>()}
 #endif
-    };
+    }*/
 
+    std::vector<std::string> formulations = {
+#ifdef NGEN_BMI_C_LIB_ACTIVE
+        "bmi_c",
+#endif // NGEN_BMI_C_LIB_ACTIVE
+#ifdef NGEN_BMI_FORTRAN_ACTIVE
+        "bmi_fortran", 
+#endif // NGEN_BMI_FORTRAN_ACTIVE
+        "bmi_multi", 
+#ifdef ACTIVATE_PYTHON
+        "bmi_python", 
+#endif // ACTIVATE_PYTHON
+        "tshirt", 
+        "tshirt_c",
+#ifdef NGEN_LSTM_TORCH_LIB_ACTIVE
+        "lstm", 
+#endif
+        "simple_lumped"
+    };
     static bool formulation_exists(std::string formulation_type) {
-        return formulations.count(formulation_type) > 0;
+        return std::find(formulations.begin(), formulations.end(), formulation_type) != formulations.end();
     }
 
     static std::shared_ptr<Catchment_Formulation> construct_formulation(
@@ -65,15 +84,80 @@ namespace realization {
         forcing_params &forcing_config,
         utils::StreamHandler output_stream
     ) {
-        constructor formulation_constructor = formulations.at(formulation_type);
-        std::unique_ptr<forcing::ForcingProvider> fp;
+        //constructor formulation_constructor = formulations.at(formulation_type);
+        
         if (formulation_type == "tshirt" || formulation_type == "tshirt_c"  || formulation_type == "lstm" // These formulations are still using the legacy interface!
-            || forcing_config.provider == "" || forcing_config.provider == "legacy" // Permit legacy Forcing class with BMI formulations and simple_lumped--don't break old configs
-            ){
+            || forcing_config.provider == "" || forcing_config.provider == "legacy") // Permit legacy Forcing class with BMI formulations and simple_lumped--don't break old configs
+        {
+            std::unique_ptr<Forcing> fp;
             fp = std::make_unique<Forcing>(forcing_config);
+            #ifdef NGEN_BMI_C_LIB_ACTIVE
+            if (formulation_type == "bmi_c" )
+                return std::make_shared<Bmi_C_Formulation>(identifier, std::move(fp), output_stream);
+            #endif
+            #ifdef NGEN_BMI_FORTRAN_ACTIVE
+            if (formulation_type == "bmi_fortran" )
+                return std::make_shared<Bmi_Fortran_Formulation>(identifier, std::move(fp), output_stream);
+            #endif
+            if (formulation_type == "bmi_multi" )
+                return std::make_shared<Bmi_Multi_Formulation>(identifier, std::move(fp), output_stream);
+            #ifdef ACTIVATE_PYTHON
+            if (formulation_type == "bmi_python" )
+                return std::make_shared<Bmi_Py_Formulation>(identifier, std::move(fp), output_stream);
+            #endif
+            #ifdef NGEN_LSTM_TORCH_LIB_ACTIV
+            if (formulation_type == "lstm" )
+                return std::make_shared<LSTM_Realization>(identifier, std::move(fp), output_stream);
+            #endif
+            if (formulation_type == "tshirt" )
+                 return std::make_shared<Tshirt_Realization>(identifier, std::move(fp), output_stream);
+            if (formulation_type == "tshirt_c" )
+                return std::make_shared<Tshirt_C_Realization>(identifier, std::move(fp), output_stream);
+            if (formulation_type == "simple_lumped" )
+                return std::make_shared<Simple_Lumped_Model_Realization>(identifier, std::move(fp), output_stream);
+            throw std::runtime_error("Could not find a matching constructor");
         }
-        else if (forcing_config.provider == "CsvPerFeature"){
+        else if (forcing_config.provider == "CsvPerFeature")
+        {
+            std::unique_ptr<CsvPerFeatureForcingProvider> fp;
             fp = std::make_unique<CsvPerFeatureForcingProvider>(forcing_config);
+            #ifdef NGEN_BMI_C_LIB_ACTIVE
+            if (formulation_type == "bmi_c" )
+                return std::make_shared<Bmi_C_Formulation>(identifier, std::move(fp), output_stream);
+            #endif
+            #ifdef NGEN_BMI_FORTRAN_ACTIVE
+            if (formulation_type == "bmi_fortran" )
+                return std::make_shared<Bmi_Fortran_Formulation>(identifier, std::move(fp), output_stream);
+            #endif
+            if (formulation_type == "bmi_multi" )
+                return std::make_shared<Bmi_Multi_Formulation>(identifier, std::move(fp), output_stream);
+            #ifdef ACTIVATE_PYTHON
+            if (formulation_type == "bmi_python" )
+                return std::make_shared<Bmi_Py_Formulation>(identifier, std::move(fp), output_stream);
+            #endif
+            throw std::runtime_error("Could not find a matching constructor");
+        }
+        else if (forcing_config.provider == "NetCDFPerFeature")
+        {
+            std::shared_ptr<data_access::NetCDFPerFeatureDataProvider> fp;
+            fp = std::make_unique<data_access::NetCDFPerFeatureDataProvider>(forcing_config.path.c_str(), output_stream);
+            #ifdef NGEN_BMI_C_LIB_ACTIVE
+            if (formulation_type == "bmi_c" )
+                return std::make_shared<Bmi_C_Formulation>(identifier, std::move(fp), output_stream);
+            #endif
+            #ifdef NGEN_BMI_FORTRAN_ACTIVE
+            if (formulation_type == "bmi_fortran" )
+                return std::make_shared<Bmi_Fortran_Formulation>(identifier, std::move(fp), output_stream);
+            #endif
+            if (formulation_type == "bmi_multi" )
+                return std::make_shared<Bmi_Multi_Formulation>(identifier, std::move(fp), output_stream);
+            #ifdef ACTIVATE_PYTHON
+            if (formulation_type == "bmi_python" )
+                return std::make_shared<Bmi_Py_Formulation>(identifier, std::move(fp), output_stream);
+            #endif
+            if (formulation_type == "simple_lumped" )
+                return std::make_shared<Simple_Lumped_Model_Realization>(identifier, std::move(fp), output_stream);
+            throw std::runtime_error("Could not find a matching constructor");
         }
         else { // Some unknown string in the provider field?
             throw std::runtime_error(
@@ -81,7 +165,7 @@ namespace realization {
                     "\", formulation_type: \"" + formulation_type +
                     "\", provider: \"" + forcing_config.provider + "\"");
         }
-        return formulation_constructor(identifier, std::move(fp), output_stream);
+        
     };
 
     static std::string get_formulation_key(const boost::property_tree::ptree &tree) {
