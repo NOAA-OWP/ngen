8abd84f1ebdaf28a3e476e188c0e07c47e638c6d
Nels Frazier <nels.frazier@noaa.gov>
2022-04-04 18:23:48 +0000

    use get_values interface for update calls when appropriate

--- include/realizations/catchment/Bmi_Module_Formulation.hpp
+++ include/realizations/catchment/Bmi_Module_Formulation.hpp
@@ -1106,16 +1106,25 @@ namespace realization {
                 //  this type of behavior
                 // TODO: account for arrays later
                 int varItemSize = get_bmi_model()->GetVarItemsize(var_name);
-                if (varItemSize != get_bmi_model()->GetVarNbytes(var_name)) {
-                    throw std::runtime_error(
-                            "BMI input variable '" + var_name + "' is an array - not currently supported");
-                }
-                double value = provider->get_value(CatchmentAggrDataSelector("",var_map_alias, model_epoch_time, t_delta,
-                                                   get_bmi_model()->GetVarUnits(var_name)));
+                std::shared_ptr<void> value_ptr;
                 // Finally, use the value obtained to set the model input
                 std::string type = get_bmi_model()->get_analogous_cxx_type(get_bmi_model()->GetVarType(var_name),
                                                                            varItemSize);
-                std::shared_ptr<void> value_ptr = get_value_as_type(type, value);
+                if (varItemSize != get_bmi_model()->GetVarNbytes(var_name)) {
+                    //more than a single value needed for var_name
+                    auto values = provider->get_values(var_map_alias, model_epoch_time, t_delta,
+                                                   get_bmi_model()->GetVarUnits(var_name));
+                    //need to marshal data types to the reciever as well
+                    //this could be done a little more elegantly if the provider interface were
+                    //"type aware", but for now, this will do (but requires yet another copy)
+                    value_ptr = get_values_as_type( type, values.begin(), values.end() );
+
+                } else {
+                    //scalar value
+                    double value = provider->get_value(var_map_alias, model_epoch_time, t_delta,
+                                                    get_bmi_model()->GetVarUnits(var_name));
+                    value_ptr = get_value_as_type(type, value);      
+                }
                 get_bmi_model()->SetValue(var_name, value_ptr.get());
             }
         }
