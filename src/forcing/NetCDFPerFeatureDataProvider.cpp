91a4b134f8b1eac36e624f98a1900647b5dc3a32
Matt Williamson <matthew.williamson@gov>
2022-07-12 14:51:09 +0000

    Fix build errors... this will conflict with #421

--- src/forcing/NetCDFPerFeatureDataProvider.cpp
+++ src/forcing/NetCDFPerFeatureDataProvider.cpp
@@ -1,6 +1,7 @@
+#ifdef NETCDF_ACTIVE
 #include "NetCDFPerFeatureDataProvider.hpp"
-#include <mutex>
-#include <map>
 
 std::mutex data_access::NetCDFPerFeatureDataProvider::shared_providers_mutex;
 std::map<std::string, std::shared_ptr<data_access::NetCDFPerFeatureDataProvider>> data_access::NetCDFPerFeatureDataProvider::shared_providers;
+
+#endif
