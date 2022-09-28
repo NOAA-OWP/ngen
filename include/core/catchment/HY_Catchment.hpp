862958355519f618d9eed55456c971cc67553646
Donald Johnson <Donald.W.Johnson@noaa.gov>
2022-09-28 16:43:34 +0000

    Added level variable to HY_Catchment. Added function to get and set catchment level. Added constant variables to mark predefined catchment level values.

--- include/core/catchment/HY_Catchment.hpp
+++ include/core/catchment/HY_Catchment.hpp
@@ -21,7 +21,7 @@ class HY_Catchment : public HY_HydroFeature
     public:
 
     HY_Catchment();
-    HY_Catchment(std::string id, Nexuses inflows, Nexuses outflows, std::shared_ptr<HY_CatchmentRealization> realization):
+    HY_Catchment(std::string id, Nexuses inflows, Nexuses outflows, std::shared_ptr<HY_CatchmentRealization> realization, long lv = 0):
     id(std::move(id)),
     inflows(std::move(inflows)),
     outflows(std::move(outflows)),
@@ -30,10 +30,40 @@ class HY_Catchment : public HY_HydroFeature
     conjoint_catchment({}),
     upper_catchment({}),
     lower_catchment({}),
-    realization(realization){}
+    realization(realization),
+    level(lv){}
     virtual ~HY_Catchment();
     const Nexuses& get_outflow_nexuses(){ return outflows; }
     std::shared_ptr<HY_CatchmentRealization> realization;
+
+    /*! \brief get the hydrofabric level of this catchment
+     *
+     * The hydrofabric is divided into multiple level which are intended to be processed in decreasing numeric order. 
+     * The current defined levels are:
+     *      80 -- Atmospheric Models (high)
+     *      60 -- Atmospeheric Models (low)
+     *      40 -- Canopy Models
+     *      20 -- Snow melt models
+     *      0  -- Surface water processes
+     *      -20 -- Ground Water (Shallow)
+     *      -40 -- Ground Water (Deep)
+     */
+    long get_level() { return level; }
+
+    /*! \brief set the level of this catchment
+     *
+     * This function changes the level of a catchment. It should only be used if the type of model being used in the catchment changes.
+     */
+     void set_level(long lv) { level = lv; }
+
+     const long ATMOSPHERIC_LEVEL_HIGH = 80;        //!< This is the catchment level for high air atmospheric models
+     const long ATMOSPHERIC_LEVEL_LOW = 60;         //!< This is the catchment level for low air atmospheric models
+     const long CANOPY_LEVEL = 40;                  //!< This is the catchment level for canopy level models
+     const long SNOW_LEVEL = 20;                    //!< This is the catchment level for surface snow models
+     const long SURFACE_LEVEL = 0;                  //!< This is the catchment level for surface water models  
+     const long GROUND_WATER_SHALLOW_LEVEL = -20;   //!< This is the catchment level for shallow groundwater models
+     const long GROUND_WATER_DEEP_LEVEL = -40;      //!< This is the catchment level for deep groundwater models
+
     protected:
 
     private:
@@ -46,6 +76,8 @@ class HY_Catchment : public HY_HydroFeature
     Catchment conjoint_catchment;
     Catchment upper_catchment;
     Catchment lower_catchment;
+
+    long level;
 
 };
 
