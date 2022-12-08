69ba34a6155560febb189b45f3588db74a90f309
Shengting Cui <shengting.cui@noaa.gov>
2022-12-08 21:22:06 +0000

    Revise HY_CatchmentRealization.hpp

--- include/core/catchment/HY_CatchmentRealization.hpp
+++ include/core/catchment/HY_CatchmentRealization.hpp
@@ -40,10 +40,12 @@ class HY_CatchmentRealization
      */
     virtual double get_response(time_step_t t_index, time_step_t t_delta) = 0;
 
+  #ifdef NETCDF_ACTIVE
     std::shared_ptr<data_access::GenericDataProvider> get_forcing()
     {
         return forcing;
     }
+  #endif
 
     protected:
 
