9e151a83a2739cf3d0fed940500f3f3105a53761
Shengting Cui <shengting.cui@noaa.gov>
2022-12-08 20:41:46 +0000

    Adding NETCDF_ACTIVE guards to GenericDataProvider.hpp

--- include/forcing/GenericDataProvider.hpp
+++ include/forcing/GenericDataProvider.hpp
@@ -4,21 +4,26 @@
 #include "DataProvider.hpp"
 #include "DataProviderSelectors.hpp"
 
+#ifdef NETCDF_ACTIVE
 #include <memory>
 #include <netcdf>
+#endif
 
 namespace data_access
 {
     class GenericDataProvider : public DataProvider<double, CatchmentAggrDataSelector>
     {
         public:
+
+  #ifdef NETCDF_ACTIVE
         /** Retrun the shared_ptr to the NcFile */
         virtual std::shared_ptr<netCDF::NcFile> get_nc_file() {
             return 0;
         }
+  #endif
 
         private:
     };
 }
 
-#endif
+#endif //NGEN_GENRIC_DATA_PROVIDER
--- src/forcing/GenericDataProvider.cpp
+++
@@ -1,6 +0,0 @@
-#ifdef NETCDF_ACTIVE
-#include "GenericDataProvider.hpp"
-
-//Include the header file in this superfluous code so that the github actions could see <netcdf> header
-
-#endif
