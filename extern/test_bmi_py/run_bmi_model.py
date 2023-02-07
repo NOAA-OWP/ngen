29576c871e441c91cf8607a64fa9d7ef4bd66354
Nels Frazier <nels.frazier@noaa.gov>
2023-02-07 17:12:00 -0600

    initialize an abritrary grid from the bmi driver

--- extern/test_bmi_py/run_bmi_model.py
+++ extern/test_bmi_py/run_bmi_model.py
@@ -42,6 +42,42 @@ def execute():
     print('Now loop through the inputs, set the values, and update the model')
     print('\n')
     print('model time', 'input 1', 'input 2', 'output 1', 'output 2', 'output 3')
+    grid_data = np.ones((5,5))
+    grid_origin = (0,0)
+    grid_spacing = (1,1)
+    num_grids = np.array(0, dtype=np.int16)
+
+    model.get_value("grid:count", num_grids)
+    grids = np.zeros((num_grids), dtype=np.int32)
+    model.get_value("grid:ids", grids)
+    ranks = np.zeros((num_grids), dtype=np.int16)
+    ranks = model.get_value("grid:ranks", ranks)
+    
+    variables = model.get_input_var_names() # + model.get_output_var_names()
+    #Example of determining grid mapping/compatibility.  Mininmum requirement -- rank/dimensions are the same
+    for var in variables:
+        grid = model.get_var_grid(var)
+        idx = np.flatnonzero(grids == grid)[0]
+        if ranks[idx] == grid_data.ndim and ranks[idx] != 0:
+            print(f"Initiating {var} with grid_data shape, origin, and spacing")
+            #FIXME this can init more than once...
+            model.set_value(f'grid_{grid}_shape', grid_data.shape)
+            model.set_value(f'grid_{grid}_origin', grid_origin)
+            model.set_value(f'grid_{grid}_spacing', grid_spacing)
+
+    #Verify some extra grid meta data is available...
+    xs = np.array(())
+    model.get_grid_x(0, xs)
+    print("Grid 0 x coordinates: ", xs)
+    ys = np.ndarray( grid_data.shape[1] )
+    model.get_grid_y(1, ys) 
+    print("Grid 1 y coordinates: ", ys)
+    try:
+        model.get_grid_z(1, ys)
+        print("Any z's???", ys)
+    except ValueError:
+        print("No z's")
+
     for x in range(10):
 
         # Create test case inputs from random values ###########
