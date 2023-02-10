664bdae4a54b8695838a7558171c0ccdbe8d9226
Nels Frazier <nels.frazier@noaa.gov>
2023-02-10 11:57:20 -0600

    use numpy arrays for grid meta data

--- extern/test_bmi_py/bmi_grid.py
+++ extern/test_bmi_py/bmi_grid.py
@@ -12,7 +12,7 @@ import numpy as np
 
 if TYPE_CHECKING:
     from typing import Tuple
-    from numpy import ndarray
+    from numpy.typing import NDArray
 
 class GridType(str, Enum):
     """
@@ -40,13 +40,24 @@ class Grid():
         """
         self._id: int = id
         self._rank: int = rank
-        self._size: int = 0 #FIXME make property
+        self._size: int = 0
         self._type: GridType = type #FIXME validate type/rank?
-        self._shape: Tuple[int] = None #tuple of size rank
-        self._spacing: Tuple[float] = None #tuple of size rank
-        self._origin: Tuple[float] = None #tuple of size rank
-    
-        self._shape = tuple((0 for i in range(rank))) #set the shape rank, with 0 allocated values
+        self._shape: 'NDArray[np.int32]' = None #array of size rank
+        self._spacing: 'NDArray[np.float64]' = None #array of size rank
+        self._origin: 'NDArray[np.float64]' = None #array of size rank
+        if( rank == 0 ):
+            # We have to use a 1 dim representation for a scalar cause numpy initialization is weird
+            # np.zeros( [1] ) gives you an array([0.])
+            # np.zeros( [0] ) gives you an emptty array([])
+            # np.zeros( () ) gives a scalar wrapped in an array array(0.)
+            # This latter is really what we want, but then it is hard to communicate the actual size
+            # (as a numerical value...)
+            self._shape = np.zeros( (), np.int32 ) #note, int32 is important here -- assumed by ngen
+            #self._shape[...] = 1
+        else:
+            self._shape = np.zeros( rank, np.int32) #set the shape rank, with 0 allocated values
+        #Make the array "immutable", can only modify via setting
+        self._shape.flags.writeable = False
 
     # TODO consider restricting resetting of grid values after they have been initialized
 
@@ -75,7 +86,7 @@ class Grid():
         Returns:
             int: number of grid elements
         """
-        if not self.shape: #it is None or ()
+        if not self.shape or self.shape.ndim == 0: #it is None or () or np.array( () )
             return 0
         else:
             #multiply the shape of each dimension together
@@ -91,7 +102,7 @@ class Grid():
         return self._type
     
     @property
-    def shape(self) -> 'Tuple[int]':
+    def shape(self) -> 'NDArray[np.int32]':
         """The shape of the grid (the size of each dimension)
 
         Returns:
@@ -100,16 +111,18 @@ class Grid():
         return self._shape
     
     @shape.setter
-    def shape(self, shape: 'Tuple[int]') -> None:
+    def shape(self, shape: 'NDArray[np.int32]') -> None:
         """Set the shape of the grid to the provided shape
 
         Args:
             shape (Tuple[int]): the size of each dimension of the grid
         """
-        self._shape = shape
+        #Create a new shape array and replace the old one, make it immutable
+        self._shape = np.array(shape, dtype=np.int32)
+        self._shape.flags.writeable = False
     
     @property
-    def spacing(self) -> 'Tuple[float]':
+    def spacing(self) -> 'NDArray[np.float64]':
         """The spacing of the grid
 
         Returns:
@@ -118,7 +131,7 @@ class Grid():
         return self._spacing
     
     @spacing.setter
-    def spacing(self, spacing: 'Tuple[float]') -> None:
+    def spacing(self, spacing: 'NDArray[np.float64]') -> None:
         """Set the spacing of each grid dimension.
 
         Args:
@@ -127,7 +140,7 @@ class Grid():
         self._spacing = spacing
 
     @property
-    def origin(self) -> 'Tuple[float]':
+    def origin(self) -> 'NDArray[np.float64]':
         """The origin point of the grid
 
         Returns:
@@ -136,7 +149,7 @@ class Grid():
         return self._origin
     
     @origin.setter
-    def origin(self, origin: 'Tuple[float]') -> None:
+    def origin(self, origin: 'NDArray[np.float64]') -> None:
         """Set the grid origin location
 
         Args:
@@ -145,43 +158,43 @@ class Grid():
         self._origin = origin
 
     @property
-    def grid_x(self) -> 'ndarray':
+    def grid_x(self) -> 'NDArray[np.float64]':
         """Coordinates of the x components of the grid
 
         Returns:
             ndarray: array of cooridnate values in the x direction
         """
         if len(self.shape) > 0:
-            return np.array( [ self.origin[0] + self.spacing[0]*x for x in range(self.shape[0]) ] )
+            return np.array( [ self.origin[0] + self.spacing[0]*x for x in range(self.shape[0]) ], dtype=np.float64 )
         else:    
             #TODO should this raise an error or return an empty array?
             #raise RuntimeError(f"Cannot get x coordinates of grid with shape {self.shape}")
-            return np.array(())
+            return np.array((), dtype=np.float64)
     
     @property
-    def grid_y(self) -> 'ndarray':
+    def grid_y(self) -> 'NDArray[np.float64]':
         """Coordinates of the y components of the grid
 
         Returns:
             ndarray: array of coordinate values in the y direction
         """
         if len(self.shape) > 1:
-            return np.array( [ self.origin[1] + self.spacing[1]*y for y in range(self.shape[1]) ] )
+            return np.array( [ self.origin[1] + self.spacing[1]*y for y in range(self.shape[1]) ], dtype=np.float64 )
         else:    
             #TODO should this raise an error or return an empty array?
             #raise RuntimeError(f"Cannot get y coordinates of grid with shape {self.shape}")
-            return np.array(())
+            return np.array((), dtype=np.float64)
     
     @property
-    def grid_z(self) -> 'ndarray':
+    def grid_z(self) -> 'NDArray[np.float64]':
         """Coordinates of the z components of the grid
 
         Returns:
             ndarray: array of coordinate values in the z direction
         """
         if len(self.shape) > 2:
-            return np.array( [ self.origin[2] + self.spacing[2]*z for z in range(self.shape[2]) ] )
+            return np.array( [ self.origin[2] + self.spacing[2]*z for z in range(self.shape[2]) ], dtype=np.float64 )
         else:    
             #TODO should this raise an error or return an empty array?
             #raise RuntimeError(f"Cannot get z coordinates of grid with shape {self.shape}")
-            return np.array(())
+            return np.array((), dtype=np.float64)
