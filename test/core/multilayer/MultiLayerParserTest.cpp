dbf2b27772101dd35096e07def123fa014d012e8
Donald Johnson <Donald.W.Johnson@noaa.gov>
2023-03-01 21:41:07 +0000

    Update test code for multilayer.

--- test/core/multilayer/MultiLayerParserTest.cpp
+++ test/core/multilayer/MultiLayerParserTest.cpp
@@ -5,7 +5,11 @@
 class MultiLayerParserTest : public ::testing::Test {
     protected:
 
-        MultiLayerParserTest() {
+        MultiLayerParserTest() : 
+            nexus_data_path("../data/multilayer/nexus_data.geojson"),
+            catchment_data_path("../data/multilayer/nexus_data.geojson"),
+            realization_config_path("../data/multilayer/realization_config.json")
+        {
 
         }
 
@@ -13,11 +17,11 @@ class MultiLayerParserTest : public ::testing::Test {
 
         }
 
-        void Setup() override {
+        void SetUp() override {
             nexus_collection = geojson::read(nexus_data_path.c_str());
             std::cout << "Building Catchment collection" << std::endl;
 
-            catchment_collection = geojson::read(catchement_data_path.c_str());
+            catchment_collection = geojson::read(catchment_data_path.c_str());
             
             // add the catchments to the nexus collection
             for(auto& feature: *catchment_collection)
@@ -39,14 +43,19 @@ class MultiLayerParserTest : public ::testing::Test {
     std::string realization_config_path;
 
 
-}
+};
 
 TEST_F(MultiLayerParserTest, TestInit0)
 {
     manager = std::make_shared<realization::Formulation_Manager>(realization_config_path.c_str());
+
+    ASSERT_TRUE(true);
 }
 
 TEST_F(MultiLayerParserTest, TestRead0)
 {
-    manager->read(catchment_collection, utils::getStdOut());
+    manager = std::make_shared<realization::Formulation_Manager>(realization_config_path.c_str());
+    manager->read(nexus_collection, utils::getStdOut());
+
+    ASSERT_TRUE(true);
 }
