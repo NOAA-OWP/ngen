b8784c68ed5819d6c15e7fbea6e7d0bd474d18d1
Matt Williamson <87771120+mattw-nws@users.noreply.github.com>
2023-03-23 18:20:01 +0000

    Now backwards compatible if an old ngen_routing is installed

--- src/routing/Routing_Py_Adapter.cpp
+++ src/routing/Routing_Py_Adapter.cpp
@@ -13,8 +13,21 @@ Routing_Py_Adapter::Routing_Py_Adapter(std::string t_route_config_file_with_path
   interperter = utils::ngenPy::InterpreterUtil::getInstance();
   //Import ngen_main.  Will throw error if module isn't available
   //in the embedded interperters PYTHON_PATH
-  this->t_route_module = utils::ngenPy::InterpreterUtil::getPyModule("nwm_routing.__main__");
+  try {
+    this->t_route_module = utils::ngenPy::InterpreterUtil::getPyModule("ngen_routing.ngen_main");
+    std::cout<<"WARN: Legacy t-route module detected; use of this version is deprecated!"<<std::endl;
   }
+  catch (const pybind11::error_already_set& e){
+    try {
+      // The legacy module has a `nwm_routing.__main__`, so we have to try this one second!
+      this->t_route_module = utils::ngenPy::InterpreterUtil::getPyModule("nwm_routing.__main__");
+    }
+    catch (const pybind11::error_already_set& e){
+      std::cerr<<"FAIL: Unable to import a supported routing module."<<std::endl;
+      throw e;
+    }
+  }
+}
 
 void Routing_Py_Adapter::route(int number_of_timesteps, int delta_time,
                           const std::vector<double> &flow_vector){
@@ -34,9 +47,16 @@ void Routing_Py_Adapter::route(int number_of_timesteps, int delta_time)
   py::list arg_list = py::cast(arg_vector);
 
   //Create object for the ngen_main subroutine
-  py::object ngen_main = t_route_module.attr("main_v04");
 
-  //Call ngen_main subroutine
+  py::object ngen_main;
+  try {
+    // Try the legacy method first... this time because if we lose an exeption, we should favor one from the newer version.
+    ngen_main = t_route_module.attr("ngen_main");
+  }
+  catch (const pybind11::error_already_set& e){
+    ngen_main = t_route_module.attr("main_v04");
+  }
+
   ngen_main(arg_list);
 
   std::cout << "Finished routing" << std::endl;
