fdb81f9a6115768aa717c3c0e53e83589045e0ea
program-- <justin@justinsingh.me>
2023-05-08 13:32:34 -0700

    fix OOR error; better errors for geopackage::read

--- src/geopackage/read.cpp
+++ src/geopackage/read.cpp
@@ -12,28 +12,45 @@ std::shared_ptr<geojson::FeatureCollection> geopackage::read(
 
     // Check if layer exists
     if (!db.has_table(layer)) {
-        throw std::runtime_error("table " + layer + " does not exist");
+        std::string errmsg = "[" + std::string(sqlite3_errmsg(db.connection())) + "] " +
+                             "table " + layer + " does not exist.\n\tTables: ";
+
+        auto errquery = db.query("SELECT name FROM sqlite_master WHERE type='table'").next();
+        while(!errquery.done()) {
+            errmsg += errquery.get<std::string>(0);
+            errmsg += ", ";
+            errquery.next();
+        }
+
+        throw std::runtime_error(errmsg);
     }
 
     // Layer exists, getting statement for it
     std::string joined_ids = "";
     if (!ids.empty()) {
-        std::accumulate(
-            ids.begin(),
-            ids.end(),
-            joined_ids,
-            [](const std::string& origin, const std::string& append) {
-                return origin.empty() ? append : origin + "," + append;
-            }
-        );
-
-        joined_ids = " WHERE id (" + joined_ids + ")";
+        joined_ids = " WHERE id IN (?";
+        for (size_t i = 1; i < ids.size(); i++) {
+            joined_ids += ", ?";
+        }
+        joined_ids += ")";
     }
 
     // Get number of features
-    sqlite_iter query_get_layer_count = db.query("SELECT COUNT(*) FROM " + layer);
+    sqlite_iter query_get_layer_count = db.query("SELECT COUNT(*) FROM " + layer + joined_ids, ids);
     query_get_layer_count.next();
     const int layer_feature_count = query_get_layer_count.get<int>(0);
+
+    #ifndef NGEN_QUIET
+    std::cout << "Reading " << layer_feature_count << " features in layer " << layer;
+    if (!ids.empty()) {
+        std::cout << " (id subset:";
+        for (auto& id : ids) {
+            std::cout << " " << id;
+        }
+        std::cout << ")";
+    }
+    std::cout << std::endl;
+    #endif
 
     // Get layer feature metadata (geometry column name + type)
     sqlite_iter query_get_layer_geom_meta = db.query("SELECT column_name FROM gpkg_geometry_columns WHERE table_name = ?", layer);
@@ -41,7 +58,7 @@ std::shared_ptr<geojson::FeatureCollection> geopackage::read(
     const std::string layer_geometry_column = query_get_layer_geom_meta.get<std::string>(0);
 
     // Get layer
-    sqlite_iter query_get_layer = db.query("SELECT * FROM " + layer);
+    sqlite_iter query_get_layer = db.query("SELECT * FROM " + layer + joined_ids, ids);
     query_get_layer.next();
 
     std::vector<geojson::Feature> features;
@@ -52,7 +69,7 @@ std::shared_ptr<geojson::FeatureCollection> geopackage::read(
             layer_geometry_column
         );
 
-        features.emplace_back(feature);
+        features.push_back(feature);
         query_get_layer.next();
     }
 
