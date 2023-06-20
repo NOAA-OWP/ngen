1e9ed4b70e0f2fce5f8f98c38bb36aac5e756c9a
Shengting Cui <shengting.cui@noaa.gov>
2023-06-20 16:28:25 +0000

    Attempt to fix some of the unit test errors.

--- src/realizations/catchment/Bmi_Multi_Formulation.cpp
+++ src/realizations/catchment/Bmi_Multi_Formulation.cpp
@@ -294,7 +294,6 @@ string Bmi_Multi_Formulation::get_output_line_for_timestep(int timestep, std::st
 
     // Start by first checking whether we are NOT just using the last module's values
     if (!is_out_vars_from_last_mod) {
-
         // TODO: see Github issue 355: this design (and formulation output handling in general) needs to be reworked
         // Clear anything currently in the multi formulation's stream buffer
         output_text_stream->str(std::string());
@@ -304,13 +303,16 @@ string Bmi_Multi_Formulation::get_output_line_for_timestep(int timestep, std::st
         if (output_var_names.empty()) { return ""; }
 
         try {
+            time_t t_delta = timestep;
             // Do the first separately, without the leading comma
             auto output_data_provider_iter = availableData.find(output_var_names[0]);
-            *output_text_stream << get_var_value_as_double(output_var_names[0]);
+
+            // Do the first without the leading comma
+            *output_text_stream << get_var_vec_as_double(0, output_var_names[0])[0];
 
             // Do the rest with a leading comma
             for (int i = 1; i < output_var_names.size(); ++i) {
-                *output_text_stream << "," << get_var_value_as_double(output_var_names[i]);
+                *output_text_stream << "," << get_var_vec_as_double(0, output_var_names[i])[0];
             }
             return output_text_stream->str();
         }
