a3591fdca13e1209df15680c600b1d2ddd238dd5
Phil Miller <philip.miller@noaa.gov>
2023-07-03 16:27:25 -0700

    Fix typo and missing EOF newline

--- cmake/dynamic_sourced_library.cmake
+++ cmake/dynamic_sourced_library.cmake
@@ -1,6 +1,6 @@
 ## Name : dynamic_sourced_library
 ## Params: LIB_NAME, WORK_DIR
-## Add/create a new library with the given name, passing a source files all the '*.cpp' files found in the given working directory for the library
+## Add/create a new library with the given name, passing as source files all the '*.cpp' files found in the given working directory for the library
 function(dynamic_sourced_cxx_library LIB_NAME WORK_DIR)
     execute_process(COMMAND ls
             COMMAND grep .cpp
