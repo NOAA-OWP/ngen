2f6dd4302f63484f5aa1b8e82f376a7b0096650e
Phil Miller <philip.miller@noaa.gov>
2023-07-24 09:21:49 -0700

    Fix guard on compiler-bug-workaround code to silence warnings from newer compilers

--- include/all.h
+++ include/all.h
@@ -1,12 +1,5 @@
 #pragma once
 
-#ifndef __APPLE__
-#define GCC_VERSION (__GNUC__ * 10000 \
-                     + __GNUC_MINOR__ * 100 \
-                     + __GNUC_PATCHLEVEL__)
-
-#endif
-
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 #define SOURCE_LOC " (" __FILE__ ":" TOSTRING(__LINE__) ")"
