10a4bac4e3494da0b56c6616d89080ed151a0647
Phil Miller <philip.miller@noaa.gov>
2023-07-25 13:41:35 -0700

    #582: Introduce span usage in GeoPackage internal function interfaces

--- include/utilities/EndianCopy.hpp
+++ include/utilities/EndianCopy.hpp
@@ -1,9 +1,8 @@
 #ifndef NGEN_ENDIANCOPY_H
 #define NGEN_ENDIANCOPY_H
 
-#include <vector>
-
 #include <boost/endian.hpp>
+#include "span.hpp"
 
 namespace utils {
 
@@ -21,7 +20,7 @@ namespace utils {
  * @param order endianness value (0x01 == Little; 0x00 == Big)
  */
 template<typename S>
-void copy_from(const std::vector<uint8_t>& src, int& index, S& dst, uint8_t order)
+void copy_from(const boost::span<const uint8_t> src, int& index, S& dst, uint8_t order)
 {
     std::memcpy(&dst, src.data() + index, sizeof(dst));
 
@@ -37,7 +36,7 @@ void copy_from(const std::vector<uint8_t>& src, int& index, S& dst, uint8_t orde
 // boost::endian doesn't support using primitive doubles
 // see: https://github.com/boostorg/endian/issues/36
 template<>
-inline void copy_from<double>(const std::vector<uint8_t>& src, int& index, double& dst, uint8_t order)
+inline void copy_from<double>(const boost::span<const uint8_t> src, int& index, double& dst, uint8_t order)
 {
     static_assert(sizeof(uint64_t) == sizeof(double), "sizeof(uint64_t) is not the same as sizeof(double)!");
 
