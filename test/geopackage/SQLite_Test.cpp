7316c25570f7fe4804afafaba8826160c47d7e00
Justin Singh-M. - NOAA <justin@justinsingh.me>
2023-09-26 13:10:33 -0700

    Update test routing config to alleviate pydantic errors (#654)
    * tests: update test routing config to alleviate pydantic errors
    
    * fix: uncomment shim lines
    
    * fix: attempt another fix
    
    * tests: update routing geopackage to pre-release hf
    
    * tests: set column name mapping
    
    * tests: add remaining column mappings
    
    * tests: update gpkg tests for new gpkg file

--- test/geopackage/SQLite_Test.cpp
+++ test/geopackage/SQLite_Test.cpp
@@ -44,7 +44,7 @@ TEST_F(SQLite_Test, sqlite_query_test)
     }
 
     // user provides a query
-    const std::string query = "SELECT * FROM gpkg_contents LIMIT 1";
+    const std::string query = "SELECT * FROM gpkg_contents WHERE table_name = 'flowpaths' LIMIT 1";
     sqlite_iter iter = db.query(query);
 
     EXPECT_EQ(iter.num_columns(), 10);
@@ -69,11 +69,10 @@ TEST_F(SQLite_Test, sqlite_query_test)
     EXPECT_EQ(iter.get<std::string>(1), "features");
     EXPECT_EQ(iter.get<std::string>(2), "flowpaths");
     EXPECT_EQ(iter.get<std::string>(3), "");
-    EXPECT_EQ(iter.get<std::string>(4), "2022-10-25T14:33:51.668Z");
-    EXPECT_EQ(iter.get<double>(5), 1995218.564876059);
-    EXPECT_EQ(iter.get<double>(6), 2502240.321178956);
-    EXPECT_EQ(iter.get<double>(7), 2002525.992495368);
-    EXPECT_EQ(iter.get<double>(8), 2508383.058762011);
+    EXPECT_NEAR(iter.get<double>(5), 1995219.0, 1e2);
+    EXPECT_NEAR(iter.get<double>(6), 2502240.0, 1e2);
+    EXPECT_NEAR(iter.get<double>(7), 2002526.0, 1e2);
+    EXPECT_NEAR(iter.get<double>(8), 2508383.0, 1e2);
     EXPECT_EQ(iter.get<int>(9), 5070);
 
     // using column_names
@@ -81,11 +80,10 @@ TEST_F(SQLite_Test, sqlite_query_test)
     EXPECT_EQ(iter.get<std::string>("data_type"), "features");
     EXPECT_EQ(iter.get<std::string>("identifier"), "flowpaths");
     EXPECT_EQ(iter.get<std::string>("description"), "");
-    EXPECT_EQ(iter.get<std::string>("last_change"), "2022-10-25T14:33:51.668Z");
-    EXPECT_EQ(iter.get<double>("min_x"), 1995218.564876059);
-    EXPECT_EQ(iter.get<double>("min_y"), 2502240.321178956);
-    EXPECT_EQ(iter.get<double>("max_x"), 2002525.992495368);
-    EXPECT_EQ(iter.get<double>("max_y"), 2508383.058762011);
+    EXPECT_NEAR(iter.get<double>(5), 1995219.0, 1e2);
+    EXPECT_NEAR(iter.get<double>(6), 2502240.0, 1e2);
+    EXPECT_NEAR(iter.get<double>(7), 2002526.0, 1e2);
+    EXPECT_NEAR(iter.get<double>(8), 2508383.0, 1e2);
     EXPECT_EQ(iter.get<int>("srs_id"), 5070);
 
     // reiteration
