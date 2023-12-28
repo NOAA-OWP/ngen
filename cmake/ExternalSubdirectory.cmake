cde8857e188b954d4d6d27549ed9036c6774b359
program-- <justin@justinsingh.me>
2023-12-28 07:49:06 -0800

    cmake: automate building extern libraries; see extended commit msg
    - adds options:
      + NGEN_WITH_EXTERN_CXX_MODELS
      + NGEN_WITH_EXTERN_C_MODELS
      + NGEN_WITH_EXTERN_FORTRAN_MODELS
    - modifies ngen_add_test to allow `DEPENDS` arguments,
      which are dependent targets for that test. This differs
      from `REQUIRES` and `LIBRARIES` in that `DEPENDS` are simply
      dependent targets, and otherwise have no relation/linkage to the target.
    - adds cmake/ExternalSubdirectory.cmake which provides the
      `add_external_subdirectory` function. This is a simple convenience
      function wrapping `add_subdirectory`.

---
+++ cmake/ExternalSubdirectory.cmake
@@ -0,0 +1,19 @@
+# Add an external subdirectory to build
+function(add_external_subdirectory)
+    set(options GIT_UPDATE)
+    set(oneValueArgs SOURCE OUTPUT)
+    set(multiValueArgs "")
+    cmake_parse_arguments(EXTERNAL_TARGET
+        "${options}" "${oneValueArgs}"
+        "${multiValueArgs}" ${ARGN})
+
+    if(NGEN_UPDATE_GIT_SUBMODULES AND EXTERNAL_TARGET_GIT_UPDATE)
+        git_update_submodule("${EXTERNAL_TARGET_SOURCE}")
+    endif()
+
+    if("EXTERNAL_TARGET_OUTPUT" IN_LIST EXTERNAL_TARGET_MISSING_VALUES)
+        add_subdirectory("${EXTERNAL_TARGET_SOURCE}")
+    else()
+        add_subdirectory("${EXTERNAL_TARGET_SOURCE}" "${EXTERNAL_TARGET_OUTPUT}")
+    endif()
+endfunction()
