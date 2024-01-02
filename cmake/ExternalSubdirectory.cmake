7350a207664abd4a8567b352a68128065307a57a
program-- <justin@justinsingh.me>
2024-01-02 09:18:09 -0800

    cmake: update submodules on configure

--- cmake/ExternalSubdirectory.cmake
+++ cmake/ExternalSubdirectory.cmake
@@ -1,14 +1,16 @@
+include(GitUpdateSubmodules)
+
 # Add an external subdirectory to build
 function(add_external_subdirectory)
-    set(options GIT_UPDATE)
-    set(oneValueArgs SOURCE OUTPUT)
+    set(options "")
+    set(oneValueArgs SOURCE OUTPUT GIT_UPDATE)
     set(multiValueArgs "")
     cmake_parse_arguments(EXTERNAL_TARGET
         "${options}" "${oneValueArgs}"
         "${multiValueArgs}" ${ARGN})
 
     if(NGEN_UPDATE_GIT_SUBMODULES AND EXTERNAL_TARGET_GIT_UPDATE)
-        git_update_submodule("${EXTERNAL_TARGET_SOURCE}")
+        git_update_submodule("${EXTERNAL_TARGET_GIT_UPDATE}")
     endif()
 
     if("EXTERNAL_TARGET_OUTPUT" IN_LIST EXTERNAL_TARGET_MISSING_VALUES)
