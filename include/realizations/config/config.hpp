67bb1e1bf7a043f506d7ba0dd56b19bbbfb87d6c
hellkite500 <nfrazier@lynker.com>
2024-02-26 07:52:32 -0700

    feat(config): Add structure for managing reusable config for ngen formulation/forcing

---
+++ include/realizations/config/config.hpp
@@ -0,0 +1,63 @@
+#ifndef NGEN_REALIZATION_CONFIG_H
+#define NGEN_REALIZATION_CONFIG_H
+
+#include <boost/property_tree/ptree.hpp>
+
+#include "formulation.hpp"
+#include "forcing.hpp"
+
+#include<iostream>
+
+namespace realization{
+  namespace config{
+
+    /**
+     * @brief Structure representing the configuration for a general Formulation.
+     * 
+     */
+    struct Config{
+        
+        /**
+         * @brief Construct a new Config object
+         *
+         */
+        Config() = default;
+
+        /**
+         * @brief Construct a new Config object from a property tree
+         * 
+         * @param tree 
+         */
+        Config(const boost::property_tree::ptree& tree):formulation_tree(tree){
+        
+            auto possible_forcing = tree.get_child_optional("forcing");
+
+            if (possible_forcing) {
+                forcing = Forcing(*possible_forcing);
+            }
+            //get first empty key under formulations (corresponds to first json array element)
+            auto possible_formulation_tree = tree.get_child_optional("formulations..");
+            if(possible_formulation_tree){
+                formulation = Formulation(*possible_formulation_tree);
+            }
+        }
+
+        /**
+         * @brief Determine if the config has a formulation
+         * 
+         * @return true if the formulation name/type is set or if model parameters are present
+         * @return false if either the type or parameters are empty
+         */
+        bool has_formulation(){
+            return !(formulation.type.empty() || formulation.parameters.empty());
+        }
+
+        Formulation formulation;
+        boost::property_tree::ptree formulation_tree;
+        Forcing forcing;
+    };
+
+    
+  };//end namespace config
+}//end namespace realization
+#endif //NGEN_REALIZATION_CONFIG_H
