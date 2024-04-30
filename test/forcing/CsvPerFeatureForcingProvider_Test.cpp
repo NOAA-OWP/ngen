61eb791248696bd5e252b3f6c2814b67b1eaf10c
Phil Miller <philip.miller@noaa.gov>
2024-04-30 11:51:42 -0700

    Test CsvPerFeatureForcingProvider with base CatchmentAggrDataSelector instead of pointless child class

--- test/forcing/CsvPerFeatureForcingProvider_Test.cpp
+++ test/forcing/CsvPerFeatureForcingProvider_Test.cpp
@@ -94,24 +94,24 @@ TEST_F(CsvPerFeatureForcingProviderTest, TestForcingDataRead)
     time_t t = begin+(i*3600);
     std::cerr << std::ctime(&t) << std::endl;
 
-    current_precipitation = Forcing_Object->get_value(CSVDataSelector(CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
+    current_precipitation = Forcing_Object->get_value(CatchmentAggrDataSelector("", CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
 
     EXPECT_NEAR(current_precipitation, 7.9999999999999996e-07, 0.00000005);
 
-    double temp_k = Forcing_Object->get_value(CSVDataSelector(CSDMS_STD_NAME_SURFACE_TEMP, begin+(i*3600), 3600, ""), data_access::MEAN);
+    double temp_k = Forcing_Object->get_value(CatchmentAggrDataSelector("", CSDMS_STD_NAME_SURFACE_TEMP, begin+(i*3600), 3600, ""), data_access::MEAN);
 
     EXPECT_NEAR(temp_k, 286.9, 0.00001);
 
     int current_epoch;
 
     i = 387;
-    current_precipitation = Forcing_Object->get_value(CSVDataSelector(CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
+    current_precipitation = Forcing_Object->get_value(CatchmentAggrDataSelector("", CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
 
     EXPECT_NEAR(current_precipitation, 6.9999999999999996e-07, 0.00000005);
 
     //Check exceeding the forcing range to retrieve the last forcing precipation rate
     i = 388;
-    current_precipitation = Forcing_Object->get_value(CSVDataSelector(CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
+    current_precipitation = Forcing_Object->get_value(CatchmentAggrDataSelector("", CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
 
     EXPECT_NEAR(current_precipitation, 6.9999999999999996e-07, 0.00000005);
 }
@@ -127,18 +127,18 @@ TEST_F(CsvPerFeatureForcingProviderTest, TestForcingDataReadAltFormat)
     time_t t = begin+(i*3600);
     std::cerr << std::ctime(&t) << std::endl;
 
-    current_precipitation = Forcing_Object_2->get_value(CSVDataSelector(CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
+    current_precipitation = Forcing_Object_2->get_value(CatchmentAggrDataSelector("", CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
 
     EXPECT_NEAR(current_precipitation, 0.00032685, 0.00000001);
 
-    double temp_k = Forcing_Object_2->get_value(CSVDataSelector(CSDMS_STD_NAME_SURFACE_TEMP, begin+(i*3600), 3600, ""), data_access::MEAN);
+    double temp_k = Forcing_Object_2->get_value(CatchmentAggrDataSelector("", CSDMS_STD_NAME_SURFACE_TEMP, begin+(i*3600), 3600, ""), data_access::MEAN);
 
     EXPECT_NEAR(temp_k, 265.77, 0.00001);
 
     int current_epoch;
 
     i = 34;
-    current_precipitation = Forcing_Object_2->get_value(CSVDataSelector(CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
+    current_precipitation = Forcing_Object_2->get_value(CatchmentAggrDataSelector("", CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
 
     EXPECT_NEAR(current_precipitation, 0.00013539, 0.00000001);
 
@@ -156,11 +156,11 @@ TEST_F(CsvPerFeatureForcingProviderTest, TestForcingDataUnitConversion)
     time_t t = begin+(i*3600);
     std::cerr << std::ctime(&t) << std::endl;
 
-    current_precipitation = Forcing_Object->get_value(CSVDataSelector(CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
+    current_precipitation = Forcing_Object->get_value(CatchmentAggrDataSelector("", CSDMS_STD_NAME_LIQUID_EQ_PRECIP_RATE, begin+(i*3600), 3600, ""), data_access::SUM);
 
     EXPECT_NEAR(current_precipitation, 7.9999999999999996e-07, 0.00000005);
 
-    double temp_f = Forcing_Object->get_value(CSVDataSelector(CSDMS_STD_NAME_SURFACE_TEMP, begin+(i*3600), 3600, "degF"), data_access::MEAN);
+    double temp_f = Forcing_Object->get_value(CatchmentAggrDataSelector("", CSDMS_STD_NAME_SURFACE_TEMP, begin+(i*3600), 3600, "degF"), data_access::MEAN);
 
     EXPECT_NEAR(temp_f, 56.749989014, 0.00001);
 
@@ -199,12 +199,12 @@ TEST_F(CsvPerFeatureForcingProviderTest, TestForcingUnitHeaderParsing)
         const auto expected_out_units = std::get<2>(*ite);
 
         const double in_value = this->Forcing_Object_3->get_value(
-            CSVDataSelector(expected_name, t, 3600, expected_in_units),
+            CatchmentAggrDataSelector("", expected_name, t, 3600, expected_in_units),
             data_access::SUM
         );
     
         const double out_value = this->Forcing_Object_3->get_value(
-            CSVDataSelector(expected_name, t, 3600, expected_out_units),
+            CatchmentAggrDataSelector("", expected_name, t, 3600, expected_out_units),
             data_access::SUM
         );
 
