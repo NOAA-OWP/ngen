1baa8884b961e3d96a1201dfa1986e06493c1177
Shengting Cui <shengting.cui@noaa.gov>
2024-05-10 19:30:38 +0000

    Fix NetCDF file close error running MPI

--- include/realizations/catchment/Formulation_Manager.hpp
+++ include/realizations/catchment/Formulation_Manager.hpp
@@ -246,6 +246,20 @@ namespace realization {
                     return "";
             }
 
+#if NGEN_WITH_NETCDF
+        void close_nc_file()
+        {
+            for (auto const& fmap: formulations)
+            {
+                if (fmap.second->get_forcing()->get_nc_file() != nullptr) {
+                    fmap.second->get_forcing()->get_nc_file()->close();
+                } else {
+                    continue;
+                }
+            }
+        }
+#endif
+
             /**
              * @brief Get the formatted output root
              *
