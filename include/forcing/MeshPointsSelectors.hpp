c9e780cf6b643cf274f48eb9b8824443487e056d
Phil Miller <philip.miller@noaa.gov>
2024-06-24 08:03:01 -0700

    Clean up compiler issues

--- include/forcing/MeshPointsSelectors.hpp
+++ include/forcing/MeshPointsSelectors.hpp
@@ -9,8 +9,8 @@ struct all_points {};
 struct MeshPointsSelector
 {
     std::string variable_name;
-    std::chrono::time_point init_time;
-    std::chrono::duration duration;
+    std::chrono::time_point<std::chrono::steady_clock> init_time;
+    std::chrono::duration<std::chrono::steady_clock> duration;
     std::string output_units;
     boost::variant<all_points, std::vector<int>> points;
 };
