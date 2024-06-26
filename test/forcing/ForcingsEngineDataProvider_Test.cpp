03e388035f56fa29920d0add86ac9a85e48633b6
program-- <justin@justinsingh.me>
2024-06-26 10:57:53 -0700

    rev: add forcings engine test file; add explicit template instatiation

---
+++ test/forcing/ForcingsEngineDataProvider_Test.cpp
@@ -0,0 +1,5 @@
+#include <forcing/ForcingsEngineDataProvider.hpp>
+
+#include "DataProviderSelectors.hpp"
+
+template struct data_access::ForcingsEngineDataProvider<double, CatchmentAggrDataSelector>;
