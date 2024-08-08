6dd95d33688f51f7b34386a9929b6815d43fac26
Phil Miller <philip.miller@noaa.gov>
2024-08-07 17:12:36 -0700

    Update to match const-qualification changes

--- include/realizations/coastal/SchismFormulation.hpp
+++ include/realizations/coastal/SchismFormulation.hpp
@@ -21,12 +21,12 @@ public:
     ~SchismFormulation();
 
     // Implementation of DataProvider
-    boost::span<const std::string> get_available_variable_names() override;
+    boost::span<const std::string> get_available_variable_names() const override;
 
-    long get_data_start_time() override;
-    long get_data_stop_time() override;
-    long record_duration() override;
-    size_t get_ts_index_for_time(const time_t &epoch_time) override;
+    long get_data_start_time() const override;
+    long get_data_stop_time() const override;
+    long record_duration() const override;
+    size_t get_ts_index_for_time(const time_t &epoch_time) const override;
 
     data_type get_value(const selection_type& selector, data_access::ReSampleMethod m) override;
     std::vector<data_type> get_values(const selection_type& selector, data_access::ReSampleMethod m) override;
