8afa3f8e57083fb01a86fc5e8363b9e4c43c25c4
Phil Miller <philip.miller@noaa.gov>
2024-09-17 12:40:01 -0700

    Firm up MeshPointsSelector

--- include/forcing/MeshPointsSelectors.hpp
+++ include/forcing/MeshPointsSelectors.hpp
@@ -10,7 +10,7 @@ static AllPoints all_points;
 struct MeshPointsSelector
 {
     std::string variable_name;
-    std::chrono::time_point<std::chrono::steady_clock> init_time;
+    std::chrono::time_point<std::chrono::system_clock> init_time;
     std::chrono::seconds duration;
     std::string output_units;
     boost::variant<AllPoints, std::vector<int>> points;
