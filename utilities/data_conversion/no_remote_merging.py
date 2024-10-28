4018ebf708f6cf7c520db8c156f0f90d0b62d2db
Josh Cunningham <josh.cu@gmail.com>
2024-10-28 14:37:25 -0500

    fix filenames in output merging script

--- utilities/data_conversion/no_remote_merging.py
+++ utilities/data_conversion/no_remote_merging.py
@@ -11,7 +11,7 @@ def sum_csv_files(file_pattern: str):
     # Group by timestamp and sum the values
     df = df.group_by("index").agg(
         pl.col("timestamp").first().str.strip_chars().alias("timestamp"),
-        pl.col("value").str.strip_chars().cast(pl.Float32).sum().alias("total_value"),
+        pl.col("value").str.strip_chars().cast(pl.Float64).sum().alias("total_value"),
     )
 
     return df.sort("index")
@@ -34,14 +34,16 @@ def merge_outputs(output_path: Path) -> None:
     for file in output_files:
         nexus_id = file.stem.split("_")[0]
         if nexuse_counts[nexus_id] == 1:
-            os.rename(file, output_path / f"{nexus_id}.csv")
+            os.rename(file, output_path / f"{nexus_id}_output.csv")
 
     for nexus, count in nexuse_counts.items():
         if count > 1:
-            df = sum_csv_files(f"{output_path}/{nexus}_*.csv")
-            df.collect().write_csv(f"{output_path}/{nexus}.csv", include_header=False)
-            for file in output_path.glob(f"{nexus}_*.csv"):
+            df = sum_csv_files(f"{output_path}/{nexus}_rank_*.csv")
+            df.collect().write_csv(f"{output_path}/{nexus}_output.csv", include_header=False)
+            for file in output_path.glob(f"{nexus}_rank_*.csv"):
                 file.unlink()
+            # use sed to add the spaces in the csv files
+            os.system(f"sed -i 's/,/, /g' {output_path}/{nexus}_output.csv")
 
     # delete the files that were merged
 
