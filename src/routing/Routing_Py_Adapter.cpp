32a353026e12256359a53d3344b1daf67708708c
Carolyn.Maynard <carolyn.maynard@rtx.com>
2025-09-18 14:26:19 -0700

    Removed deprecated code per OWP

--- src/routing/Routing_Py_Adapter.cpp
+++ src/routing/Routing_Py_Adapter.cpp
@@ -16,25 +16,15 @@ Routing_Py_Adapter::Routing_Py_Adapter(std::string t_route_config_file_with_path
   t_route_config_path(t_route_config_file_with_path){
   //hold a reference to the interpreter, ensures an interpreter exists as long as the reference is held
   interpreter = utils::ngenPy::InterpreterUtil::getInstance();
-  //Import ngen_main.  Will throw error if module isn't available
-  //in the embedded interpreters PYTHON_PATH
+  // Import the routing module.  An error will be thrown if module isn't available
+  // in the embedded interpreters PYTHON_PATH
   try {
-    LOG("Trying t-route module ngen_routing.ngen_main.", LogLevel::INFO);
-    this->t_route_module = utils::ngenPy::InterpreterUtil::getPyModule("ngen_routing.ngen_main");
-    LOG("Routing module ngen_routing.ngen_main detected and used. Use of this version is deprecated!", LogLevel::WARNING);
+    this->t_route_module = utils::ngenPy::InterpreterUtil::getPyModule("nwm_routing.__main__");
+    LOG("Routing module nwm_routing.__main__ detected and used.", LogLevel::INFO);
   }
-  catch (const pybind11::error_already_set& e){
-    try {
-      LOG("Module ngen_routing.ngen_main not found; Trying different t-route module", LogLevel::WARNING);
-      LOG("Trying t-route module nwm_routing.__main__.", LogLevel::INFO);
-      // The legacy module has a `nwm_routing.__main__`, so we have to try this one second!
-      this->t_route_module = utils::ngenPy::InterpreterUtil::getPyModule("nwm_routing.__main__");
-      LOG("Routing module nwm_routing.__main__ detected and used.", LogLevel::INFO);
-    }
-    catch (const pybind11::error_already_set& e){
-      LOG("Unable to import a supported routing module.", LogLevel::SEVERE);
-      throw e;
-    }
+  catch (const pybind11::error_already_set& e) {
+    LOG("Unable to import a supported routing module.", LogLevel::FATAL);
+    throw e;
   }
 }
 
