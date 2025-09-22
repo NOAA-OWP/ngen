6a3e2aad70267ea8b6ba53ec38299ae465f57677
Phil Miller <pmiller@lynker.com>
2025-09-22 16:55:53 -0700

    Refactor ngen main() to encapsulate state during execution

---
+++ include/core/NgenSimulation.hpp
@@ -0,0 +1,56 @@
+#ifndef NGENSIMULATION_HPP
+#define NGENSIMULATION_HPP
+
+#include <Simulation_Time.hpp>
+#include <realizations/catchment/Formulation_Manager.hpp>
+#include <Layer.hpp>
+
+namespace routing_py_adapter {
+    class Routing_Py_Adapter;
+}
+
+#include <memory>
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+// Contains all of the dynamic state and logic to run a NextGen hydrologic simulation
+class NgenSimulation
+{
+public:
+    NgenSimulation(
+                   std::shared_ptr<realization::Formulation_Manager> formulation_manager,
+                   std::vector<std::shared_ptr<ngen::Layer>> layers,
+                   std::unordered_map<std::string, int> catchment_indexes,
+                   std::vector<double> catchment_outflows,
+                   std::unordered_map<std::string, int> nexus_indexes,
+                   std::vector<double> nexus_downstream_flows
+                   );
+    NgenSimulation() = delete;
+
+    void run_catchments();
+    void run_routing();
+    void advance_one_output_step();
+
+    std::shared_ptr<Simulation_Time> sim_time_;
+    
+    // Catchment data
+    std::shared_ptr<realization::Formulation_Manager> catchment_formulation_manager_;
+    std::vector<std::shared_ptr<ngen::Layer>> layers_;
+
+    // Routing data structured for t-route
+    std::unordered_map<std::string, int> catchment_indexes_;
+    std::vector<double> catchment_outflows_;
+    std::unordered_map<std::string, int> nexus_indexes_;
+    std::vector<double> nexus_downstream_flows_;
+
+#if NGEN_WITH_ROUTING
+    std::unique_ptr<routing_py_adapter::Routing_Py_Adapter> router_;
+#endif
+
+    // Serialization template will be defined and instantiated in the .cpp file
+    template <class Archive>
+    void serialize(Archive& ar);
+};
+
+#endif
