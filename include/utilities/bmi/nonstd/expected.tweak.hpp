533b9993eb64b1be2e75924624f8d76389f71214
hellkite500 <nfrazier@lynker.com>
2025-10-24 08:10:10 -0600

    fix(build): fix expected.tweak macro name

--- include/utilities/bmi/nonstd/expected.tweak.hpp
+++ include/utilities/bmi/nonstd/expected.tweak.hpp
@@ -1,4 +1,4 @@
 //tweaks for the expected library configuration/build
 // see https://github.com/martinmoene/expected-lite/tree/master?tab=readme-ov-file#configuration
 // for documentation on configuration
-#define nsel_CONFIG_ WIN32_LEAN_AND_MEAN 0
+#define nsel_CONFIG_WIN32_LEAN_AND_MEAN 0
