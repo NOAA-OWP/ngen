8db437f3925c8864852ee17eabf545db0297f17e
Phil Miller <pmiller@lynker.com>
2025-11-06 10:40:21 -0800

    Refactor: extract single-macro-step updating into a separate function

--- src/core/NgenSimulation.cpp
+++ src/core/NgenSimulation.cpp
@@ -33,74 +33,76 @@ NgenSimulation::~NgenSimulation() = default;
 
 void NgenSimulation::run_catchments()
 {
-    std::stringstream ss;
-
     // Now loop some time, iterate catchments, do stuff for total number of output times
     auto num_times = get_num_output_times();
 
     for (; simulation_step_ < num_times; simulation_step_++) {
-        // The Inner loop will advance all layers unless doing so will break one of two constraints
-        // 1) A layer may not proceed ahead of the master simulation object's current time
-        // 2) A layer may not proceed ahead of any layer that is computed before it
-        // The do while loop ensures that all layers are tested at least once while allowing
-        // layers with small time steps to be updated more than once
-        // If a layer with a large time step is after a layer with a small time step the
-        // layer with the large time step will wait for multiple timesteps from the preceeding
-        // layer.
-
         // Make room for this output step's results
         catchment_outflows_.resize(catchment_outflows_.size() + catchment_indexes_.size());
         nexus_downstream_flows_.resize(nexus_downstream_flows_.size() + nexus_indexes_.size());
 
-        // this is the time that layers are trying to reach (or get as close as possible)
-        auto next_time = catchment_formulation_manager_->Simulation_Time_Object->next_timestep_epoch_time();
-        auto next_time_private = sim_time_->next_timestep_epoch_time();
-
-        // this is the time that the layer above the current layer is at
-        auto prev_layer_time = next_time;
-
-        // this is the time that the least advanced layer is at
-        auto layer_min_next_time = next_time;
-        do {
-            for (auto& layer : layers_) {
-                auto layer_next_time = layer->next_timestep_epoch_time();
-
-                // only advance if you would not pass the master next time and the previous layer
-                // next time
-                if (layer_next_time <= next_time && layer_next_time <= prev_layer_time) {
-                    if (simulation_step_ % 100 == 0) {
-                        ss << "Updating layer: '" << layer->get_name() << "' at output step " << simulation_step_;
-                        LOG(ss.str(), LogLevel::DEBUG);
-                        ss.str("");
-                    }
-
-                    boost::span<double> catchment_span(catchment_outflows_.data() + (simulation_step_ * catchment_indexes_.size()),
-                                                       catchment_indexes_.size());
-                    boost::span<double> nexus_span(nexus_downstream_flows_.data() + (simulation_step_ * nexus_indexes_.size()),
-                                                   nexus_indexes_.size());
-                    layer->update_models(
-                        catchment_span,
-                        catchment_indexes_,
-                        nexus_span,
-                        nexus_indexes_,
-                        simulation_step_
-                    ); // assume update_models() calls time->advance_timestep()
-                    prev_layer_time = layer_next_time;
-                } else {
-                    layer_min_next_time = prev_layer_time = layer->current_timestep_epoch_time();
-                }
-
-                if (layer_min_next_time > layer_next_time) {
-                    layer_min_next_time = layer_next_time;
-                }
-            } // done layers
-        } while (layer_min_next_time < next_time); // rerun the loop until the last layer would pass the master next time
+        advance_one_output_step();
 
         if (simulation_step_ + 1 < num_times) {
             sim_time_->advance_timestep();
             catchment_formulation_manager_->Simulation_Time_Object->advance_timestep();
         }
     }
+}
+
+void NgenSimulation::advance_one_output_step()
+{
+    // The Inner loop will advance all layers unless doing so will break one of two constraints
+    // 1) A layer may not proceed ahead of the master simulation object's current time
+    // 2) A layer may not proceed ahead of any layer that is computed before it
+    // The do while loop ensures that all layers are tested at least once while allowing
+    // layers with small time steps to be updated more than once
+    // If a layer with a large time step is after a layer with a small time step the
+    // layer with the large time step will wait for multiple timesteps from the preceeding
+    // layer.
+
+    // this is the time that layers are trying to reach (or get as close as possible)
+    auto next_time = catchment_formulation_manager_->Simulation_Time_Object->next_timestep_epoch_time();
+    auto next_time_private = sim_time_->next_timestep_epoch_time();
+
+    // this is the time that the layer above the current layer is at
+    auto prev_layer_time = next_time;
+
+    // this is the time that the least advanced layer is at
+    auto layer_min_next_time = next_time;
+    do {
+        for (auto& layer : layers_) {
+            auto layer_next_time = layer->next_timestep_epoch_time();
+
+            // only advance if you would not pass the master next time and the previous layer
+            // next time
+            if (layer_next_time <= next_time && layer_next_time <= prev_layer_time) {
+                if (simulation_step_ % 100 == 0) {
+                    LOG(("Updating layer: '" + layer->get_name() + "' at output step " + std::to_string(simulation_step_)), LogLevel::DEBUG);
+                }
+
+                boost::span<double> catchment_span(catchment_outflows_.data() + (simulation_step_ * catchment_indexes_.size()),
+                                                   catchment_indexes_.size());
+                boost::span<double> nexus_span(nexus_downstream_flows_.data() + (simulation_step_ * nexus_indexes_.size()),
+                                               nexus_indexes_.size());
+                layer->update_models(
+                                     catchment_span,
+                                     catchment_indexes_,
+                                     nexus_span,
+                                     nexus_indexes_,
+                                     simulation_step_
+                                     ); // assume update_models() calls time->advance_timestep()
+                prev_layer_time = layer_next_time;
+            } else {
+                layer_min_next_time = prev_layer_time = layer->current_timestep_epoch_time();
+            }
+
+            if (layer_min_next_time > layer_next_time) {
+                layer_min_next_time = layer_next_time;
+            }
+        } // done layers
+    } while (layer_min_next_time < next_time); // rerun the loop until the last layer would pass the master next time
+
 }
 
 int NgenSimulation::get_nexus_index(std::string const& nexus_id) const
@@ -112,11 +114,6 @@ int NgenSimulation::get_nexus_index(std::string const& nexus_id) const
 double NgenSimulation::get_nexus_outflow(int nexus_index, int timestep_index) const
 {
     return nexus_downstream_flows_[timestep_index * nexus_indexes_.size() + nexus_index];
-}
-
-void NgenSimulation::advance_one_output_step()
-{
-
 }
 
 void NgenSimulation::run_routing(NgenSimulation::hy_features_t &features)
