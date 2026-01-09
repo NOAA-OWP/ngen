a3541fe16131f745713203e8124afa23738f61df
Robert Bartel <robert.bartel@noaa.gov>
2026-01-09 10:14:05 -0500

    Adjust NexusOutputsMgr interface for receive data.
    Change receive_data_entry signature in NexusOutputsMgr (and subclasses)
    to also accept epoch time when receiving data (some subtypes use time
    stamp, so keeping that, but accepting both so as to not need to add any
    conversion logic, since both are provided from the simulation time
    object).

--- include/utilities/PerFormulationNexusOutputMgr.hpp
+++ include/utilities/PerFormulationNexusOutputMgr.hpp
@@ -202,11 +202,13 @@ namespace utils
          * @param formulation_id The id of the formulation involved in producing this data.
          * @param nexus_id The id for the nexus to which this data applies.
          * @param current_time_index The simulation output time index for the data.
-         * @param current_timestamp The timestamp for the data.
+         * @param current_epoch_time The corresponding epoch simulation time for current time index, and for the data.
+         * @param current_timestamp The timestamp corresponding to the current epoche time.
          * @param flow_data_at_t The nexus flow contribution at this time index (the main data to write).
          */
         void receive_data_entry(const std::string& formulation_id, const std::string& nexus_id, long current_time_index,
-                                const std::string& current_timestamp, double flow_data_at_t) override
+                                const time_t& current_epoch_time, const std::string &current_timestamp,
+                                double flow_data_at_t) override
         {
             if (current_formulation_id.empty()) {
                 current_formulation_id = formulation_id;
