6bde3c4a69eaea033b749b7bbd0e7e0c94f59331
Robert Bartel <robert.bartel@noaa.gov>
2026-01-09 10:16:55 -0500

    Update/optimize SurfaceLayer update_models.
    Updating for changes to NexusOutputsMgr.receive_data_entry signature,
    and also moving the retrieval of simulation time details from outside of
    loop for writing nexus outputs that only loops over the nexuses.

--- src/core/SurfaceLayer.cpp
+++ src/core/SurfaceLayer.cpp
@@ -12,11 +12,15 @@ void ngen::SurfaceLayer::update_models()
 
     //At this point, could make an internal routing pass, extracting flows from nexuses and routing
     //across the flowpath to the next nexus.
+
+    // Grab time details (but only once since the output_time_index doesn' (and shouldn't) change
+    std::string current_timestamp = simulation_time.get_timestamp(current_time_index);
+    // Remember: above call to simulation_time.get_timestamp(current_time_index) has to be made first (see those funcs)
+    time_t current_date_time_epoch = simulation_time.get_current_epoch_time();
+
     //Once everything is updated for this timestep, dump the nexus output
     for(const auto& id : features.nexuses()) 
     {
-        std::string current_timestamp = simulation_time.get_timestamp(current_time_index);
-        
         #if NGEN_WITH_MPI
         if (features.is_remote_sender_nexus(id)) {
             //Ensures only one side of the dual sided remote nexus actually doing this...
@@ -41,7 +45,8 @@ void ngen::SurfaceLayer::update_models()
         double contribution_at_t = features.nexus_at(id)->get_downstream_flow(cat_id, current_time_index, 100.0);
         // TODO: (later) eventually may want to use this form, if we support multiple formulations per catchment
         //nexus_outputs_mgr->receive_data_entry(form_id, id, current_time_index, current_timestamp, contribution_at_t);
-        nexus_outputs_mgr->receive_data_entry(id, current_time_index, current_timestamp, contribution_at_t);
+        nexus_outputs_mgr->receive_data_entry(id, current_time_index, current_date_time_epoch,
+                                              current_timestamp, contribution_at_t);
 
     } //done nexuses
     nexus_outputs_mgr->commit_writes();
