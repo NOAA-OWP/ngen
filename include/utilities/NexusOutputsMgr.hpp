7abf05f8d5fdca34704a3f1c0b538b9773929d38
Robert Bartel <robert.bartel@noaa.gov>
2026-01-14 10:54:50 -0500

    Create time_marker struct for NexusOutputsMgr.
    Creating time_marker struct for time params needed when manager receives
    data, updating NexusOutputsMgr and subclass function signatures and
    definitions to use, and updating usages in main code and tests.

--- include/utilities/NexusOutputsMgr.hpp
+++ include/utilities/NexusOutputsMgr.hpp
@@ -9,6 +9,16 @@
 
 namespace utils
 {
+    /** Simple helper struct for tracking meaningful points in time for output writing. */
+    struct time_marker
+    {
+        const long sim_time_index;
+        const time_t epoch_time;
+        const std::string time_stamp;
+
+        time_marker(const long sim_time_index, const time_t epoch_time, const std::string& time_stamp)
+            : sim_time_index(sim_time_index), epoch_time(epoch_time), time_stamp(time_stamp) {}
+    };
 
     /**
      * Abstract class for managing and writing to nexus data files.
@@ -34,36 +44,26 @@ namespace utils
         /**
          * Receive a data entry for this nexus, specifying details including the formulation id.
          *
-         * Implementations can write immediately or cache
-         *
          * @param formulation_id The id of the formulation involved in producing this data.
          * @param nexus_id The id for the nexus to which this data applies.
-         * @param current_time_index The simulation output time index for the data.
-         * @param current_epoch_time The corresponding epoch simulation time for current time index, and for the data.
-         * @param current_timestamp The timestamp corresponding to the current epoche time.
+         * @param data_time_marker A marker for the current simulation time for the data.
          * @param flow_data_at_t The nexus flow contribution at this time index (the main data to write).
          */
         virtual void receive_data_entry(const std::string &formulation_id, const std::string &nexus_id,
-                                        long current_time_index, const time_t& current_epoch_time,
-                                        const std::string &current_timestamp, double flow_data_at_t) = 0;
+                                        const time_marker &data_time_marker, const double flow_data_at_t) = 0;
 
         /**
          * Receive a data entry for this nexus, specifying details but using the default formulation id (however that is
          * determined).
          *
          * @param nexus_id The id for the nexus to which this data applies.
-         * @param current_time_index The simulation output time index for the data.
-         * @param current_epoch_time The corresponding epoch simulation time for current time index, and for the data
-         * @param current_timestamp The timestamp corresponding to the current epoche time.
+         * @param data_time_marker A marker for the current simulation time for the data.
          * @param flow_data_at_t The nexus flow contribution at this time index (the main data to write).
          *
          * @see get_default_formulation_id
          */
-        virtual void receive_data_entry(const std::string &nexus_id, long current_time_index,
-                                        const time_t& current_epoch_time, const std::string &current_timestamp,
-                                        double flow_data_at_t) {
-            receive_data_entry(get_default_formulation_id(), nexus_id, current_time_index, current_epoch_time,
-                               current_timestamp, flow_data_at_t);
+        virtual void receive_data_entry(const std::string &nexus_id, const time_marker &data_time_marker, const double flow_data_at_t) {
+            receive_data_entry(get_default_formulation_id(), nexus_id, data_time_marker, flow_data_at_t);
         }
 
     protected:
