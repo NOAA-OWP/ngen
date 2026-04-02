21eea932379c939259cb48f2dd51a1a9286b0cc0
Ian Todd <itodd@dewberry.com>
2026-04-02 08:24:04 -0400

    Ensure realization catchment IDs start with "cat-" when searching the geometry fabric

--- include/realizations/catchment/Formulation_Manager.hpp
+++ include/realizations/catchment/Formulation_Manager.hpp
@@ -170,13 +170,18 @@ namespace realization {
                     for (std::pair<std::string, boost::property_tree::ptree> catchment_config : *possible_catchment_configs) {
                         ss.str(""); ss << "Processing catchment: " << catchment_config.first << std::endl;
                         LOG(ss.str(), LogLevel::DEBUG);
+                        // ensure catchment's ID starts with "cat-" so it can be found in the fabric
+                        std::string catchment_id = catchment_config.first;
+                        if (strncmp(catchment_id.c_str(), "cat-", 4) != 0) {
+                            catchment_id = "cat-" + catchment_id;
+                        }
 
-                        int catchment_index = fabric->find(catchment_config.first);
+                        int catchment_index = fabric->find(catchment_id);
                         if (catchment_index == -1) {
                             #ifndef NGEN_QUIET
                                 ss.str("");
                                 ss << "Formulation_Manager::read: Cannot create formulation for catchment "
-                                   << catchment_config.first
+                                   << catchment_id
                                    << " that isn't identified in the hydrofabric or requested subset" << std::endl;
                                 LOG(ss.str(), LogLevel::WARNING);
                             #endif
@@ -200,7 +205,7 @@ namespace realization {
                         this->add_formulation(
                             this->construct_formulation_from_config(
                                 simulation_time_config,
-                                "cat-" + catchment_config.first,
+                                catchment_id,
                                 catchment_formulation,
                                 output_stream
                             )
