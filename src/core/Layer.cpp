94c07c49b7b1c136888520678b826bbe12c4f29c
Phil Miller <phil@intensecomputing.com>
2026-05-26 13:36:20 -0700

    Fix whitespace mismatch

--- src/core/Layer.cpp
+++ src/core/Layer.cpp
@@ -47,7 +47,7 @@ void ngen::Layer::update_models(boost::span<double> catchment_outflows,
         }
 #if NGEN_WITH_ROUTING && NGEN_WITH_ROUTING_TROUTE_BMI
         int results_index = catchment_indexes[id];
-	// XXX: This is currently accumulating in meters of depth, which may not be desirable
+        // XXX: This is currently accumulating in meters of depth, which may not be desirable
         catchment_outflows[results_index] += response;
 #endif // NGEN_WITH_ROUTING && NGEN_WITH_ROUTING_TROUTE_BMI
         std::string output = std::to_string(output_time_index)+","+current_timestamp+","+
